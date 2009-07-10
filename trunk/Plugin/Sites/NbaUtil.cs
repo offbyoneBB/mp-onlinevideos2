eae263e36bf4602fb486b89c60d8285d0bb25d82
offbyone@gmx.de <offbyone@gmx.de@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2009-07-10 14:18:37 +0000

    Added NBA. Changed Json API to a more modern one (LitJson to JayRock). chnaged the dynamic category discovery signature to allow it to be more flexible.

---
+++ trunk/Plugin/Sites/NbaUtil.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Text;
+using System.Text.RegularExpressions;
+using MediaPortal.GUI.Library;
+using MediaPortal.Configuration;
+using Jayrock.Json;
+
+namespace OnlineVideos.Sites
+{
+    /// <summary>
+    /// Description of NbaUtil.
+    /// </summary>
+    public class NbaUtil : SiteUtilBase
+    {
+        static Regex subCategoriesAvailableRegEx = new Regex(@"<div\sid=""nbaSubNav""", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+        static Regex subCategoriesRegEx = new Regex(@"\s<li><a\sid=""(?<url>.+)\#subNav""\shref=""javascript\:void\(0\)\;"">(?<name>.+)</a></li>", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+        static Regex videosRegEx = new Regex(@"loadVideoArray\(\snew\sArray\((\s+'(?<url>[^']+)'[^']+)*\)\)\;", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.ExplicitCapture);
+
+        static Regex amountPagesInSectionRegEx = new Regex(@"<div\sid=""nbaVideoFileCount"">(\d+)</div>", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+        static Regex sectionPath1RegEx = new Regex(@"<div\sid=""nbaVideoFilePath"">(.+)</div>", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+        static Regex sectionPath2RegEx = new Regex(@"<div\sid=""nbaVideoFileMap"">(.+)</div>", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+        
+        public override int DiscoverDynamicCategories(SiteSettings site)
+        {
+            Dictionary<string, List<Category>> dynamicOnes = new Dictionary<string, List<Category>>();
+
+            foreach(RssLink link in site.Categories.Values)
+            {
+                string data = GetWebData(link.Url);
+                if (subCategoriesAvailableRegEx.IsMatch(data))
+                {
+                    dynamicOnes.Add(link.Name, new List<Category>());
+                    Match match = subCategoriesRegEx.Match(data);
+                    while (match.Success)
+                    {
+                        dynamicOnes[link.Name].Add(new RssLink() { Name = string.Format("{0}: {1}", link.Name, System.Web.HttpUtility.HtmlDecode(match.Groups["name"].Value)), Url = link.Url.Substring(0, link.Url.LastIndexOf('/')+1) + match.Groups["url"].Value + ".html" });
+                        match = match.NextMatch();
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<string, List<Category>> o in dynamicOnes)
+            {
+                foreach (RssLink link in o.Value) if (!site.Categories.ContainsKey(link.Name)) site.Categories.Add(link.Name, link);                
+            }
+
+            foreach (string key in dynamicOnes.Keys) site.Categories.Remove(key);
+
+            site.DynamicCategoriesDiscovered = true;
+            return site.Categories.Count;
+        }
+
+        public override List<VideoInfo> getVideoList(Category category)
+        {
+            currentPage = 1; pagesInCategory = 1; sectionBaseUrl = ""; // reset next/prev fields
+            return getVideoList(((RssLink)category).Url);
+        }
+
+        List<VideoInfo> getVideoList(string inUrl)
+        {
+            List<VideoInfo> videos = new List<VideoInfo>();
+
+            string data = GetWebData(inUrl);
+
+            // prepare next previous infos
+            Match m = amountPagesInSectionRegEx.Match(data);
+            if (m.Success)
+            {
+                pagesInCategory = int.Parse(m.Groups[1].Value);
+
+                m = sectionPath1RegEx.Match(data);
+                if (m.Success) sectionBaseUrl = "http://www.nba.com/" + m.Groups[1].Value;
+
+                m = sectionPath2RegEx.Match(data);
+                if (m.Success) sectionBaseUrl += m.Groups[1].Value;
+            }
+
+            m = videosRegEx.Match(data);
+            if (m.Success)
+            {
+                foreach (Capture c in m.Groups["url"].Captures)
+                {
+                    VideoInfo vi = new VideoInfo();
+
+                    string jsonUrl = c.Value;
+                    if (!jsonUrl.StartsWith("/"))
+                        jsonUrl = "/video/" + jsonUrl;
+                    if (!jsonUrl.EndsWith(".json")) 
+                        jsonUrl += ".json";
+
+                    jsonUrl = "http://www.nba.com" + jsonUrl;
+                    JsonObject jsonData = (JsonObject)GetWebDataAsJson(jsonUrl);
+                    vi.Title = (string)jsonData["headline"];
+                    vi.Description = (string)jsonData["description"];
+                    vi.Length = (string)jsonData["dateCreated"];
+                    vi.ImageUrl = (string)((jsonData["images"] as JsonArray)[2] as JsonObject)["resource"];
+
+                    vi.VideoUrl = string.Format("http://nba.cdn.turner.com/nba/big{0}_nba_{1}.flv", (string)jsonData["location"], (string)((JsonArray)jsonData["sizes"])[0]);
+
+                    videos.Add(vi);
+                }                
+            }
+
+            return videos;
+        }
+
+        int pagesInCategory = 1;
+        int currentPage = 1;
+        string sectionBaseUrl = "";
+        public override bool hasNextPage()
+        {
+            return currentPage < pagesInCategory;
+        }
+                
+        public override bool hasPreviousPage()
+        {
+            return currentPage > 1;
+        }
+
+        public override List<VideoInfo> getNextPageVideos()
+        {
+            currentPage++;
+            return getVideoList(sectionBaseUrl + currentPage.ToString() + ".html");
+        }
+
+        public override List<VideoInfo> getPreviousPageVideos()
+        {
+            currentPage--;
+            return getVideoList(sectionBaseUrl + currentPage.ToString() + ".html");
+        }
+    }
+}
