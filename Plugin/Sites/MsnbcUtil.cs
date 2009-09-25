48b174775e786ffa3c7017519de77d09154d965e
offbyone <offbyone@gmx.de>
2009-09-25 18:00:05 +0000

    Updated Msnbc to use the new rss feeds instead of outdated ones.

--- Plugin/Sites/MsnbcUtil.cs
+++ Plugin/Sites/MsnbcUtil.cs
@@ -12,11 +12,23 @@ namespace OnlineVideos.Sites
 	/// </summary>
 	public class MsnbcUtil: SiteUtilBase
 	{
-        static Regex idRegex = new Regex("#(.*)");
+        static Regex idRegex = new Regex(@"vPlayer\('(\d{5,10})'", RegexOptions.Compiled);
 
 		public override string getUrl(OnlineVideos.VideoInfo video, OnlineVideos.SiteSettings foSite)
 		{
-            return String.Format("http://www.msnbc.msn.com/default.cdnx/id/{0}/displaymode/1157/?t=.flv", video.VideoUrl);
+            if (video.VideoUrl.Contains("/vp/"))
+            {
+                return String.Format("http://www.msnbc.msn.com/id/{0}/displaymode/1157/?t=.flv", video.VideoUrl.Substring(video.VideoUrl.LastIndexOf("#") + 1));
+            }
+            else
+            {
+                string data = GetWebData(video.VideoUrl);
+                Match m = idRegex.Match(data);
+                if (m.Success)
+                    return String.Format("http://www.msnbc.msn.com/id/{0}/displaymode/1157/?t=.flv", m.Groups[1].Value);
+                else
+                    return "";
+            }
 		}
 
         public override List<VideoInfo> getVideoList(Category category)
@@ -26,12 +38,16 @@ namespace OnlineVideos.Sites
             VideoInfo video;            
             foreach (RssItem rssItem in loRssItemList)
             {
+                if (rssItem.contentList.Count == 0) continue;
+                if (rssItem.contentList[0].medium != "video") continue;
+
                 video = new VideoInfo();
                 video.Description = rssItem.description;
                 video.ImageUrl = rssItem.contentList[0].url;
                 video.Title = rssItem.title.Replace("Video: ", "");
                 video.Length = rssItem.contentList[0].duration;
-                video.VideoUrl = idRegex.Match(rssItem.guid).Groups[1].Value;
+                if (string.IsNullOrEmpty(video.Length)) video.Length = rssItem.pubDate;
+                video.VideoUrl = rssItem.guid;
                 loVideoList.Add(video);
             }
             return loVideoList;
