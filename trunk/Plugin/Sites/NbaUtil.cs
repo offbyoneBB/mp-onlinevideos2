3f10e1f669e1e39f2834bacd91e143616f807793
offbyone@gmx.de <offbyone@gmx.de@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2010-01-01 22:51:24 +0000

    Added Search to NbaUtil

--- trunk/Plugin/Sites/NbaUtil.cs
+++ trunk/Plugin/Sites/NbaUtil.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections;
+using System.ComponentModel;
 using System.IO;
 using System.Net;
 using System.Xml;
@@ -16,7 +17,10 @@ namespace OnlineVideos.Sites
     /// Description of NbaUtil.
     /// </summary>
     public class NbaUtil : SiteUtilBase
-    {        
+    {
+        [Category("OnlineVideosUserConfiguration"), Description("Defines number of videos to retrieve as one page.")]
+        int itemsPerPage = 15;
+
         public override int DiscoverDynamicCategories()
         {
             string js = GetWebData("http://i.cdn.turner.com/nba/nba/z/.e/js/pkg/video/652.js");
@@ -48,7 +52,7 @@ namespace OnlineVideos.Sites
         public override List<VideoInfo> getVideoList(Category category)
         {
             currentPage = 1; pagesInCategory = 1; sectionBaseUrl = ((RssLink)category).Url; // reset next/prev fields
-            return getVideoList("http://searchapp.nba.com/nba-search/query.jsp?type=advvideo&start=1&npp=" + nPerPage + "&" + ((RssLink)category).Url + "&season=0910&sort=recent");
+            return getVideoList("http://searchapp.nba.com/nba-search/query.jsp?type=advvideo&start=1&npp=" + itemsPerPage + "&" + ((RssLink)category).Url + "&season=0910&sort=recent");
         }
 
         List<VideoInfo> getVideoList(string inUrl)
@@ -59,7 +63,7 @@ namespace OnlineVideos.Sites
             string json = Regex.Match(data, "<textarea id=\"jsCode\">(?<json>.+)</textarea>", RegexOptions.Singleline).Groups["json"].Value;
             JsonObject jsonData = Jayrock.Json.Conversion.JsonConvert.Import(json) as JsonObject;
             int NumVideosTotal = ((JsonNumber)((JsonObject)jsonData["metaResults"])["advvideo"]).ToInt32();
-            pagesInCategory = NumVideosTotal / nPerPage;
+            pagesInCategory = NumVideosTotal / itemsPerPage;
             foreach (JsonObject jo in ((JsonArray)jsonData["results"])[0] as JsonArray)
             {
                 VideoInfo vi = new VideoInfo();
@@ -79,9 +83,10 @@ namespace OnlineVideos.Sites
         static DateTime UNIXTimeToDateTime(double unixTime)
         {
             return new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(unixTime).ToLocalTime();
-        } 
+        }
 
-        int nPerPage = 15;
+        #region Paging
+
         int pagesInCategory = 1;
         int currentPage = 1;
         string sectionBaseUrl = "";
@@ -98,15 +103,34 @@ namespace OnlineVideos.Sites
         public override List<VideoInfo> getNextPageVideos()
         {
             currentPage++;
-            int start = ((currentPage - 1) * nPerPage) + 1;
-            return getVideoList("http://searchapp.nba.com/nba-search/query.jsp?type=advvideo&start=" + start.ToString() + "&npp=" + nPerPage + "&" + sectionBaseUrl + "&season=0910&sort=recent");
+            int start = ((currentPage - 1) * itemsPerPage) + 1;
+            return getVideoList("http://searchapp.nba.com/nba-search/query.jsp?type=advvideo&start=" + start.ToString() + "&npp=" + itemsPerPage + "&" + sectionBaseUrl + "&season=0910&sort=recent");
         }
 
         public override List<VideoInfo> getPreviousPageVideos()
         {
             currentPage--;
-            int start = ((currentPage - 1) * nPerPage) + 1;
-            return getVideoList("http://searchapp.nba.com/nba-search/query.jsp?type=advvideo&start=" + start.ToString() + "&npp=" + nPerPage + "&" + sectionBaseUrl + "&season=0910&sort=recent");
+            int start = ((currentPage - 1) * itemsPerPage) + 1;
+            return getVideoList("http://searchapp.nba.com/nba-search/query.jsp?type=advvideo&start=" + start.ToString() + "&npp=" + itemsPerPage + "&" + sectionBaseUrl + "&season=0910&sort=recent");
         }
+
+        #endregion
+
+        #region Search
+
+        string searchUrl = "http://searchapp.nba.com/nba-search/query.jsp?type=advvideo&start=1&npp={1}&text={0}&sort=recent";
+
+        public override bool CanSearch { get { return true; } }
+
+        public override List<VideoInfo> Search(string query)
+        {
+            query = System.Web.HttpUtility.UrlEncode(query);
+            currentPage = 1; pagesInCategory = 1; sectionBaseUrl = "text="+query; // reset next/prev fields
+            string url = string.Format(searchUrl, query, itemsPerPage);
+            return getVideoList(url);
+        }
+
+        #endregion
+
     }
 }
