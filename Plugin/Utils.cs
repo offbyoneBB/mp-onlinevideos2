c43edcc2dcc8c35345ed5633e1dc7f7aaace21b9
offbyone <offbyone@gmx.de>
2010-03-06 21:41:00 +0000

    ImImproved Apple Trailers.

--- Plugin/Utils.cs
+++ Plugin/Utils.cs
@@ -11,5 +11,16 @@ namespace OnlineVideos
         {
             return Regex.Replace(PascalString, "(?!^)([A-Z])", " $1");
         }
+
+        public static string ReplaceEscapedUnicodeCharacter(string input)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(
+                input,
+                @"(?:\\|%)[uU]([0-9A-Fa-f]{4})",
+                delegate(System.Text.RegularExpressions.Match match)
+                {
+                    return ((char)Int32.Parse(match.Value.Substring(2), System.Globalization.NumberStyles.HexNumber)).ToString();
+                });
+        }
     }
 }
