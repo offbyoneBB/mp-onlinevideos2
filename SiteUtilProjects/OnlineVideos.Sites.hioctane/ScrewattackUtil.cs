6a1279e34dc994d046453462d8595a401df5fdd2
hioctane <hioctane@gmx.net>
2010-03-13 16:14:40 +0000

    Added ScrewattackUtil Improved GenericSiteUtil

---
+++ SiteUtilProjects/OnlineVideos.Sites.hioctane/ScrewattackUtil.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace OnlineVideos.Sites
+{
+    public class ScrewattackUtil : GenericSiteUtil
+    {
+
+        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to parse a html page for videos. Group names: 'VideoUrl', 'ImageUrl', 'Title', 'Duration', 'Description'.")]
+        string videoListRegEx;
+        [Category("OnlineVideosConfiguration"), Description("Url used for prepending relative links.")]
+        string baseUrl;
+        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to parse a html page for videoUrl. Group names: 'vid', 'pid'.")]
+        string urlRegEx;
+
+        Regex regEx_Url;
+
+        public override void Initialize(SiteSettings siteSettings)
+        {
+            base.Initialize(siteSettings);
+            base.videoListRegEx = this.videoListRegEx;
+            base.baseUrl = this.baseUrl;
+            regEx_Url = new Regex(urlRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
+        }
+
+        public override String getUrl(VideoInfo video)
+        {
+            string data = GetWebData(video.VideoUrl);
+
+            if (!string.IsNullOrEmpty(data))
+            {
+                Match m = regEx_Url.Match(data);
+                if (m.Success)
+                {
+                    string url = "http://v.giantrealm.com/sax/" + m.Groups["pid"].Value + "/" + m.Groups["vid"].Value;
+                    data = GetWebData(url);
+                    if (!string.IsNullOrEmpty(data))
+                    {
+                        m = Regex.Match(data, @"<file-hq>\s*(.+?)\s*</file-hq>");
+                        if (m.Success) return m.Groups[1].Value;
+
+                        m = Regex.Match(data, @"<file>\s*(.+?)\s*</file>");
+                        if (m.Success) return m.Groups[1].Value;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
