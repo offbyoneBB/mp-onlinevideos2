c5c343d6ab4c5e5d194fbf8f543a35fe3a65f493
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2010-03-24 22:06:01 +0000

    added KabouterWesley

---
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/KabouterWesleyUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineVideos.Sites
+{
+    public class KabouterWesleyUtil : GenericSiteUtil
+    {
+        public override string getUrl(VideoInfo video)
+        {
+            string s = base.getUrl(video);
+            return UrlTricks.YoutubeTrick(s, video);
+        }
+    }
+}
