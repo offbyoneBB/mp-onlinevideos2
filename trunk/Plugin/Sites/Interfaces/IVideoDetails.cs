c4691fc9632552da400605d903a4789c9dca8f6f
apondman <apondman@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2010-04-12 21:38:41 +0000

    Added ReverseProxy class and request handler interfaces Added IVideoDetails interface to allow publishing of extended VideoInfo properties Added initial logic to publish extended properties when available

---
+++ trunk/Plugin/Sites/Interfaces/IVideoDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineVideos {
+    public interface IVideoDetails {
+
+        /// <summary>
+        /// Populates a dictionary with custom information that can be published to the skin
+        /// </summary>
+        /// <returns></returns>
+        Dictionary<string, string> GetExtendedProperties();
+
+    }
+}
