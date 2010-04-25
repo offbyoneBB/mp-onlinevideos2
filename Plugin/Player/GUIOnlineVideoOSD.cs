a700a79cbbe1c02e93d94f0734b87316974af9e6
offbyone <offbyone@gmx.de>
2010-04-25 18:59:50 +0000

    Created OnlineVideos own Fullscreen class with own OSD. Simply took the classes and skinfiles from the normal VideoFullScreen and OSD, but added the TV progressbar. Now the progress of the buffer is displayed while watching when opening any of the OSDs.

---
+++ Plugin/Player/GUIOnlineVideoOSD.cs
@@ -0,0 +1,17 @@
+﻿using MediaPortal.GUI.Video;
+using MediaPortal.GUI.Library;
+
+namespace OnlineVideos.Player
+{
+    public class GUIOnlineVideoOSD : GUIVideoOSD
+    {
+        public const int WINDOW_ONLINEVIDEOS_OSD = 4759;
+        public override int GetID { get { return WINDOW_ONLINEVIDEOS_OSD; } set { } }
+
+        public override bool Init()
+        {
+            bool bResult = Load(GUIGraphicsContext.Skin + @"\myonlinevideosOSD.xml");
+            return bResult;
+        }        
+    }
+}
