6a768cbfc6dfc990555b43ac1ca8a35cca8ef4f4
offbyone@gmx.de <offbyone@gmx.de@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2010-06-12 18:15:32 +0000

    Improved Miro, now has user setting how many category pages to retrieve at once (default 5 pages = 50 categories).

--- trunk/SiteUtilProjects/OnlineVideos.Sites.offbyone/MiroUtil.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.offbyone/MiroUtil.cs
@@ -20,8 +20,12 @@ namespace OnlineVideos.Sites
                                               <h4><a\shref=""(?<mirourl>[^""]+)""[^>]*>(?<name>[^<]*)</a></h4>\s*
                                               <p>(?<desc>(?:(?!</p>).)*)</p>\s*</div>
                                               (?:(?!http\://subscribe\.getmiro\.com).)*(?<url>[^""]*)""";
+        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to find how many pages a subcategory has")]
+        string lastPageRegEx = @"<li\s*>\s*<span><a\s*href=""[^""]+"">\s*(?<lastPage>\d+)\s*</a></span>\s*</li>\s*</ul>";
+        [Category("OnlineVideosUserConfiguration"), Description("Number of Pages to retrieve when looking for categories of a genre. Default is 5.")]
+        int maxCategoryPages = 5;
 
-        Regex regEx_dynamicCategories, regEx_dynamicSubCategories;
+        Regex regEx_dynamicCategories, regEx_dynamicSubCategories, regEx_lastPage;
 
         public override void Initialize(SiteSettings siteSettings)
         {
@@ -29,6 +33,7 @@ namespace OnlineVideos.Sites
 
             if (!string.IsNullOrEmpty(dynamicCategoriesRegEx)) regEx_dynamicCategories = new Regex(dynamicCategoriesRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture);
             if (!string.IsNullOrEmpty(dynamicSubCategoriesRegEx)) regEx_dynamicSubCategories = new Regex(dynamicSubCategoriesRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture);
+            if (!string.IsNullOrEmpty(lastPageRegEx)) regEx_lastPage = new Regex(lastPageRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture);
         }
 
         public override int DiscoverDynamicCategories()
@@ -58,21 +63,39 @@ namespace OnlineVideos.Sites
             parentCategory.SubCategories = new List<Category>();
             if (!string.IsNullOrEmpty(catsString))
             {
-                Match m = regEx_dynamicSubCategories.Match(catsString);
-                while (m.Success)
+                int maxPages = 1;
+                Match m = regEx_lastPage.Match(catsString);
+                if (m.Success) int.TryParse(m.Groups["lastPage"].Value, out maxPages);
+                if (maxPages > maxCategoryPages) maxPages = maxCategoryPages;
+                int currentPage = 1;
+                while (currentPage <= maxPages)
                 {
-                    RssLink rss = new RssLink();
-                    rss.SubCategoriesDiscovered = true;
-                    rss.HasSubCategories = false;                    
-                    rss.Name = m.Groups["name"].Value;
-                    rss.Url = System.Web.HttpUtility.ParseQueryString(System.Web.HttpUtility.HtmlDecode(new System.Uri(m.Groups["url"].Value).Query))[0];
-                    rss.Description = m.Groups["desc"].Value;
-                    string feedId = m.Groups["mirourl"].Value.Substring(m.Groups["mirourl"].Value.LastIndexOf('/') + 1);
-                    rss.Thumb = string.Format("http://s3.miroguide.com/static/media/thumbnails/200x133/{0}.jpeg", feedId)
-                    + "|" + string.Format("http://s3.miroguide.com/static/media/thumbnails/97x65/{0}.jpeg", feedId);
-                    parentCategory.SubCategories.Add(rss);
-                    rss.ParentCategory = parentCategory;
-                    m = m.NextMatch();
+                    m = regEx_dynamicSubCategories.Match(catsString);
+                    while (m.Success)
+                    {
+                        RssLink rss = new RssLink();
+                        rss.SubCategoriesDiscovered = true;
+                        rss.HasSubCategories = false;
+                        rss.Name = m.Groups["name"].Value;
+                        rss.Url = System.Web.HttpUtility.ParseQueryString(System.Web.HttpUtility.HtmlDecode(new System.Uri(m.Groups["url"].Value).Query))[0];
+                        rss.Description = m.Groups["desc"].Value;
+                        string feedId = m.Groups["mirourl"].Value.Substring(m.Groups["mirourl"].Value.LastIndexOf('/') + 1);
+                        rss.Thumb = string.Format("http://s3.miroguide.com/static/media/thumbnails/200x133/{0}.jpeg", feedId)
+                        + "|" + string.Format("http://s3.miroguide.com/static/media/thumbnails/97x65/{0}.jpeg", feedId);
+                        parentCategory.SubCategories.Add(rss);
+                        rss.ParentCategory = parentCategory;
+                        m = m.NextMatch();
+                    }
+                    currentPage++;
+                    try
+                    {
+                        catsString = GetWebData((parentCategory as RssLink).Url + "?page=" + currentPage.ToString());
+                    }
+                    catch (Exception)
+                    {
+                        break; // couldn't get page, stop trying to get any further
+                    }
+                    
                 }
                 parentCategory.SubCategoriesDiscovered = true;
             }
