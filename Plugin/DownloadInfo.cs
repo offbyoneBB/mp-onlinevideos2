313653905ec38ed355e14799c83c3c13b4529146
offbyone <offbyone@gmx.de>
2010-07-09 15:02:57 +0000

    Improved DownloadInfo.

--- Plugin/DownloadInfo.cs
+++ Plugin/DownloadInfo.cs
@@ -15,23 +15,29 @@ namespace OnlineVideos
         public string LocalFile { get; set; }
         public DateTime Start { get; private set; }
         public int PercentComplete { get; private set; }
+        public int KbDownloaded { get; private set; }
         public int KbTotal { get; private set; }
         public IDownloader Downloader { get; set; }
         public void DownloadProgressCallback(long TotalBytesToReceive, long currentBytes)
         {
-            if (TotalBytesToReceive != 0) PercentComplete = (int)((float)currentBytes / TotalBytesToReceive * 100f);
-            KbTotal = (int)(TotalBytesToReceive / 1024);
-
+            if (TotalBytesToReceive > 0)
+            {
+                PercentComplete = (int)((float)currentBytes / TotalBytesToReceive * 100f);
+                KbTotal = (int)(TotalBytesToReceive / 1024);
+            }
+            KbDownloaded = (int)(currentBytes / 1024);
         }
         public void DownloadProgressCallback(object sender, DownloadProgressChangedEventArgs e)
         {
             PercentComplete = e.ProgressPercentage;
             KbTotal = (int)(e.TotalBytesToReceive / 1024);
+            KbDownloaded = (int)(e.BytesReceived / 1024);
         }
         public void DownloadProgressCallback(object sender, MMSStreamProgressChangedEventArgs e)
         {
             PercentComplete = e.ProgressPercentage;
             KbTotal = (int)(e.TotalBytesToReceive / 1024);
+            KbDownloaded = (int)(e.BytesReceived / 1024);
         }
     }
 }
