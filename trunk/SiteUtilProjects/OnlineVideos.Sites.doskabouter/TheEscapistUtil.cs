2dbd79414349f67c32533373448f8af181c13d6d
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2010-08-10 21:20:06 +0000

    - added EscapistMagazine - work on SiteParser

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/TheEscapistUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineVideos.Sites
+{
+    public class TheEscapistUtil : GenericSiteUtil
+    {
+        public override int DiscoverDynamicCategories()
+        {
+            base.DiscoverDynamicCategories();
+            foreach (Category cat in Settings.Categories)
+                cat.HasSubCategories = (((RssLink)cat).Url.EndsWith(@"videos/galleries"));
+            return Settings.Categories.Count;
+        }
+
+        public override List<VideoInfo> getVideoList(Category category)
+        {
+            string webData = GetWebData(((RssLink)category).Url);
+            int p = webData.IndexOf(@"<div class='video_box_footer'>");
+            if (p >= 0) webData = webData.Substring(0, p);
+            return Parse(null, webData);
+        }
+    }
+}
