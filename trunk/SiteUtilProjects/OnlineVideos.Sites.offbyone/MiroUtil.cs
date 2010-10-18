ae29411c32ae5ab63f1297fe4ab3274bbe339cc6
offbyone@gmx.de <offbyone@gmx.de@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2010-10-18 10:12:11 +0000

    Enhanced Miro to resolve Hoster as well. Should get more videos to play.

--- trunk/SiteUtilProjects/OnlineVideos.Sites.offbyone/MiroUtil.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.offbyone/MiroUtil.cs
@@ -10,36 +10,14 @@ namespace OnlineVideos.Sites
     /// <summary>
     /// Miro API documentation: https://develop.participatoryculture.org/trac/democracy/wiki/MiroGuideApi
     /// </summary>
-    public class MiroUtil : SiteUtilBase
+    public class MiroUtil : GenericSiteUtil
     {
-        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to parse the results of apiUrl_categoryList for dynamic categories. Group names: 'url', 'title'.")]
-        string dynamicCategoriesRegEx = @"\{'url'\:\su'(?<url>[^']+)',\s'name'\:\su'(?<title>[^']+)'";
-        [Category("OnlineVideosConfiguration"), Description("Url for retrieving the list of categories.")]
-        string apiUrl_categoryList = "https://www.miroguide.com/api/list_categories";
-        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to parse a html page for dynamic categories. Group names: 'url', 'name', 'desc', 'mirourl'.")]
-        string dynamicSubCategoriesRegEx = @"<div\sclass=""searchResultContent"">\s*
-                                              <h4><a\shref=""(?<mirourl>[^""]+)""[^>]*>(?<name>[^<]*)</a></h4>\s*
-                                              <p>(?<desc>(?:(?!</p>).)*)</p>\s*</div>
-                                              (?:(?!http\://subscribe\.getmiro\.com).)*(?<url>[^""]*)""";
-        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to find how many pages a subcategory has")]
-        string lastPageRegEx = @"<li\s*>\s*<span><a\s*href=""[^""]+"">\s*(?<lastPage>\d+)\s*</a></span>\s*</li>\s*</ul>";
         [Category("OnlineVideosUserConfiguration"), Description("Number of Pages to retrieve when looking for categories of a genre. Default is 5.")]
-        int maxCategoryPages = 5;
-
-        Regex regEx_dynamicCategories, regEx_dynamicSubCategories, regEx_lastPage;
-
-        public override void Initialize(SiteSettings siteSettings)
-        {
-            base.Initialize(siteSettings);
-
-            if (!string.IsNullOrEmpty(dynamicCategoriesRegEx)) regEx_dynamicCategories = new Regex(dynamicCategoriesRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture);
-            if (!string.IsNullOrEmpty(dynamicSubCategoriesRegEx)) regEx_dynamicSubCategories = new Regex(dynamicSubCategoriesRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture);
-            if (!string.IsNullOrEmpty(lastPageRegEx)) regEx_lastPage = new Regex(lastPageRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture);
-        }
-
+        int maxCategoryPages = 5;        
+        
         public override int DiscoverDynamicCategories()
         {
-            string catsString = GetWebData(apiUrl_categoryList);
+            string catsString = GetWebData(baseUrl);
             if (!string.IsNullOrEmpty(catsString))
             {
                 Settings.Categories.Clear();
@@ -65,7 +43,7 @@ namespace OnlineVideos.Sites
             if (!string.IsNullOrEmpty(catsString))
             {
                 int maxPages = 1;
-                Match m = regEx_lastPage.Match(catsString);
+                Match m = regEx_NextPage.Match(catsString);
                 if (m.Success) int.TryParse(m.Groups["lastPage"].Value, out maxPages);
                 if (maxPages > maxCategoryPages) maxPages = maxCategoryPages;
                 int currentPage = 1;
@@ -101,31 +79,6 @@ namespace OnlineVideos.Sites
                 parentCategory.SubCategoriesDiscovered = true;
             }
             return parentCategory.SubCategories.Count;
-        }
-
-        public override List<VideoInfo> getVideoList(Category category)
-        {
-            List<VideoInfo> loVideoList = new List<VideoInfo>();
-            string catsString = GetWebData((category as RssLink).Url);
-            if (!string.IsNullOrEmpty(catsString))
-            {
-                foreach (RssItem rssItem in GetWebData<RssDocument>(((RssLink)category).Url).Channel.Items)
-                {
-                    loVideoList.Add(VideoInfo.FromRssItem(rssItem, false, new Predicate<string>(delegate(string url) { return url.StartsWith("http://"); })));
-                }
-            }
-            return loVideoList;
-        }
-
-        public override string getUrl(VideoInfo video)
-        {
-            if (video.VideoUrl.ToLower().Contains("youtube.com")) 
-            {
-
-                video.PlaybackOptions = Hoster.Base.HosterFactory.GetHoster("Youtube").getPlaybackOptions(video.VideoUrl);
-                return (video.PlaybackOptions == null || video.PlaybackOptions.Count == 0) ? "" : video.PlaybackOptions.First().Value;
-            }
-            else return base.getUrl(video);
         }
     }
 }
