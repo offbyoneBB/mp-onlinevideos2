2c31eb522b1fcbe81fe08fbd974e0144bcf65ddb
offbyone@gmx.de <offbyone@gmx.de@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2010-10-28 20:15:30 +0000

    Documented interfaces a little.

--- trunk/Plugin/Sites/Interfaces/IUserStore.cs
+++ trunk/Plugin/Sites/Interfaces/IUserStore.cs
@@ -2,6 +2,9 @@
 
 namespace OnlineVideos
 {
+    /// <summary>
+    /// This Interface defines the methods needed to store and retrieve arbitrary configurational data for fields tagged with the Category attribute 'OnlineVideosUserConfiguration'.
+    /// </summary>
     public interface IUserStore
     {
         string GetValue(string key);
--- trunk/Plugin/Sites/Interfaces/IVideoDetails.cs
+++ trunk/Plugin/Sites/Interfaces/IVideoDetails.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace OnlineVideos {
-    public interface IVideoDetails {
-
+namespace OnlineVideos
+{
+    /// <summary>
+    /// If the object found at <see cref="VideoInfo.Other"/> implements this interface, the additional info is set to the GUI.
+    /// </summary>
+    public interface IVideoDetails
+    {
         /// <summary>
         /// Populates a dictionary with custom information that can be published to the skin
         /// </summary>
         /// <returns></returns>
         Dictionary<string, string> GetExtendedProperties();
-
     }
 }
