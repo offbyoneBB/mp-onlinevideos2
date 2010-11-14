fd5f99ee7c16c28e91f0db60d47c4009c7f7fe0d
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2010-11-14 22:28:34 +0000

    fixed onsnetnuenen attempt to improve Tudou

--- trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/OnsNetNuenenUtil.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/OnsNetNuenenUtil.cs
@@ -11,108 +11,39 @@ using System.Text.RegularExpressions;
 
 namespace OnlineVideos.Sites
 {
-    public class OnsNetNuenenUtil : SiteUtilBase
+    public class OnsNetNuenenUtil : GenericSiteUtil
     {
-        private string baseUrl = "http://onsnet.video4all.nl/";
-        private string categoryRegex = @"<li\sclass=""[^\s]+\sSubMenuItem[^""]+""><a\shref=""(?<url>[^""]+)""\s+><span>(?<title>[^<]+)</span>";
-        private string videoListRegex = @"<div\sclass=""VideoThumbnail""><a\shref=""\#""\s+onclick=""getElementById\('VideoFrame'\)\.src='(?<url>[^']+)';""><img\ssrc=""(?<thumb>[^""]+)""\swidth=""[^""]+""\sheight=""[^""]+""\stitle=""(?<title>[^""]+)""";
-        private string subCategoryRegex = @"<option\s+value=""(?<id>[^""]+)""[^>]*>(?<title>[^<]+)</option>";
-        private string mainPageRegex = @"<div\sclass=""(New|Search)VideoThumb""><a\shref=""index\.php\?cid=(?<id>[^&]+)&[^<]+<img\swidth=""[^""]+""\ssrc=""(?<thumb>[^""]+)""\stitle=""(?<title>[^""]+)""";
+        private string mainPageRegex = @"<div\sclass=""NewVideoThumb""><a\shref=""index\.php\?visid=(?<id>[^&]+)&[^<]+<img\swidth=""[^""]+""\ssrc=""(?<thumb>[^""]+)""\stitle=""(?<title>[^""]+)""";
+        private string searchRegex = @"<div\sclass=""SearchVideoThumb""><a\shref=""index\.php\?(?:(?!visid).)*visid=(?<id>[^&]+)&[^<]+<img\ssrc=""(?<thumb>[^""]+)""[^>]*>[^>]*>(?<title>[^<]*)<";
 
         public OnsNetNuenenUtil()
         {
         }
 
-        private Regex regEx_Category;
-        private Regex regEx_SubCategory;
-        private Regex regEx_VideoList;
         private Regex regEx_MainPage;
+        private Regex regEx_Search;
 
         public override void Initialize(SiteSettings siteSettings)
         {
             base.Initialize(siteSettings);
-
-            regEx_Category = new Regex(categoryRegex, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
-            regEx_SubCategory = new Regex(subCategoryRegex, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
-            regEx_VideoList = new Regex(videoListRegex, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
             regEx_MainPage = new Regex(mainPageRegex, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
+            regEx_Search = new Regex(searchRegex, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
         }
 
         public override int DiscoverDynamicCategories()
         {
-            Settings.Categories.Clear();
 
-            string data = GetWebData(baseUrl + "corp/index.php?page=48&site=4");
-            if (!string.IsNullOrEmpty(data))
-            {
-                RssLink cat2 = new RssLink();
-                cat2.Name = "Beste video's";
-                cat2.Url = "!http://onsnet.video4all.nl/corp/index.php?ord=hig&page=48&site=4&ln=nl";
-                Settings.Categories.Add(cat2);
+            RssLink cat2 = new RssLink();
+            cat2.Name = "Beste video's";
+            cat2.Url = "!http://www.onsnet.tv/corp/index.php?ord=hig&page=48&site=4&ln=nl";
+            Settings.Categories.Add(cat2);
 
-                cat2 = new RssLink();
-                cat2.Name = "Nieuwste video's";
-                cat2.Url = "!http://onsnet.video4all.nl/corp/index.php?ord=new&page=48&site=4&ln=nl";
-                Settings.Categories.Add(cat2);
+            cat2 = new RssLink();
+            cat2.Name = "Nieuwste video's";
+            cat2.Url = "!http://www.onsnet.tv/corp/index.php?ord=new&page=48&site=4&ln=nl";
+            Settings.Categories.Add(cat2);
 
-                Match m = regEx_Category.Match(data);
-                while (m.Success)
-                {
-                    RssLink cat = new RssLink();
-                    cat.Name = HttpUtility.HtmlDecode(m.Groups["title"].Value);
-                    string url = baseUrl + "corp/" + HttpUtility.HtmlDecode(m.Groups["url"].Value);
-                    cat.Url = url;
-                    cat.HasSubCategories = true;
-                    Settings.Categories.Add(cat);
-                    m = m.NextMatch();
-                }
-
-
-                Settings.DynamicCategoriesDiscovered = true;
-                return Settings.Categories.Count;
-            }
-            return 0;
-        }
-
-        public override int DiscoverSubCategories(Category parentCategory)
-        {
-            string url = ((RssLink)parentCategory).Url;
-
-            string webData = GetWebData(url);
-            int i = webData.IndexOf("videoalbumsubject");
-            if (i >= 0) webData = webData.Substring(i);
-
-            int p = url.IndexOf("itemid=");
-            if (p >= 0)
-            {
-                int q = url.IndexOf('&', p);
-                url = url.Substring(0, p + 7) + "{0}" + url.Substring(q);
-            }
-
-            parentCategory.SubCategories = new List<Category>();
-
-            Match m = regEx_SubCategory.Match(webData);
-            while (m.Success)
-            {
-                RssLink cat = new RssLink();
-                cat.Name = HttpUtility.HtmlDecode(m.Groups["title"].Value);
-                cat.Url = String.Format(url, HttpUtility.HtmlDecode(m.Groups["id"].Value));
-                cat.SubCategoriesDiscovered = true;
-                cat.HasSubCategories = false;
-
-                parentCategory.SubCategories.Add(cat);
-                cat.ParentCategory = parentCategory;
-                m = m.NextMatch();
-            }
-
-            parentCategory.SubCategoriesDiscovered = true;
-            return parentCategory.SubCategories.Count;
-        }
-
-        public override String getUrl(VideoInfo video)
-        {
-            string url = video.VideoUrl;
-            return ParseASX(url)[0];
+            return base.DiscoverDynamicCategories() + 2;
         }
 
         public override bool CanSearch
@@ -122,53 +53,36 @@ namespace OnlineVideos.Sites
 
         public override List<VideoInfo> Search(string query)
         {
-            return getPagedVideoList(String.Format("!http://onsnet.video4all.nl/corp/?page=54&site=4&search=1&srch_str={0}&x=0&y=0&submit_frmSearch=1", query));
+            return getVideoList(String.Format("http://www.onsnet.tv/corp/?page=54&site=4&search=1&srch_str={0}&x=0&y=0&submit_frmSearch=1", query),
+                regEx_Search);
         }
 
         public override List<VideoInfo> getVideoList(Category category)
         {
-            RssLink rssLink = (RssLink)category;
-            return getPagedVideoList(rssLink.Url);
+            string url = ((RssLink)category).Url;
+            if (!url.StartsWith("!")) return base.getVideoList(category);
+            else
+                return getVideoList(url.Substring(1), regEx_MainPage);
         }
 
-        private List<VideoInfo> getPagedVideoList(string url)
+        private List<VideoInfo> getVideoList(string url, Regex regex)
         {
-            Regex regEx = null;
-            if (url.StartsWith("!"))
-            {
-                url = url.Substring(1);
-                regEx = regEx_MainPage;
-            }
-            else
-                regEx = regEx_VideoList;
-
             string webData = GetWebData(url);
             List<VideoInfo> videos = new List<VideoInfo>();
             if (!string.IsNullOrEmpty(webData))
             {
-                Match m = regEx.Match(webData);
+                Match m = regex.Match(webData);
                 while (m.Success)
                 {
                     VideoInfo video = new VideoInfo();
-
                     video.Title = HttpUtility.HtmlDecode(HttpUtility.HtmlDecode(m.Groups["title"].Value));
-                    if (HttpUtility.HtmlDecode(m.Groups["url"].Value) == String.Empty)
-                        video.VideoUrl = baseUrl + "src/getVideo.php?tp=wmv&cid=" + m.Groups["id"].Value + "&lvl=3&tid=328";
-                    else
-                    {
-                        video.VideoUrl = baseUrl.TrimEnd('/') + HttpUtility.HtmlDecode(m.Groups["url"].Value);
-                        video.VideoUrl = video.VideoUrl.Replace(@"/win_video.php?tra=yes&tit=no&cid=", @"/getVideo.php?tp=wmv&cid=") +
-                            "&lvl=3&tid=328";
-                    }
-                    video.ImageUrl = baseUrl.TrimEnd('/') + HttpUtility.HtmlDecode(m.Groups["thumb"].Value);
+                    video.VideoUrl = string.Format(videoListRegExFormatString, m.Groups["id"].Value);
+                    video.ImageUrl = new Uri(new Uri(baseUrl), HttpUtility.HtmlDecode(m.Groups["thumb"].Value)).AbsoluteUri;
                     videos.Add(video);
                     m = m.NextMatch();
                 }
-
             }
             return videos;
-
         }
-
     }
 }
