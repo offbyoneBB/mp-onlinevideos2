5a4b5f81794ae702a36a39ac3951534f416c7963
offbyone <offbyone@gmx.de>
2010-11-28 21:11:31 +0000

    Changed logging from error to info in Play.

--- OnlineVideos.MediaPortal1/Player/OnlineVideosPlayer.cs
+++ OnlineVideos.MediaPortal1/Player/OnlineVideosPlayer.cs
@@ -496,7 +496,7 @@ namespace OnlineVideos.MediaPortal1.Player
             if (!engine.LoadSubtitles(graphBuilder, m_strCurrentFile))
             {
                 tmpstr = engine.ToString().Substring(engine.ToString().LastIndexOf(".") + 1);
-                Log.Instance.Error("VideoPlayerVMR7_OnlineVideo: {0} subtitle configured in MP but misconfigured, disabled!", tmpstr);
+                Log.Instance.Info("VideoPlayerVMR7_OnlineVideo: {0} subtitle configured in MP but misconfigured, disabled!", tmpstr);
                 SubEngine.engine = new SubEngine.DummyEngine();
             }
 
@@ -504,7 +504,7 @@ namespace OnlineVideos.MediaPortal1.Player
             if (!postengine.LoadPostProcessing(graphBuilder))
             {
                 tmpstr = postengine.ToString().Substring(postengine.ToString().LastIndexOf(".") + 1);
-                Log.Instance.Error("VideoPlayerVMR7_OnlineVideo: {0} postprocessing configured in MP but misconfigured!", tmpstr);
+                Log.Instance.Info("VideoPlayerVMR7_OnlineVideo: {0} postprocessing configured in MP but misconfigured!", tmpstr);
                 PostProcessingEngine.engine = new PostProcessingEngine.DummyEngine();
             }
 #endif
