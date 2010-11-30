2a32dd1d710504c6943ced74f6d1c1c7e52c869f
hioctane <hioctane@gmx.net>
2010-11-30 11:22:36 +0000

    fixed MyVideoSerien

--- SiteUtilProjects/OnlineVideos.Sites.hioctane/MyVideoSerienUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.hioctane/MyVideoSerienUtil.cs
@@ -10,20 +10,8 @@ namespace OnlineVideos.Sites
         {
             string videoId = video.VideoUrl.Substring(0, video.VideoUrl.LastIndexOf("/"));
             videoId = videoId.Substring(videoId.LastIndexOf("/") + 1);
-            
-            string data = GetWebData(video.VideoUrl);
-            string regex = @"/de/(?<url>[^t]+)thumbs/" + videoId;
-            Match m = Regex.Match(data, regex);
-            if (m.Success)
-            {
-                string part = m.Groups["url"].Value;
-                m = Regex.Match(data, @"addVariable\('SERVER','(?<url>[^']+)'\)");
-                if(m.Success){
-                    string server = m.Groups["url"].Value;
-                    return "http://" + server + "/" + part + videoId + ".flv";
-                }
-            }
-            return null;
+            string url = video.ImageUrl.Substring(0, video.ImageUrl.IndexOf("/thumbs")) + "/" + videoId + ".flv";
+            return url;
         }
     }
 }
