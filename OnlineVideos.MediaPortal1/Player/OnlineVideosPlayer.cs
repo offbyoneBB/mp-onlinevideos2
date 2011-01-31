06dfdf1b14722654ccbe9adfc554082c0e7de0d7
offbyone <offbyone@gmx.de>
2011-01-31 17:19:51 +0000

    simplified OnlineVideosPlayer, removed unused code, mms:// graphs now also started in background

--- OnlineVideos.MediaPortal1/Player/OnlineVideosPlayer.cs
+++ OnlineVideos.MediaPortal1/Player/OnlineVideosPlayer.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using System.Threading;
 using DirectShowLib;
 using DShowNET.Helper;
 using MediaPortal.GUI.Library;
 using MediaPortal.Player;
 using MediaPortal.Profile;
-using System.Globalization;
+
 #if !MP11
 using MediaPortal.Player.Subtitles;
 using MediaPortal.Player.PostProcessing;
@@ -32,214 +31,11 @@ namespace OnlineVideos.MediaPortal1.Player
 
         protected override bool GetInterfaces()
         {
-            string currentFileLower = CurrentFile.ToLower();
-
-            /*if( currentFileLower.Contains(".avi") && !currentFileLower.Contains(".flv"))
-                return BuildGraphForDivxStream();
-            else*/
-            
             if (graphBuilder != null) // graph was already started and playback file buffered
                 return FinishPreparedGraph();
-            else if (currentFileLower.StartsWith("mms://") || currentFileLower.Contains(".asf"))
-                return BuildGraphForMMS();
-            else if (currentFileLower.StartsWith("rtsp://"))
-                return BuildGraphForRTSP();
             else
                 return base.GetInterfaces();
         }      
-
-        bool BuildGraphForDivxStream()
-        {
-            base.graphBuilder = (IGraphBuilder)new FilterGraph();
-            // add video renderer
-            Log.Instance.Info("VideoPlayerVMR9: Enabling DX9 exclusive mode", new object[0]);
-            GUIMessage message = new GUIMessage(GUIMessage.MessageType.GUI_MSG_SWITCH_FULL_WINDOWED, 0, 0, 0, 1, 0, null);
-            GUIWindowManager.SendMessage(message);
-            this.Vmr9 = new VMR9Util();
-            this.Vmr9.AddVMR9(base.graphBuilder);
-            this.Vmr9.Enable(false);
-
-            // add the audio renderer
-            using (Settings settings = new MPSettings())
-            {
-                string audiorenderer = settings.GetValueAsString("movieplayer", "audiorenderer", "Default DirectSound Device");
-                DirectShowUtil.AddAudioRendererToGraph(base.graphBuilder, audiorenderer, false);
-            }
-
-            // add the source filter
-            IBaseFilter sourceFilter = DirectShowUtil.AddFilterToGraph(base.graphBuilder, "Haali Media Splitter");
-            if (sourceFilter == null) return false;
-            int result = ((IFileSourceFilter)sourceFilter).Load(CurrentFile, null);
-            if (result != 0) return false;
-
-            IEnumPins enumPins;
-            IPin[] sourceFilterPins = new IPin[2];
-            int fetched;
-            result = sourceFilter.EnumPins(out enumPins);
-            result = enumPins.Next(2, sourceFilterPins, out fetched);
-            // connect the pins automatically
-            result = base.graphBuilder.Render(sourceFilterPins[0]); // audio
-            result = base.graphBuilder.Render(sourceFilterPins[1]); // video
-
-            // cleanup resources
-            DirectShowUtil.ReleaseComObject(sourceFilter);
-            DirectShowUtil.ReleaseComObject(enumPins);
-            DirectShowUtil.ReleaseComObject(sourceFilterPins[0]);
-            DirectShowUtil.ReleaseComObject(sourceFilterPins[1]);
-
-            // set fields for playback
-            base.mediaCtrl = (IMediaControl)base.graphBuilder;
-            base.mediaEvt = (IMediaEventEx)base.graphBuilder;
-            base.mediaSeek = (IMediaSeeking)base.graphBuilder;
-            base.mediaPos = (IMediaPosition)base.graphBuilder;
-            base.basicAudio = base.graphBuilder as IBasicAudio;
-            DirectShowUtil.EnableDeInterlace(base.graphBuilder);
-            base.m_iVideoWidth = this.Vmr9.VideoWidth;
-            base.m_iVideoHeight = this.Vmr9.VideoHeight;
-
-            if (!this.Vmr9.IsVMR9Connected)
-            {
-                base.mediaCtrl = null;
-                CloseInterfaces();
-                return false;
-            }
-            this.Vmr9.SetDeinterlaceMode();
-
-            return true;
-        }
-
-        bool BuildGraphForRTSP()
-        {
-            base.graphBuilder = (IGraphBuilder)new FilterGraph();
-            // add video renderer
-            Log.Instance.Info("VideoPlayerVMR9: Enabling DX9 exclusive mode", new object[0]);
-            GUIMessage message = new GUIMessage(GUIMessage.MessageType.GUI_MSG_SWITCH_FULL_WINDOWED, 0, 0, 0, 1, 0, null);
-            GUIWindowManager.SendMessage(message);
-            this.Vmr9 = new VMR9Util();
-            this.Vmr9.AddVMR9(base.graphBuilder);
-            this.Vmr9.Enable(false);
-
-            // add the audio renderer
-            using (Settings settings = new MPSettings())
-            {
-                string audiorenderer = settings.GetValueAsString("movieplayer", "audiorenderer", "Default DirectSound Device");
-                DirectShowUtil.AddAudioRendererToGraph(base.graphBuilder, audiorenderer, false);
-            }
-
-            // add the source filter
-            IBaseFilter sourceFilter = DirectShowUtil.AddFilterToGraph(base.graphBuilder, "MediaPortal File Reader");
-            if (sourceFilter == null) return false;
-            int result = ((IFileSourceFilter)sourceFilter).Load(CurrentFile, null);
-            if (result != 0) return false;
-
-            IEnumPins enumPins;
-            IPin[] sourceFilterPins = new IPin[2];
-            int fetched;
-            result = sourceFilter.EnumPins(out enumPins);
-            result = enumPins.Next(2, sourceFilterPins, out fetched);
-            // connect the pins automatically
-            result = base.graphBuilder.Render(sourceFilterPins[0]); // audio
-            result = base.graphBuilder.Render(sourceFilterPins[1]); // video
-
-            // cleanup resources
-            DirectShowUtil.ReleaseComObject(sourceFilter);
-            DirectShowUtil.ReleaseComObject(enumPins);
-            DirectShowUtil.ReleaseComObject(sourceFilterPins[0]);
-            DirectShowUtil.ReleaseComObject(sourceFilterPins[1]);
-
-            // set fields for playback
-            base.mediaCtrl = (IMediaControl)base.graphBuilder;
-            base.mediaEvt = (IMediaEventEx)base.graphBuilder;
-            base.mediaSeek = (IMediaSeeking)base.graphBuilder;
-            base.mediaPos = (IMediaPosition)base.graphBuilder;
-            base.basicAudio = base.graphBuilder as IBasicAudio;
-            DirectShowUtil.EnableDeInterlace(base.graphBuilder);
-            base.m_iVideoWidth = this.Vmr9.VideoWidth;
-            base.m_iVideoHeight = this.Vmr9.VideoHeight;
-
-            if (!this.Vmr9.IsVMR9Connected)
-            {
-                base.mediaCtrl = null;
-                CloseInterfaces();
-                return false;
-            }
-            this.Vmr9.SetDeinterlaceMode();
-
-            return true;
-        }
-
-        bool BuildGraphForMMS()
-        {
-            base.graphBuilder = (IGraphBuilder)new FilterGraph();
-            _rotEntry = new DsROTEntry((IFilterGraph)graphBuilder);
-
-            Log.Instance.Info("VideoPlayerVMR9: Enabling DX9 exclusive mode", new object[0]);
-            GUIMessage message = new GUIMessage(GUIMessage.MessageType.GUI_MSG_SWITCH_FULL_WINDOWED, 0, 0, 0, 1, 0, null);
-            GUIWindowManager.SendMessage(message);
-            this.Vmr9 = new VMR9Util();
-            this.Vmr9.AddVMR9(base.graphBuilder);
-            this.Vmr9.Enable(false);
-
-            // add the source filter manually
-            IBaseFilter sourceFilter = DirectShowUtil.AddFilterToGraph(base.graphBuilder, "WM ASF Reader");
-
-            if (sourceFilter == null)
-            {
-                Error.SetError("Unable to load DirectshowFilter: WM ASF Reader", "Windows Media Player not installed?");
-                Log.Instance.Error("Unable to load DirectshowFilter: WM ASF Reader", new object[0]);
-                return false;
-            }
-
-            // load the file with the source filter
-            int result = ((IFileSourceFilter)sourceFilter).Load(CurrentFile, null);
-            if (result != 0) return false;
-
-            // mms streams allow skipping so set buffered to 100%
-            PercentageBuffered = 100.0f;
-            GUIPropertyManager.SetProperty("#TV.Record.percent3", PercentageBuffered.ToString());
-
-            // add the audio renderer
-            using (Settings settings = new MPSettings())
-            {
-                string audiorenderer = settings.GetValueAsString("movieplayer", "audiorenderer", "Default DirectSound Device");
-                DirectShowUtil.AddAudioRendererToGraph(graphBuilder, audiorenderer, false);
-            }
-
-            // get the output pins of the WM ASF Reader
-            IEnumPins enumPins;
-            IPin[] sourceFilterPins = new IPin[2];
-            int fetched;
-            result = sourceFilter.EnumPins(out enumPins);
-            result = enumPins.Next(2, sourceFilterPins, out fetched);
-            // connect the pins automatically
-            base.graphBuilder.Render(sourceFilterPins[0]);
-            base.graphBuilder.Render(sourceFilterPins[1]);
-
-            // cleanup resources
-            DirectShowUtil.ReleaseComObject(sourceFilter);
-            DirectShowUtil.ReleaseComObject(enumPins);
-            DirectShowUtil.ReleaseComObject(sourceFilterPins[0]);
-            DirectShowUtil.ReleaseComObject(sourceFilterPins[1]);
-
-            // set fields for playback
-            base.mediaCtrl = (IMediaControl)base.graphBuilder;
-            base.mediaEvt = (IMediaEventEx)base.graphBuilder;
-            base.mediaSeek = (IMediaSeeking)base.graphBuilder;
-            base.mediaPos = (IMediaPosition)base.graphBuilder;
-            base.basicAudio = base.graphBuilder as IBasicAudio;
-            DirectShowUtil.EnableDeInterlace(base.graphBuilder);
-            base.m_iVideoWidth = this.Vmr9.VideoWidth;
-            base.m_iVideoHeight = this.Vmr9.VideoHeight;
-
-            if (!this.Vmr9.IsVMR9Connected)
-            {
-                base.mediaCtrl = null;
-                CloseInterfaces();
-                return false;
-            }
-            this.Vmr9.SetDeinterlaceMode();
-            return true;
-        }
 
         float PercentageBuffered;
 
@@ -274,7 +70,7 @@ namespace OnlineVideos.MediaPortal1.Player
         }
 
         public bool BufferingStopped { get; protected set; }
-        public void StopBuffering() { BufferingStopped = true; }     
+        public void StopBuffering() { BufferingStopped = true; }
 
         /// <summary>
         /// If the url to be played can be buffered before starting playback, this function
@@ -285,16 +81,17 @@ namespace OnlineVideos.MediaPortal1.Player
         public bool? PrepareGraph()
         {
             Uri uri = new Uri(CurrentFile);
-            if (uri.Scheme == "http")
+            string sourceFilterName = uri.Scheme == "http" ? PluginConfiguration.Instance.httpSourceFilterName : (uri.Scheme == "mms" || CurrentFile.ToLower().Contains(".asf")) ? "WM ASF Reader" : string.Empty;
+            if (!string.IsNullOrEmpty(sourceFilterName))
             {
                 graphBuilder = (IGraphBuilder)new FilterGraph();
                 _rotEntry = new DsROTEntry((IFilterGraph)graphBuilder);
-                
+
                 Vmr9 = new VMR9Util();
                 Vmr9.AddVMR9(graphBuilder);
                 Vmr9.Enable(false);
                 // set VMR9 back to NOT Active -> otherwise GUI is not refreshed while graph is building
-                GUIGraphicsContext.Vmr9Active = false;                
+                GUIGraphicsContext.Vmr9Active = false;
 
                 // add the audio renderer
                 using (Settings settings = new MPSettings())
@@ -311,21 +108,21 @@ namespace OnlineVideos.MediaPortal1.Player
                 basicAudio = (IBasicAudio)graphBuilder;
                 videoWin = (IVideoWindow)graphBuilder;
 
-                // add the http source filter
+                // add the source filter
                 IBaseFilter sourceFilter = null;
                 try
                 {
-                    sourceFilter = DirectShowUtil.AddFilterToGraph(graphBuilder, PluginConfiguration.Instance.httpSourceFilterName);
+                    sourceFilter = DirectShowUtil.AddFilterToGraph(graphBuilder, sourceFilterName);
                 }
                 catch (Exception ex)
                 {
-                    Log.Instance.Warn("Error adding '{0}' filter to graph and loading with url: {1}", PluginConfiguration.Instance.httpSourceFilterName, ex.Message);
+                    Log.Instance.Warn("Error adding '{0}' filter to graph: {1}", sourceFilterName, ex.Message);
+                    return null;
                 }
                 finally
                 {
                     if (sourceFilter != null) DirectShowUtil.ReleaseComObject(sourceFilter, 2000);
-                }                
-
+                }
                 return true;
             }
             else
@@ -342,15 +139,18 @@ namespace OnlineVideos.MediaPortal1.Player
         /// <returns>true, when playback can be started</returns>
         public bool BufferFile()
         {
+            Uri uri = new Uri(CurrentFile);
             bool PlaybackReady = false;
             IBaseFilter sourceFilter = null;
             try
             {
-                int result = graphBuilder.FindFilterByName(PluginConfiguration.Instance.httpSourceFilterName, out sourceFilter);
+                string sourceFilterName = uri.Scheme == "http" ? PluginConfiguration.Instance.httpSourceFilterName : (uri.Scheme == "mms" || CurrentFile.ToLower().Contains(".asf")) ? "WM ASF Reader" : string.Empty;
+                int result = graphBuilder.FindFilterByName(sourceFilterName, out sourceFilter);
                 if (result != 0) return false;
-
+                
+                // translate url if usage of rtmp proxy is not wanted
                 string urlToLoad = CurrentFile;
-                if (!PluginConfiguration.Instance.useRtmpProxy)
+                if (uri.Scheme == "http" && !PluginConfiguration.Instance.useRtmpProxy)
                 {
                     string proxyIndicator = ReverseProxy.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance, "rtmp://");
                     if (CurrentFile.StartsWith(proxyIndicator))
@@ -413,6 +213,7 @@ namespace OnlineVideos.MediaPortal1.Player
                 {
                     DirectShowUtil.RenderUnconnectedOutputPins(graphBuilder, sourceFilter);
                     PercentageBuffered = 100.0f; // no progress reporing possible
+                    GUIPropertyManager.SetProperty("#TV.Record.percent3", PercentageBuffered.ToString());
                     PlaybackReady = true;
                 }
             }
