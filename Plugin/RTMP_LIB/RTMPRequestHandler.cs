be10ec59f04843065dd4cd41d86cd66016f16fbc
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2011-02-08 20:16:59 +0000

    added yle-areena

--- Plugin/RTMP_LIB/RTMPRequestHandler.cs
+++ Plugin/RTMP_LIB/RTMPRequestHandler.cs
@@ -27,56 +27,30 @@ namespace RTMP_LIB
             return invalidHeader;
         }
 
-        public void HandleRequest(string url, HybridDSP.Net.HTTP.HTTPServerRequest request, HybridDSP.Net.HTTP.HTTPServerResponse response)
+        public static void ConnectAndGetStream(RTMP rtmp, HybridDSP.Net.HTTP.HTTPServerRequest request, HybridDSP.Net.HTTP.HTTPServerResponse response,
+            ref bool invalidHeader)
         {
-            Thread.CurrentThread.Name = "RTMPProxy";
-            Logger.Log("Request");
-            RTMP rtmp = null;
             Stream responseStream = null;
             try
             {
-                NameValueCollection paramsHash = System.Web.HttpUtility.ParseQueryString(new Uri(url).Query);
-
-                Link link = new Link();
-                if (!string.IsNullOrEmpty(paramsHash["rtmpurl"])) link = Link.FromRtmpUrl(new Uri(paramsHash["rtmpurl"]));
-                if (!string.IsNullOrEmpty(paramsHash["app"])) link.app = paramsHash["app"];
-                if (!string.IsNullOrEmpty(paramsHash["tcUrl"])) link.tcUrl = paramsHash["tcUrl"];
-                if (!string.IsNullOrEmpty(paramsHash["hostname"])) link.hostname = paramsHash["hostname"];
-                if (!string.IsNullOrEmpty(paramsHash["port"])) link.port = int.Parse(paramsHash["port"]);
-                if (!string.IsNullOrEmpty(paramsHash["playpath"])) link.playpath = paramsHash["playpath"];
-                if (!string.IsNullOrEmpty(paramsHash["subscribepath"])) link.subscribepath = paramsHash["subscribepath"];
-                if (!string.IsNullOrEmpty(paramsHash["pageurl"])) link.pageUrl = paramsHash["pageurl"];
-                if (!string.IsNullOrEmpty(paramsHash["swfurl"])) link.swfUrl = paramsHash["swfurl"];
-                if (!string.IsNullOrEmpty(paramsHash["swfsize"])) link.SWFSize = int.Parse(paramsHash["swfsize"]);
-                if (!string.IsNullOrEmpty(paramsHash["swfhash"])) link.SWFHash = Link.ArrayFromHexString(paramsHash["swfhash"]);
-                if (!string.IsNullOrEmpty(paramsHash["swfVfy"])) { link.swfUrl = paramsHash["swfVfy"]; link.swfVerify = true; }
-                if (!string.IsNullOrEmpty(paramsHash["live"])) bool.TryParse(paramsHash["live"], out link.bLiveStream);
-                if (!string.IsNullOrEmpty(paramsHash["auth"])) link.auth = paramsHash["auth"];
-                if (!string.IsNullOrEmpty(paramsHash["token"])) link.token = paramsHash["token"];
-                if (!string.IsNullOrEmpty(paramsHash["conn"])) link.extras = Link.ParseAMF(paramsHash["conn"]);
-                if (link.tcUrl != null && link.tcUrl.ToLower().StartsWith("rtmpe")) link.protocol = Protocol.RTMPE;
-
-                if (link.swfVerify) link.GetSwf();
-
-                rtmp = new RTMP() { Link = link };
                 bool connected = rtmp.Connect();
-                
+
                 if (connected)
                 {
                     request.KeepAlive = true; // keep connection alive
                     response.ContentType = "video/x-flv";
                     response.KeepAlive = true;
                     //response.ChunkedTransferEncoding = true;
-                    
+
                     FLVStream fs = new FLVStream();
-                    
+
                     fs.WriteFLV(rtmp, delegate()
                     {
                         // we must set a content length for the File Source filter, otherwise it thinks we have no content
                         // but don't set a length if it is our user agent, so a download will always be complete
                         if (request.Get("User-Agent") != OnlineVideos.OnlineVideoSettings.Instance.UserAgent)
-                            response.ContentLength = fs.EstimatedLength;      
-                                                
+                            response.ContentLength = fs.EstimatedLength;
+
                         responseStream = response.Send();
                         return responseStream;
                     }, response._session._socket);
@@ -119,7 +93,43 @@ namespace RTMP_LIB
                     response.StatusAndReason = HybridDSP.Net.HTTP.HTTPServerResponse.HTTPStatus.HTTP_INTERNAL_SERVER_ERROR;
                     response.Send().Close();
                 }
-                Logger.Log(ex.ToString());                
+                Logger.Log(ex.ToString());
+            }
+        }
+
+        public void HandleRequest(string url, HybridDSP.Net.HTTP.HTTPServerRequest request, HybridDSP.Net.HTTP.HTTPServerResponse response)
+        {
+            Thread.CurrentThread.Name = "RTMPProxy";
+            Logger.Log("Request");
+            RTMP rtmp = null;
+            try
+            {
+                NameValueCollection paramsHash = System.Web.HttpUtility.ParseQueryString(new Uri(url).Query);
+
+                Link link = new Link();
+                if (!string.IsNullOrEmpty(paramsHash["rtmpurl"])) link = Link.FromRtmpUrl(new Uri(paramsHash["rtmpurl"]));
+                if (!string.IsNullOrEmpty(paramsHash["app"])) link.app = paramsHash["app"];
+                if (!string.IsNullOrEmpty(paramsHash["tcUrl"])) link.tcUrl = paramsHash["tcUrl"];
+                if (!string.IsNullOrEmpty(paramsHash["hostname"])) link.hostname = paramsHash["hostname"];
+                if (!string.IsNullOrEmpty(paramsHash["port"])) link.port = int.Parse(paramsHash["port"]);
+                if (!string.IsNullOrEmpty(paramsHash["playpath"])) link.playpath = paramsHash["playpath"];
+                if (!string.IsNullOrEmpty(paramsHash["subscribepath"])) link.subscribepath = paramsHash["subscribepath"];
+                if (!string.IsNullOrEmpty(paramsHash["pageurl"])) link.pageUrl = paramsHash["pageurl"];
+                if (!string.IsNullOrEmpty(paramsHash["swfurl"])) link.swfUrl = paramsHash["swfurl"];
+                if (!string.IsNullOrEmpty(paramsHash["swfsize"])) link.SWFSize = int.Parse(paramsHash["swfsize"]);
+                if (!string.IsNullOrEmpty(paramsHash["swfhash"])) link.SWFHash = Link.ArrayFromHexString(paramsHash["swfhash"]);
+                if (!string.IsNullOrEmpty(paramsHash["swfVfy"])) { link.swfUrl = paramsHash["swfVfy"]; link.swfVerify = true; }
+                if (!string.IsNullOrEmpty(paramsHash["live"])) bool.TryParse(paramsHash["live"], out link.bLiveStream);
+                if (!string.IsNullOrEmpty(paramsHash["auth"])) link.auth = paramsHash["auth"];
+                if (!string.IsNullOrEmpty(paramsHash["token"])) link.token = paramsHash["token"];
+                if (!string.IsNullOrEmpty(paramsHash["conn"])) link.extras = Link.ParseAMF(paramsHash["conn"]);
+                if (link.tcUrl != null && link.tcUrl.ToLower().StartsWith("rtmpe")) link.protocol = Protocol.RTMPE;
+
+                if (link.swfVerify) link.GetSwf();
+
+                rtmp = new RTMP() { Link = link };
+
+                ConnectAndGetStream(rtmp, request, response, ref invalidHeader);
             }
             finally
             {
