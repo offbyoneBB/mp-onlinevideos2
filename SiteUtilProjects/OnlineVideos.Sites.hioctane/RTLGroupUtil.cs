11fb0d5119588ce8c6c35caa95499413f449b80c
hioctane <hioctane@gmx.net>
2011-02-20 22:19:55 +0000

    fixed rtlnow/voxnow/superrtlnow

--- SiteUtilProjects/OnlineVideos.Sites.hioctane/RTLGroupUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.hioctane/RTLGroupUtil.cs
@@ -4,9 +4,47 @@ using System.Text.RegularExpressions;
 using System.Web;
 using System.Xml;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace OnlineVideos.Sites
 {
+
+    public class RTMPUrl
+    {
+        private List<string> entries;
+
+        public RTMPUrl()
+        {
+            entries = new List<string>();
+        }
+
+        public void Add(string key, string value)
+        {
+            if (key.StartsWith("&")) key = key.Substring(1);
+            if (key.EndsWith("=")) key = key.Substring(0, key.Length - 1);
+
+            if(entries.Count == 0)
+                entries.Add("?" + key + "=" + HttpUtility.UrlEncode(value));
+            else
+                entries.Add("&" + key + "=" + HttpUtility.UrlEncode(value));
+        }
+
+        private string generateUrl()
+        {
+            string ret = "http://127.0.0.1/stream.flv";
+            foreach (string s in entries)
+                ret = ret + s;
+            return ret;
+        }
+
+        public string getRTMPUrl()
+        {
+            if (entries.Count > 0)
+                return ReverseProxy.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance, generateUrl());
+            else return string.Empty;
+        }
+    }
+
     public class RTLGroupUtil : GenericSiteUtil
     {
         [Category("OnlineVideosConfiguration"), Description("Value used to RTMPe Verification")]
@@ -37,24 +75,24 @@ namespace OnlineVideos.Sites
                         string timetype = root.SelectSingleNode("./timetype").InnerText;
                         string fkcontent = root.SelectSingleNode("./fkcontent").InnerText;
                         string season = root.SelectSingleNode("./season").InnerText;
-                        string fmstoken = root.SelectSingleNode("./fmstoken").InnerText;
-                        string fmstoken_time = root.SelectSingleNode("./fmstoken_time").InnerText;
-                        string fmstoken_renew = root.SelectSingleNode("./fmstoken_renew").InnerText;
+                       // string fmstoken = root.SelectSingleNode("./fmstoken").InnerText;
+                       // string fmstoken_time = root.SelectSingleNode("./fmstoken_time").InnerText;
+                       // string fmstoken_renew = root.SelectSingleNode("./fmstoken_renew").InnerText;
                         string ivw = Regex.Match(url, "ivw=(?<ivw>[^&]+)&").Groups["ivw"].Value;
 
                         string rtmpeUrl = root.SelectSingleNode("./playlist/videoinfo/filename").InnerText;
 
-                        string tokenUrl = fmstoken_renew + "&token=" + fmstoken + "&ts=" + fmstoken_time;
-                        string fmsData = GetWebData(tokenUrl);
+                        //string tokenUrl = fmstoken_renew + "&token=" + fmstoken + "&ts=" + fmstoken_time;
+                        //string fmsData = GetWebData(tokenUrl);
 
-                        if (!string.IsNullOrEmpty(fmsData))
-                        {
-                            XmlDocument fmsDoc = new XmlDocument();
-                            fmsDoc.LoadXml(fmsData);
-                            XmlElement fmsRoot = fmsDoc.DocumentElement;
+                        //if (!string.IsNullOrEmpty(fmsData))
+                        //{
+                            //XmlDocument fmsDoc = new XmlDocument();
+                            //fmsDoc.LoadXml(fmsData);
+                            //XmlElement fmsRoot = fmsDoc.DocumentElement;
 
-                            string secret = fmsRoot.SelectSingleNode("./secret").InnerText;
-                            string onetime = fmsRoot.SelectSingleNode("./onetime").InnerText;
+                            //string secret = fmsRoot.SelectSingleNode("./secret").InnerText;
+                            //string onetime = fmsRoot.SelectSingleNode("./onetime").InnerText;
 
                             string host = rtmpeUrl.Substring(rtmpeUrl.IndexOf("//") + 2, rtmpeUrl.IndexOf("/", rtmpeUrl.IndexOf("//") + 2) - rtmpeUrl.IndexOf("//") - 2);
                             string tcUrl = "rtmpe://" + host + ":1935" + "/" + app;
@@ -70,14 +108,14 @@ namespace OnlineVideos.Sites
                             combinedPlaypath += "&fkcontent=" + fkcontent;
                             combinedPlaypath += "&season=" + season;
 
-                            string resultUrl = ReverseProxy.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance,
+                            /*string resultUrl = ReverseProxy.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance,
                                         string.Format("http://127.0.0.1/stream.flv?rtmpurl={0}&hostname={1}&tcUrl={2}&app={3}&swfurl={4}&swfsize={5}&swfhash={6}&pageurl={7}&playpath={8}&conn={9}&conn={10}&conn={11}&conn={12}&conn={13}&conn={14}&conn={15}",
                                                 tcUrl, //rtmpUrl
                                                 host, //host
                                                 tcUrl, //tcUrl
                                                 app, //app
                                                 baseUrl + "/includes/rtlnow_videoplayer09_2.swf", //swfurl
-                                                "800197",
+                                                "800197",   
                                                 "fbb8d89b02eb7712c308a673291e1bf4440167830d923d3c361cd282cce3a2ee",
                                                 video.VideoUrl, //pageUrl
                                                 HttpUtility.UrlEncode(combinedPlaypath), //playpath
@@ -88,8 +126,30 @@ namespace OnlineVideos.Sites
                                                 "S:",
                                                 "S:" + para1,
                                                 "S:" + playpath.Substring(0,playpath.Length-4)));
-                            return resultUrl;
-                        }
+                            return resultUrl;*/
+
+                            string resultUrl = string.Empty;
+                            RTMPUrl rtmpLink = new RTMPUrl();
+                            rtmpLink.Add("rtmpurl", tcUrl);
+                            rtmpLink.Add("hostname", host);
+                            rtmpLink.Add("tcUrl", tcUrl);
+                            rtmpLink.Add("app", app);
+                            rtmpLink.Add("swfUrl", baseUrl + "/includes/rtlnow_videoplayer09_2.swf");
+                            rtmpLink.Add("swfsize", "827801");
+                            rtmpLink.Add("swfhash", "23a4f9781244d1b6db02ffda936634a858d7b746e79c42da7b5eb9217b336843");
+                            rtmpLink.Add("pageurl", video.VideoUrl);
+                            rtmpLink.Add("playpath", combinedPlaypath);
+                            rtmpLink.Add("conn", "S:" + para2);
+                            rtmpLink.Add("conn", "Z:");
+                            rtmpLink.Add("conn", "Z:");
+                            //rtmpLink.Add("conn", "S:" + secret);
+                            //rtmpLink.Add("conn", "S:" + onetime);
+                            rtmpLink.Add("conn", "S:");
+                            rtmpLink.Add("conn", "S:");
+                            rtmpLink.Add("conn", "S:" + para1);
+                            rtmpLink.Add("conn", "S:" + playpath.Substring(0, playpath.Length - 4));
+                            return rtmpLink.getRTMPUrl();
+                        //}
                     }
                 }
             }
