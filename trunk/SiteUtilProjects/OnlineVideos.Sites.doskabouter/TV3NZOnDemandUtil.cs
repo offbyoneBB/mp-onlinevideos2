e318df3619ee0ac51480de92642f96fc3673af6a
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2011-02-20 21:43:21 +0000

    added TV3NZ

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/TV3NZOnDemandUtil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace OnlineVideos.Sites
+{
+    public class TV3NZOnDemandUtil : GenericSiteUtil
+    {
+        public override string getUrl(VideoInfo video)
+        {
+            string res = base.getUrl(video);
+            video.PlaybackOptions = new Dictionary<string, string>();
+            string[] bitRates = { "330K", "700K" };
+            foreach (string bitRate in bitRates)
+            {
+                Log.Debug("tv3 " + res + bitRate);
+
+                string url = ReverseProxy.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance,
+                string.Format("http://127.0.0.1/stream.flv?rtmpurl={0}&swfVfy={1}",
+                HttpUtility.UrlEncode(res + bitRate),
+                HttpUtility.UrlEncode(@"http://static.mediaworks.co.nz/video/3.9/videoPlayer3.9.swf")));
+                video.PlaybackOptions.Add(bitRate, url);
+                Log.Debug("tv3 add " + url);
+                //rtmpe://nzcontent.mediaworks.co.nz/tv3/_definst_/mp4:/transfer/07022011/HW031459_700K -W http://static.mediaworks.co.nz/video/3.9/videoPlayer3.9.swf
+            }
+            return video.PlaybackOptions[bitRates[0]];
+        }
+    }
+}
