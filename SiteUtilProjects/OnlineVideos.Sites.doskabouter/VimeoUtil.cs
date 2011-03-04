b92c0bb070ff3ddd61e3482aed22220fbef1f5f6
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2011-03-04 23:03:13 +0000

    added vimeo

---
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/VimeoUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace OnlineVideos.Sites
+{
+    public class VimeoUtil : GenericSiteUtil
+    {
+        private string searchToken;
+
+        public override int DiscoverDynamicCategories()
+        {
+            string webData = GetWebData(@"http://vimeo.com/");
+            Match m = Regex.Match(webData, @"class=""xsrft""\sname=""token""\svalue=""(?<token>[^""]*)""");
+            if (m.Success)
+                searchToken = m.Groups["token"].Value.Substring(0, 8);
+            cookies = "searchtoken=" + searchToken;
+            searchUrl = @"http://vimeo.com/search/videos/search:{0}/" + searchToken;
+
+            return base.DiscoverDynamicCategories();
+        }
+
+        public override bool CanSearch { get { return true; } }
+
+    }
+}
