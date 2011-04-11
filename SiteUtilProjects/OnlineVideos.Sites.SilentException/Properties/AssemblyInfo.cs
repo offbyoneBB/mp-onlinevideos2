21e7001fa0fb06d2e56516cc4db90932d2fd2b46
mitja.skuver <mitja.skuver@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2011-04-11 10:17:08 +0000

    Added my SiteUtils: - NaTabanu - NovaTV (inherited from sax)

---
+++ SiteUtilProjects/OnlineVideos.Sites.SilentException/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OnlineVideos.Sites.SilentException")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SilentException")]
+[assembly: AssemblyProduct("OnlineVideos.Sites.SilentException")]
+[assembly: AssemblyCopyright("Copyright © SilentException 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("904c4f95-3e3d-44f6-a176-94bda06d9e54")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
