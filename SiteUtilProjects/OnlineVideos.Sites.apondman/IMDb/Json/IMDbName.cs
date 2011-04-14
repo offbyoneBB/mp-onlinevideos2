f4f889c57d5f072d04b1dc179ed18ef676b3d90d
apondman <apondman@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2011-04-14 19:25:22 +0000

    previous commit was missing a class file

---
+++ SiteUtilProjects/OnlineVideos.Sites.apondman/IMDb/Json/IMDbName.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace OnlineVideos.Sites.Pondman.IMDb.Json {
+    
+    using Newtonsoft.Json;
+
+    public class IMDbName : IMDbPrincipal
+    {
+        
+        [JsonProperty("image")]
+        public IMDbImage Image {
+            get
+            {
+                if (this.image == null)
+                {
+                    this.Image = new IMDbImage();
+                }
+
+                return this.image;
+            }
+            set
+            {
+                this.image = value;
+            }
+        } private IMDbImage image;
+    }
+}
