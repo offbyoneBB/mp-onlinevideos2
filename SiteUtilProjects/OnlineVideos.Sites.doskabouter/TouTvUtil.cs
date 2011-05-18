3984ee5a5b90e9e0de1b2917f1afcb0b98c8693a
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2011-05-18 21:00:23 +0000

    fixed toutv

--- SiteUtilProjects/OnlineVideos.Sites.doskabouter/TouTvUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/TouTvUtil.cs
@@ -22,23 +22,19 @@ namespace OnlineVideos.Sites
             XmlNamespaceManager nsmRequest = new XmlNamespaceManager(doc.NameTable);
             nsmRequest.AddNamespace("a", @"http://www.w3.org/2001/SMIL20/Language");
 
-            string rtmpUrl = doc.SelectSingleNode("//a:head/a:meta", nsmRequest).Attributes["base"].Value;
-            Uri uri = new Uri(rtmpUrl);
-            NameValueCollection nn = HttpUtility.ParseQueryString(uri.Query);
-            string auth = nn["auth"];
-
             foreach (XmlNode node in doc.SelectNodes("//a:body/a:switch/a:video", nsmRequest))
             {
                 int bitrate = Int32.Parse(node.Attributes["system-bitrate"].Value);
                 string vidUrl = node.Attributes["src"].Value;
-
-                string url = ReverseProxy.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance,
-                    string.Format("http://127.0.0.1/stream.flv?rtmpurl={0}&playpath={1}&swfVfy={2}&auth={3}",
-                    HttpUtility.UrlEncode(rtmpUrl),
-                    HttpUtility.UrlEncode(vidUrl),
-                    HttpUtility.UrlEncode(@"http://static.tou.tv/lib/ThePlatform/4.2/swf/flvPlayer.swf"),
-                    auth));
-                video.PlaybackOptions.Add((bitrate / 1000).ToString() + "Kb", url);
+                if (vidUrl.StartsWith("rtmp:"))
+                {
+                    string[] pathParts = vidUrl.Split(new string[] { "<break>" }, StringSplitOptions.None);
+                    string url = ReverseProxy.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance,
+                        string.Format("http://127.0.0.1/stream.flv?rtmpurl={0}&playpath={1}",
+                        HttpUtility.UrlEncode(pathParts[0]),
+                        HttpUtility.UrlEncode(pathParts[1])));
+                    video.PlaybackOptions.Add((bitrate / 1000).ToString() + "Kb", url);
+                }
             }
 
             string resultUrl;
