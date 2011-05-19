122d3081492f40fa17cf8608d346309e0ca0182e
offbyone@gmx.de <offbyone@gmx.de@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2011-05-19 10:48:04 +0000

    fixed hoster: Vimeo (not yet published)

--- trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/Vimeo.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/Vimeo.cs
@@ -21,7 +21,7 @@ namespace OnlineVideos.Hoster
             string page = SiteUtilBase.GetWebData(url);
             if (!string.IsNullOrEmpty(page))
             {
-                Match n = Regex.Match(page, @"{""cached_timestamp"":[^,]*,""source"":""cache"",""signature"":""(?<signature>[^""]*)"",""timestamp"":(?<timestamp>[^,]*),""referrer"":null,""vimeo_url"":""vimeo\.com"",""player_url"":""player\.vimeo\.com"",""cdn_url"":""a\.vimeocdn\.com"",""cookie_domain"":""\.vimeo\.com""},""video"":{""id"":(?<id>[^,]*),""title");
+                Match n = Regex.Match(page, @"{""cached_timestamp"":[^,]*,""source"":""cache"",""signature"":""(?<signature>[^""]*)"",""timestamp"":(?<timestamp>[^,]*),""referrer"":.+?,""vimeo_url"":""vimeo\.com"",""player_url"":""player\.vimeo\.com"",""cdn_url"":""a\.vimeocdn\.com"",""cookie_domain"":""\.vimeo\.com""},""video"":{""id"":(?<id>[^,]*),""title");
                 if (n.Success)
                 {
                     string vidUrl = String.Format(@"http://player.vimeo.com/play_redirect?clip_id={0}&sig={1}&time={2}&codecs=H264,VP8,VP6",
