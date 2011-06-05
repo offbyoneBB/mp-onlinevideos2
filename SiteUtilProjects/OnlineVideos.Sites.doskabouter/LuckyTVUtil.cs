7c934e5364c76829a00c17f3c9d6a6244cc37b4d
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2011-06-05 20:33:07 +0000

    even simpler fix for LuckyTV works

--- SiteUtilProjects/OnlineVideos.Sites.doskabouter/LuckyTVUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/LuckyTVUtil.cs
@@ -9,13 +9,12 @@ using System.IO;
 
 namespace OnlineVideos.Sites
 {
-    public class LuckyTVUtil : GenericSiteUtil, IRequestHandler
+    public class LuckyTVUtil : GenericSiteUtil, ISimpleRequestHandler
     {
         public override void Initialize(OnlineVideos.SiteSettings siteSettings)
         {
             base.Initialize(siteSettings);
             ReverseProxy.AddHandler(this);
-
         }
 
         public override string getUrl(VideoInfo video)
@@ -24,41 +23,9 @@ namespace OnlineVideos.Sites
             return ReverseProxy.GetProxyUri(this, url);
         }
 
-        public bool DetectInvalidPackageHeader()
+        public void UpdateRequest(HttpWebRequest request)
         {
-            return false;
-        }
-
-        public void HandleRequest(string url, HTTPServerRequest request, HTTPServerResponse response)
-        {
-            HttpWebRequest proxyRequest = WebRequest.Create(url) as HttpWebRequest;
-
-            if (proxyRequest == null)
-            {
-                response.Status = HTTPServerResponse.HTTPStatus.HTTP_NOT_FOUND;
-                response.Send().Close();
-            }
-            else
-            {
-
-                HttpWebResponse siteResponse = proxyRequest.GetResponse() as HttpWebResponse;
-                // copy headers
-                foreach (string aKey in siteResponse.Headers.AllKeys) response.Set(aKey, siteResponse.Headers.Get(aKey));
-                // restream data
-                Stream responseStream = response.Send();
-                Stream siteResponseStream = siteResponse.GetResponseStream();
-                int read = 0;
-                while (read < siteResponse.ContentLength)
-                {
-                    byte[] data = new byte[1024];
-                    int fetched = siteResponseStream.Read(data, 0, 1024);
-                    read += fetched;
-                    responseStream.Write(data, 0, fetched);
-                    if (fetched == 0 || read >= siteResponse.ContentLength) break;
-                }
-                responseStream.Flush();
-                responseStream.Close();
-            }
+            return;
         }
     }
 }
