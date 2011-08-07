9f4d7adc1b9518fb430b37251ead497cf2ea7aa0
offbyone <offbyone@gmx.de>
2011-08-07 23:45:01 +0000

    added batch files for building from command line without Visual Studio (thx chemelli)

---
+++ Build/Build_OV_MP1_Release.bat
@@ -0,0 +1,36 @@
+@ECHO OFF
+
+REM detect if BUILD_TYPE should be release or debug
+if not %1!==Debug! goto RELEASE
+:DEBUG
+set BUILD_TYPE=Debug
+goto START
+:RELEASE
+set BUILD_TYPE=Release
+goto START
+
+
+:START
+REM Select program path based on current machine environment
+set progpath=%ProgramFiles%
+if not "%ProgramFiles(x86)%".=="". set progpath=%ProgramFiles(x86)%
+
+
+REM set logfile where the infos are written to, and clear that file
+set LOG=build_%BUILD_TYPE%.log
+echo. > %LOG%
+
+
+echo.
+echo -= OnlineVideos =-
+echo -= build mode: %BUILD_TYPE% =-
+echo.
+
+echo.
+echo Building OnlineVideos...
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /target:Rebuild /property:Configuration=%BUILD_TYPE% "..\OnlineVideos.sln" >> %LOG%
+
+echo Building MPEI
+copy "..\MPEI\OnlineVideos.xmp2" "..\MPEI\OnlineVideos_COPY.xmp2"
+"%progpath%\Team MediaPortal\MediaPortal\MpeMaker.exe" "..\MPEI\OnlineVideos_COPY.xmp2" /B >> %LOG%
+del "..\MPEI\OnlineVideos_COPY.xmp2"
