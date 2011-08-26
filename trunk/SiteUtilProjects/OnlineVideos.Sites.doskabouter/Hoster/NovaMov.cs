ecffd3ed520ea4665edab8c4b69ccbf026b287ac
paul.doskabouter@gmail.com <paul.doskabouter@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2011-08-26 21:38:39 +0000

    fixed tubeplus improved novamov

--- trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/NovaMov.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/NovaMov.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using OnlineVideos.Hoster.Base;
+﻿using OnlineVideos.Hoster.Base;
 
 namespace OnlineVideos.Hoster
 {
