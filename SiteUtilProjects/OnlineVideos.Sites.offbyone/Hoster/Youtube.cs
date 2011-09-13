8efae0216f3569f24034a4d6222c8baf59fc420b
offbyone <offbyone@gmx.de>
2011-09-13 18:55:59 +0000

    fixed: YouTube RTMP links

--- SiteUtilProjects/OnlineVideos.Sites.offbyone/Hoster/Youtube.cs
+++ SiteUtilProjects/OnlineVideos.Sites.offbyone/Hoster/Youtube.cs
@@ -68,6 +68,8 @@ namespace OnlineVideos.Hoster
 
             if (!string.IsNullOrEmpty(Items.Get("url_encoded_fmt_stream_map")))
             {
+                string swfUrl = Regex.Match(contents, "\"url\":\\s\"([^\"]+)\"").Groups[1].Value.Replace("\\/", "/");// "url": "http:\/\/s.ytimg.com\/yt\/swfbin\/watch_as3-vflOCLBVA.swf"
+
                 string[] FmtUrlMap = Items["url_encoded_fmt_stream_map"].Split(',');
                 string[] FmtList = Items["fmt_list"].Split(',');
 
@@ -89,62 +91,21 @@ namespace OnlineVideos.Hoster
                         type = type.Substring(type.LastIndexOfAny(new char[] { '/', '-' }) + 1);
                     }
                     string finalUrl = urlOptions.Get("url");
-                    if (!string.IsNullOrEmpty(finalUrl)) 
+                    if (!string.IsNullOrEmpty(finalUrl))
                         PlaybackOptions.Add(string.Format("{0} | {1} ({2})", quality.Key[1], type, quality.Key[0]), finalUrl + "&ext=." + type.Replace("webm", "mkv"));
-                }
-            }
-            else if (!string.IsNullOrEmpty(Items.Get("fmt_stream_map")))
-            {
-                string swfUrl = Regex.Match(contents, "\"url\":\\s\"([^\"]+)\"").Groups[1].Value.Replace("\\/", "/");// "url": "http:\/\/s.ytimg.com\/yt\/swfbin\/watch_as3-vflOCLBVA.swf"
-
-                string[] FmtMap = Items["fmt_stream_map"].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-                Array.Sort(FmtMap, new Comparison<string>(delegate(string a, string b)
-                {
-                    int a_i = int.Parse(a.Substring(0, a.IndexOf("|")));
-                    int b_i = int.Parse(b.Substring(0, b.IndexOf("|")));
-                    int index_a = Array.IndexOf(fmtOptionsQualitySorted, a_i);
-                    int index_b = Array.IndexOf(fmtOptionsQualitySorted, b_i);
-                    return index_b.CompareTo(index_a);
-                }));
-                PlaybackOptions = new Dictionary<string, string>();
-                foreach (string fmtValue in FmtMap)
-                {
-                    int fmtValueInt = int.Parse(fmtValue.Substring(0, fmtValue.IndexOf("|")));
-                    switch (fmtValueInt)
+                    else
                     {
-                        case 0:
-                        case 5:
-                        case 34:
-                            PlaybackOptions.Add("320x240 | (" + fmtValueInt.ToString().PadLeft(2, ' ') + ") | .flv", ReverseProxy.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance,
-                            string.Format("http://127.0.0.1/stream.flv?rtmpurl={0}&swfVfy={1}",
-                                System.Web.HttpUtility.UrlEncode(fmtValue.Substring(fmtValue.IndexOf("rtmp"))),
-                                System.Web.HttpUtility.UrlEncode(swfUrl)))); break;
-                        case 13:
-                        case 17:
-                            PlaybackOptions.Add("176x144 | (" + fmtValueInt.ToString().PadLeft(2, ' ') + ") | .mp4", ReverseProxy.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance,
-                            string.Format("http://127.0.0.1/stream.flv?rtmpurl={0}&swfVfy={1}",
-                                System.Web.HttpUtility.UrlEncode(fmtValue.Substring(fmtValue.IndexOf("rtmp"))),
-                                System.Web.HttpUtility.UrlEncode(swfUrl)))); break;
-                        case 18:
-                            PlaybackOptions.Add("480x360 | (" + fmtValueInt.ToString().PadLeft(2, ' ') + ") | .mp4", ReverseProxy.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance,
-                            string.Format("http://127.0.0.1/stream.flv?rtmpurl={0}&swfVfy={1}",
-                                System.Web.HttpUtility.UrlEncode(fmtValue.Substring(fmtValue.IndexOf("rtmp"))),
-                                System.Web.HttpUtility.UrlEncode(swfUrl)))); break;
-                        case 35:
-                            PlaybackOptions.Add("640x480 | (" + fmtValueInt.ToString().PadLeft(2, ' ') + ") | .flv", ReverseProxy.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance,
-                            string.Format("http://127.0.0.1/stream.flv?rtmpurl={0}&swfVfy={1}",
-                                System.Web.HttpUtility.UrlEncode(fmtValue.Substring(fmtValue.IndexOf("rtmp"))),
-                                System.Web.HttpUtility.UrlEncode(swfUrl)))); break;
-                        case 22:
-                            PlaybackOptions.Add("1280x720 | (" + fmtValueInt.ToString().PadLeft(2, ' ') + ") | .mp4", ReverseProxy.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance,
-                            string.Format("http://127.0.0.1/stream.flv?rtmpurl={0}&swfVfy={1}",
-                                System.Web.HttpUtility.UrlEncode(fmtValue.Substring(fmtValue.IndexOf("rtmp"))),
-                                System.Web.HttpUtility.UrlEncode(swfUrl)))); break;
-                        case 37:
-                            PlaybackOptions.Add("1920x1080 | (" + fmtValueInt.ToString().PadLeft(2, ' ') + ") | .mp4", ReverseProxy.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance,
-                            string.Format("http://127.0.0.1/stream.flv?rtmpurl={0}&swfVfy={1}",
-                                System.Web.HttpUtility.UrlEncode(fmtValue.Substring(fmtValue.IndexOf("rtmp"))),
-                                System.Web.HttpUtility.UrlEncode(swfUrl)))); break;
+                        string rtmpUrl = urlOptions.Get("conn");
+                        string rtmpPlayPath = urlOptions.Get("stream");
+                        if (!string.IsNullOrEmpty(rtmpUrl) && !string.IsNullOrEmpty(rtmpPlayPath))
+                        {
+                            PlaybackOptions.Add(string.Format("{0} | {1} ({2})", quality.Key[1], type, quality.Key[0]), 
+                                ReverseProxy.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance,
+                                    string.Format("http://127.0.0.1/stream.flv?rtmpurl={0}&playpath={1}&swfVfy={2}",
+                                        System.Web.HttpUtility.UrlEncode(rtmpUrl),
+                                        System.Web.HttpUtility.UrlEncode(rtmpPlayPath),
+                                        System.Web.HttpUtility.UrlEncode(swfUrl))));
+                        }
                     }
                 }
             }
