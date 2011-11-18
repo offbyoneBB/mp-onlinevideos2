11546000d5d52c72aa93d881dbde89666ce6afa3
offbyone@gmx.de <offbyone@gmx.de@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2011-11-18 20:36:36 +0000

    updated DasErsteMediathekUtil to use a) new RtmpUrl class b) paging in categories and c) all but playback options from GenericSiteUtil

--- trunk/SiteUtilProjects/OnlineVideos.Sites.offbyone/DasErsteMediathekUtil.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.offbyone/DasErsteMediathekUtil.cs
@@ -1,213 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Text.RegularExpressions;
-using System.Web;
 
 namespace OnlineVideos.Sites
 {    
-    public class DasErsteMediathekUtil : SiteUtilBase
+    public class DasErsteMediathekUtil : GenericSiteUtil
     {        
         public enum VideoQuality { Low, High, Max };
 
         [Category("OnlineVideosUserConfiguration"), Description("Choose your preferred quality for the videos according to bandwidth.")]
         VideoQuality videoQuality = VideoQuality.High;
 
-        [Category("OnlineVideosConfiguration")]
-        string categoriesRegEx = @"<div\sclass=""mt-reset\smt-categories"">\s*
-<ul>\s*
-(?:<li><a\shref=""(?<Url>[^""]+)""[^>]*>(?<Title>[^<]+)</a></li>\s*)+
-</ul>\s*
-</div>";
-        [Category("OnlineVideosConfiguration")]
-        string subcategoriesRegEx = @"<div\sclass=""mt-media_item"">\s*
-<div\sclass=""mt-image"">\s*
-<img\s(data-)?src=""(?<ImageUrl>[^""]+)""[^>]*>\s*
-</div>\s*
-<h3\sclass=""mt-title""><a\shref=""(?<Url>[^""]+)""\s[^>]*>(?<Title>[^<]+)</a></h3>";
-        [Category("OnlineVideosConfiguration")]
-        string extraSubCatPagesRegEx = @"<option\svalue=""(?<Url>[^""]+)"">[^<]*</option>";
-        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to parse a html page for videos.")]
-        string videoListRegEx = @"<div\sclass=""mt-media_item[^""]*"">\s*
-<div\sclass=""mt-image"">\s*
-<span\sclass=""mt-icon\smt-icon_video""></span>\s*
-<img\s(data-)?src=""(?<ImageUrl>[^""]+)""\s[^/]*/>\s*
-</div>\s*
-<h3\sclass=""mt-title""><a\shref=""(?<Url>[^""]+)""[^>]*>(?<Title>[^<]+)</a></h3>\s*
-<p[^>]*>[^<]*</p>\s*
-<p\sclass=""mt-airtime_channel""><span\sclass=""mt-airtime"">(?<Duration>[^<]+)</span>";
-        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to parse a html page for a next page link.")]
-        string nextPageRegEx = @"<a\s+href=""(?<NextPageUrl>[^""]+)""\s[^>]*>Weiter</a>";
-        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to parse a html page for a previous page link.")]
-        string prevPageRegEx = @"<a\s+href=""(?<PrevPageUrl>[^""]+)""\s[^>]*>Zurück</a>";
-        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to parse a html page for playback urls.")]
-        string videoUrlOptionsRegEx = @"mediaCollection.addMediaStream\((?<Info>[^)]+)";
-        [Category("OnlineVideosConfiguration"), Description("Format string used as Url for getting the results of a search. {0} will be replaced with the query.")]
-        string searchUrl = "http://www.ardmediathek.de/ard/servlet/content/3517006?inhalt=tv&s={0}";
-
-        Regex regEx_Categories, regEx_Subcategories, regEx_VideoList, regEx_extraSubCatPages, regEx_NextPage, regEx_PrevPage, regEx_VideoUrlOptions;
-
-        public override void Initialize(SiteSettings siteSettings)
-        {
-            base.Initialize(siteSettings);
-
-            regEx_Categories = new Regex(categoriesRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace | RegexOptions.Singleline);
-            regEx_Subcategories = new Regex(subcategoriesRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace | RegexOptions.Singleline);
-            regEx_VideoList = new Regex(videoListRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace | RegexOptions.Singleline);
-            regEx_extraSubCatPages = new Regex(extraSubCatPagesRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant);
-            regEx_NextPage = new Regex(nextPageRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace | RegexOptions.Singleline);
-            regEx_PrevPage = new Regex(prevPageRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace | RegexOptions.Singleline);
-            regEx_VideoUrlOptions = new Regex(videoUrlOptionsRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant);
-        }
-
-        public override int DiscoverDynamicCategories()
-        {
-            Settings.Categories.Clear();
-            string dataPage = GetWebData("http://www.ardmediathek.de/ard/servlet/");
-            Match m = regEx_Categories.Match(dataPage);
-            if(m.Success)
-            {
-                for (int i = 0; i < m.Groups["Title"].Captures.Count; i++)
-                {
-                    RssLink item = new RssLink() { HasSubCategories = true, SubCategories = new List<Category>() };
-                    item.Name = HttpUtility.HtmlDecode(m.Groups["Title"].Captures[i].Value);
-                    item.Url = m.Groups["Url"].Captures[i].Value;
-                    item.Url = item.Url.Substring(item.Url.IndexOf("?"));
-                    item.Url = "http://www.ardmediathek.de/ard/servlet/ajax-cache/3516706/view=switch/clipFilter=fernsehen/content=fernsehen/documentId=" + HttpUtility.ParseQueryString(item.Url)["documentId"] + "/index.html";                    
-                    Settings.Categories.Add(item);
-                }
-            }
-            Settings.DynamicCategoriesDiscovered = true;            
-            return Settings.Categories.Count;
-        }
-
-        public override int DiscoverSubCategories(Category parentCategory)
-        {
-            string dataPage = GetWebData(((RssLink)parentCategory).Url);
-
-            List<string> additionalPageUrls = new List<string>();
-            Match mPage = regEx_extraSubCatPages.Match(dataPage);
-            while (mPage.Success)
-            {
-                additionalPageUrls.Add(mPage.Groups["Url"].Value);
-                mPage = mPage.NextMatch();
-            }
-            Match m = regEx_Subcategories.Match(dataPage);
-            while (m.Success)
-            {
-                RssLink subCat = new RssLink()
-                {
-                    Name = HttpUtility.HtmlDecode(m.Groups["Title"].Value),
-                    Url = m.Groups["Url"].Value,                    
-                    Thumb = "http://www.ardmediathek.de" + m.Groups["ImageUrl"].Value,
-                    ParentCategory = parentCategory
-                };
-                subCat.Url = subCat.Url.Substring(subCat.Url.IndexOf("?"));
-                subCat.Url = "http://www.ardmediathek.de/ard/servlet/ajax-cache/3516962/view=list/documentId=" + HttpUtility.ParseQueryString(subCat.Url)["documentId"] + "/index.html";
-                
-                parentCategory.SubCategories.Add(subCat);
-                m = m.NextMatch();
-            }
-
-            if (additionalPageUrls.Count > 0)
-            {
-                System.Threading.ManualResetEvent[] threadWaitHandles = new System.Threading.ManualResetEvent[additionalPageUrls.Count];
-                for (int i = 0; i < additionalPageUrls.Count; i++)
-                {
-                    threadWaitHandles[i] = new System.Threading.ManualResetEvent(false);
-                    new System.Threading.Thread(delegate(object o)
-                        {
-                            int o_i = (int)o;
-                            string addDataPage = GetWebData("http://www.ardmediathek.de" + additionalPageUrls[o_i]);
-                            Match addM = regEx_Subcategories.Match(addDataPage);
-                            if (o_i > 0) System.Threading.WaitHandle.WaitAny(new System.Threading.ManualResetEvent[] { threadWaitHandles[o_i - 1] });
-                            while (addM.Success)
-                            {
-                                RssLink subCat = new RssLink()
-                                {
-                                    Name = HttpUtility.HtmlDecode(addM.Groups["Title"].Value),
-                                    Url = addM.Groups["Url"].Value,
-                                    Thumb = "http://www.ardmediathek.de" + addM.Groups["ImageUrl"].Value,
-                                    ParentCategory = parentCategory
-                                };
-                                subCat.Url = subCat.Url.Substring(subCat.Url.IndexOf("?"));
-                                subCat.Url = "http://www.ardmediathek.de/ard/servlet/ajax-cache/3516962/view=list/documentId=" + HttpUtility.ParseQueryString(subCat.Url)["documentId"] + "/index.html";
-
-                                parentCategory.SubCategories.Add(subCat);
-                                addM = addM.NextMatch();
-                            }
-
-                            threadWaitHandles[o_i].Set();
-                        }) { IsBackground = true }.Start(i);
-                }
-                System.Threading.WaitHandle.WaitAll(threadWaitHandles);
-            }
-
-            parentCategory.SubCategoriesDiscovered = true;
-            return parentCategory.SubCategories.Count;
-        }
-		/*
-		public override int DiscoverSubCategories(Category parentCategory)
-		{
-			string dataPage = GetWebData(((RssLink)parentCategory).Url);
-
-			List<string> additionalPageUrls = new List<string>();
-			Match mPage = regEx_extraSubCatPages.Match(dataPage);
-			while (mPage.Success)
-			{
-				additionalPageUrls.Add(mPage.Groups["Url"].Value);
-				mPage = mPage.NextMatch();
-			}
-			GetCats(parentCategory, dataPage);
-			if (additionalPageUrls.Count > 0)
-			{
-				parentCategory.SubCategories.Add(new NextPageCategory() { Other = additionalPageUrls, ParentCategory = parentCategory });
-			}
-			parentCategory.SubCategoriesDiscovered = true;
-			return parentCategory.SubCategories.Count;
-		}
-
-		public override int DiscoverNextPageCategories(NextPageCategory category)
-		{
-			int oldAmount = category.ParentCategory.SubCategories.Count;
-			List<string> additionalPageUrls = category.Other as List<string>;
-			if (additionalPageUrls.Count > 0)
-			{
-				category.ParentCategory.SubCategories.Remove(category);
-				GetCats(category.ParentCategory, GetWebData("http://www.ardmediathek.de" + additionalPageUrls[0]));
-				additionalPageUrls.RemoveAt(0);
-				if (additionalPageUrls.Count > 0) category.ParentCategory.SubCategories.Add(category);
-			}
-			return category.ParentCategory.SubCategories.Count - oldAmount;
-		}
-
-		void GetCats(Category parentCategory, string dataPage)
-		{
-			Match m = regEx_Subcategories.Match(dataPage);
-			while (m.Success)
-			{
-				RssLink subCat = new RssLink()
-				{
-					Name = HttpUtility.HtmlDecode(m.Groups["Title"].Value),
-					Url = m.Groups["Url"].Value,
-					Thumb = "http://www.ardmediathek.de" + m.Groups["ImageUrl"].Value,
-					ParentCategory = parentCategory
-				};
-				subCat.Url = subCat.Url.Substring(subCat.Url.IndexOf("?"));
-				subCat.Url = "http://www.ardmediathek.de/ard/servlet/ajax-cache/3516962/view=list/documentId=" + HttpUtility.ParseQueryString(subCat.Url)["documentId"] + "/index.html";
-
-				parentCategory.SubCategories.Add(subCat);
-				m = m.NextMatch();
-			}
-		}
-        */
         public override String getUrl(VideoInfo video)
         {
             if (video.PlaybackOptions == null || video.PlaybackOptions.Count == 0)
             {
                 string dataPage = GetWebData(video.VideoUrl);
                 video.PlaybackOptions = new Dictionary<string, string>();
-                Match match = regEx_VideoUrlOptions.Match(dataPage);
+                Match match = regEx_FileUrl.Match(dataPage);
                 List<string[]> options = new List<string[]>();
                 while (match.Success)
                 {
@@ -225,7 +37,7 @@ namespace OnlineVideos.Sites
                     int type = int.Parse(infos[0]);
                     VideoQuality quality = (VideoQuality)int.Parse(infos[1]);
                     string resultUrl = "";
-                    if (infos[infos.Length - 2].Contains("rtmp"))
+                    if (infos[infos.Length - 2].ToLower().StartsWith("rtmp"))
                     {
                         Uri uri = new Uri(infos[infos.Length - 2]);
                         if (uri.Host == "gffstream.fcod.llnwd.net")
@@ -234,13 +46,7 @@ namespace OnlineVideos.Sites
                         }
                         else
                         {
-                            resultUrl = ReverseProxy.Instance.GetProxyUri(RTMP_LIB.RTMPRequestHandler.Instance,
-                                string.Format("http://127.0.0.1/stream.flv?hostname={0}&port={1}&app={2}&tcUrl={3}&playpath={4}",
-                                    HttpUtility.UrlEncode(uri.Host),
-                                    "1935",
-                                    HttpUtility.UrlEncode(infos[infos.Length - 2].Substring(infos[infos.Length - 2].IndexOf('/', uri.Host.Length)).Trim('/')),
-                                    HttpUtility.UrlEncode(infos[infos.Length - 2]),
-                                    HttpUtility.UrlEncode(infos[infos.Length - 1].Trim(new char[] { '"', ' ' }))));
+							resultUrl = new MPUrlSourceFilter.RtmpUrl(infos[infos.Length - 2]) { PlayPath = infos[infos.Length - 1].Trim(new char[] { '"', ' ' }) }.ToString();
                         }
                         video.PlaybackOptions.Add(string.Format("{0} | rtmp:// | {1}", quality.ToString().PadRight(4, ' '), infos[infos.Length - 1].ToLower().Contains("mp4:") ? ".mp4" : ".flv"), resultUrl);
                     }
@@ -268,130 +74,26 @@ namespace OnlineVideos.Sites
 
             if (video.PlaybackOptions == null || video.PlaybackOptions.Count == 0)
             {
-                return ""; // no url to play available
+				// no url to play available
+                return "";
             }
             else if (video.PlaybackOptions.Count == 1 || videoQuality == VideoQuality.Low)
             {
                 //user wants low quality or only one playback option -> use first
-                string[] values = new string[video.PlaybackOptions.Count];
-                video.PlaybackOptions.Values.CopyTo(values, 0);
-                return values[0];
+                return video.PlaybackOptions.First().Value;
             }
             else if (videoQuality == VideoQuality.Max)
             {
                 // take highest available quality
-                string[] values = new string[video.PlaybackOptions.Count];
-                video.PlaybackOptions.Values.CopyTo(values, 0);
-                return values[values.Length - 1];
-            }
-            else // choose a high quality from options (first below Max)
-            {
-                string[] keys = new string[video.PlaybackOptions.Count];
-                video.PlaybackOptions.Keys.CopyTo(keys, 0);
-                int index = keys.Length - 1;
-                while (index > 0 && keys[index].StartsWith(VideoQuality.Max.ToString())) index--;
-                return video.PlaybackOptions[keys[index]];
-            }
-        }
-
-        protected List<VideoInfo> getVideoListForCurrentCategory()
-        {
-            List<VideoInfo> listOfLinks = new List<VideoInfo>();
-            string dataPage = GetWebData(lastQueriedCategoryUrl);
-            Match m = regEx_VideoList.Match(dataPage);
-            while (m.Success)
-            {
-                VideoInfo videoInfo = new VideoInfo();
-                videoInfo.Title = HttpUtility.HtmlDecode(m.Groups["Title"].Value);
-                videoInfo.VideoUrl = "http://www.ardmediathek.de" + m.Groups["Url"].Value;
-                string[] duration = m.Groups["Duration"].Value.Split('·');
-                videoInfo.Length = duration[1].Replace("min", "").Trim();
-                videoInfo.Airdate = duration[0].Trim();
-                videoInfo.ImageUrl = "http://www.ardmediathek.de" + m.Groups["ImageUrl"].Value;
-                listOfLinks.Add(videoInfo);
-                m = m.NextMatch();
-            }
-            
-            Match mN = regEx_NextPage.Match(dataPage);
-            lastQueriedCategoryHasNextPage = mN.Success;
-            Match mP = regEx_PrevPage.Match(dataPage);
-            lastQueriedCategoryHasPreviousPage = mP.Success;
-
-            return listOfLinks;            
-        }
-
-        string lastQueriedCategoryUrl = "";
-        uint lastQueriedCategoryPageIndex = 1;
-        public override List<VideoInfo> getVideoList(Category category)
-        {
-            lastQueriedCategoryUrl = (category as RssLink).Url;
-            lastQueriedCategoryPageIndex = 1;
-            lastQueriedCategoryHasPreviousPage = false;
-            lastQueriedCategoryHasNextPage = false;        
-            return getVideoListForCurrentCategory();
-        }
-
-        #region Next/Previous Page
-
-        bool lastQueriedCategoryHasNextPage = false;
-        public override bool HasNextPage
-        {
-            get { return lastQueriedCategoryHasNextPage; }
-        }
-
-        bool lastQueriedCategoryHasPreviousPage = false;
-        public override bool HasPreviousPage
-        {
-            get { return lastQueriedCategoryHasPreviousPage; }
-        }
-
-        public override List<VideoInfo> getNextPageVideos()
-        {
-            if (lastQueriedCategoryHasNextPage)
-            {
-                lastQueriedCategoryPageIndex++;
-                if (lastQueriedCategoryUrl.IndexOf("goto=") < 0)
-                {
-                    lastQueriedCategoryUrl = lastQueriedCategoryUrl.Insert(lastQueriedCategoryUrl.LastIndexOf("/"), "/goto=" + lastQueriedCategoryPageIndex);
-                }
-                else
-                {
-                    lastQueriedCategoryUrl = lastQueriedCategoryUrl.Replace("goto=" + (lastQueriedCategoryPageIndex - 1), "goto=" + lastQueriedCategoryPageIndex);
-                }
-                return getVideoListForCurrentCategory();
+				return video.PlaybackOptions.Last().Value;
             }
             else
             {
-                return new List<VideoInfo>();
+				// choose a high quality from options (first below Max)
+				return video.PlaybackOptions.Last(v => !v.Key.StartsWith(VideoQuality.Max.ToString())).Value;
             }
         }
 
-        public override List<VideoInfo> getPreviousPageVideos()
-        {
-            if (lastQueriedCategoryHasPreviousPage)
-            {
-                lastQueriedCategoryPageIndex--;
-                lastQueriedCategoryUrl = lastQueriedCategoryUrl.Replace("goto=" + (lastQueriedCategoryPageIndex + 1), "goto=" + lastQueriedCategoryPageIndex);
-                return getVideoListForCurrentCategory();
-            }
-            else
-            {
-                return new List<VideoInfo>();
-            }
-        }
-
-        #endregion
-
-        #region Search
-
-        public override bool CanSearch { get { return true; } }
-
-        public override List<VideoInfo> Search(string query)
-        {
-            return getVideoList(new RssLink() { Url = string.Format(searchUrl, query) });
-        }
-
-        #endregion
     }
 }
 
