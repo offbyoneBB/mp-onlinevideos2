81d22b2e402beaaa74345f74d1710fda0aa42b81
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2011-12-28 15:28:33 +0000

    Fixed escapist

--- SiteUtilProjects/OnlineVideos.Sites.doskabouter/TheEscapistUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/TheEscapistUtil.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace OnlineVideos.Sites
 {
     public class TheEscapistUtil : GenericSiteUtil
@@ -13,14 +9,6 @@ namespace OnlineVideos.Sites
             foreach (Category cat in Settings.Categories)
                 cat.HasSubCategories = (((RssLink)cat).Url.EndsWith(@"videos/galleries"));
             return Settings.Categories.Count;
-        }
-
-        public override List<VideoInfo> getVideoList(Category category)
-        {
-            string webData = GetWebData(((RssLink)category).Url);
-            int p = webData.IndexOf(@"<div class='video_box_footer'>");
-            if (p >= 0) webData = webData.Substring(0, p);
-            return Parse(null, webData);
         }
     }
 }
