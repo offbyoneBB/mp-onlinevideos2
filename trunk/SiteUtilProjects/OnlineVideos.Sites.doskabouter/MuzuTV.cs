913cef0ebb6ff569b902b4d8a7cc1300a9f5ff78
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-01-01 16:08:53 +0000

    minor cleanup

--- trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/MuzuTV.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/MuzuTV.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.Collections.Generic;
 using System.Xml;
 using System.Web;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace OnlineVideos.Sites
