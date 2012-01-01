92b434642e4a6d01f1c2d63b48cacaa4c64c7125
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-01-01 16:08:53 +0000

    minor cleanup

--- SiteUtilProjects/OnlineVideos.Sites.doskabouter/RtmpUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/RtmpUtil.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Web;
-using System.Text;
 using OnlineVideos.MPUrlSourceFilter;
 
 namespace OnlineVideos.Sites
