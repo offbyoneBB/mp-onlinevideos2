e429c62f58664489241fdd234811601bc3fbebd7
haroon.rafique <haroon.rafique@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-01-04 19:49:40 +0000

    added support for CanWest based sites: * DIY Network Canada, Food Network, HGTV, Showcase, Slice, TVtropolis

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.corporategadfly/ShowcaseUtil.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OnlineVideos.Sites
+{
+    public class ShowcaseUtil : CanWestUtilBase
+    {
+        public override string FeedPIDUrl { get { return @"http://www.showcase.ca/CommonUI/Scripts/video/cwpShowcaseVC.js"; } }
+        public override string PlayerTag { get { return @"z/Showcase Video Centre"; } }
+        public override Regex FeedPIDRegex
+        {
+            get
+            {
+                return
+                    new Regex(@"data.PID\s=\sdata\.PID\s\|\|\s""(?<feedPID>[^""]*)"";",
+                        RegexOptions.Compiled);
+            }
+        }
+    }
+}
---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.corporategadfly/SliceUtil.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OnlineVideos.Sites
+{
+    public class SliceUtil : CanWestUtilBase
+    {
+        public override string FeedPIDUrl { get { return @"http://www.slice.ca/Slice/Watch/Default.aspx?ID=v"; } }
+        public override string PlayerTag { get { return @"z/Slice Player - New Video Center"; } }
+        public override Regex FeedPIDRegex
+        {
+            get
+            {
+                return
+                    new Regex(@"var\scwpPid\s+=\s""(?<feedPID>[^""]*)"";",
+                        RegexOptions.Compiled);
+            }
+        }
+    }
+}
