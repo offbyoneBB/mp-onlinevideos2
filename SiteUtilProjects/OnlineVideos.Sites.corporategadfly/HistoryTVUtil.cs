137c057b3826d77158ac9cf204dd1b407e6c8e36
haroon.rafique <haroon.rafique@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-01-04 01:55:07 +0000

    * moved GlobalUtil to base class CanWestUtilBase * added support for History Television (by extending base class)

---
+++ SiteUtilProjects/OnlineVideos.Sites.corporategadfly/HistoryTVUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OnlineVideos.Sites
+{
+    public class HistoryTVUtil : CanWestUtilBase
+    {
+        public override string FeedPIDUrl { get { return @"http://www.history.ca/video/default.aspx"; } }
+        public override string PlayerTag { get { return @"z/History Player - Video Center"; } }
+        public override Regex FeedPIDRegex
+        {
+            get
+            {
+                return
+                    new Regex(@"PID:\s""(?<feedPID>[^""]*)"",",
+                        RegexOptions.Compiled);
+            }
+        }
+
+    }
+}
