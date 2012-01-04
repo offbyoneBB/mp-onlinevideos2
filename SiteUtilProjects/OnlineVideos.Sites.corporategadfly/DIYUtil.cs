e7aa773df3f3c023337eb756a8f35b512c0dfa95
haroon.rafique <haroon.rafique@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-01-04 19:49:40 +0000

    added support for CanWest based sites: * DIY Network Canada, Food Network, HGTV, Showcase, Slice, TVtropolis

---
+++ SiteUtilProjects/OnlineVideos.Sites.corporategadfly/DIYUtil.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OnlineVideos.Sites
+{
+    public class DIYUtil : CanWestUtilBase
+    {
+        public override string FeedPIDUrl { get { return @"http://www.diy.ca/video/"; } }
+        public override string PlayerTag { get { return @"z/DIY Network - Video Centre"; } }
+        public override Regex FeedPIDRegex
+        {
+            get
+            {
+                return
+                    new Regex(@"PID:\s""(?<feedPID>[^""]*)"",",
+                        RegexOptions.Compiled);
+            }
+        }
+    }
+}
