376f68d75c0e09caa22eb4e41d658d08e7a0e316
haroon.rafique <haroon.rafique@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-01-07 02:43:55 +0000

    fixed sections of abc.com which were delivered using a slightly different mechanism (conviva)

--- trunk/SiteUtilProjects/OnlineVideos.Sites.corporategadfly/ABCUtil.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.corporategadfly/ABCUtil.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
+using System.Xml;
 
 namespace OnlineVideos.Sites
 {
@@ -13,7 +15,7 @@ namespace OnlineVideos.Sites
             RegexOptions.Compiled);
         private Regex rssLinkRegex = new Regex(@"<link href=""(?<link>[^""]*)"" rel=""alternate"" type=""application/rss\+xml""",
             RegexOptions.Compiled);
-        private Regex episodeListRegex = new Regex(@"<item><description>(?<description>[^<]*)</description>.*?<image>(?<image>[^<]*)</image>.*?<title>(?<title>[^<]*)</title>",
+        private Regex episodeListRegex = new Regex(@"<item><description>(?<description>[^<]*)</description>.*?<image>(?<image>[^<]*)</image><link>(?<link>[^<]*)</link>.*?<title>(?<title>[^<]*)</title>",
             RegexOptions.Compiled);
         private Regex thumbnailRegex = new Regex(@"http://cdn\.video\.abc\.com/abcvideo/video_fep/thumbnails/220x124/(?<episode>.*?)_220x124\.jpg",
             RegexOptions.Compiled);
@@ -103,6 +105,7 @@ namespace OnlineVideos.Sites
                             info.Title = m.Groups["title"].Value;
                             info.ImageUrl = m.Groups["image"].Value;
                             info.Description = m.Groups["description"].Value;
+                            info.Other = m.Groups["link"].Value;
                             info.VideoUrl = info.ImageUrl;
 
                             result.Add(info);
@@ -116,6 +119,8 @@ namespace OnlineVideos.Sites
 
         public override String getUrl(VideoInfo video)
         {
+            string rtmpUrl = @"rtmp://abcondemandfs.fplive.net:1935/abcondemand";
+
             string result = video.VideoUrl;
 
             // look for thumbnail
@@ -126,7 +131,7 @@ namespace OnlineVideos.Sites
                 string episode = episodeMatch.Groups["episode"].Value;
                 Log.Debug(@"episode found: {0}", episode);
 
-                string rtmpUrl = @"rtmp://abcondemandfs.fplive.net:1935/abcondemand";
+                // convert thumbnail URL to the playPath
                 string playPath = String.Format(@"mp4:/abcvideo/video_fep/mov/{0}_768x432_700.mov", episode.ToLower());
                 Log.Debug(@"playPath: {0}", playPath);
 
@@ -135,8 +140,46 @@ namespace OnlineVideos.Sites
             }
             else
             {
-                Log.Warn(@"could not extract RTMP Url from Thumbnail Url: {0}", result);
-                result = String.Empty;
+                // start with empty result
+                result = string.Empty;
+
+                // find the conviva content
+                Log.Warn(@"Could not extract RTMP Url from Thumbnail Url: {0}. Reverting to conviva content", result);
+
+                video.PlaybackOptions = new Dictionary<string, string>();
+                // keep track of bitrates and URLs
+                Dictionary<int, string> urlsDictionary = new Dictionary<int, string>();
+
+                // find the conviva content
+                // split on /
+                string[] parts = ((string) video.Other).Split('/');
+                string convivaId = parts[parts.Length - 2];     // 2nd last part
+                string convivaUrl = String.Format(@"http://cdn.abc.go.com/vp2/ws/s/contents/2003/utils/video/mov/13/9024/{0}/432?v=06000007_3", convivaId);
+
+                string webData = GetWebData(convivaUrl);
+                XmlDocument xml = new XmlDocument();
+                xml.LoadXml(webData);
+
+                foreach (XmlNode node in xml.SelectNodes("//videos/video"))
+                {
+                    string playPath = node.Attributes["src"].Value;
+
+                    // do not bother unless src is non-empty
+                    if (string.IsNullOrEmpty(playPath)) continue;
+
+                    Log.Debug(@"Found video playPath: {0}", playPath);
+                    int bitrate = int.Parse(node.Attributes["bitrate"].Value);
+
+                    urlsDictionary.Add(bitrate, new MPUrlSourceFilter.RtmpUrl(rtmpUrl) { PlayPath = playPath }.ToString());
+                }
+
+                // sort the URLs ascending by bitrate
+                foreach (var item in urlsDictionary.OrderBy(u => u.Key))
+                {
+                    video.PlaybackOptions.Add(string.Format("{0} kbps", item.Key), item.Value);
+                    // return last URL as the default (will be the highest bitrate)
+                    result = item.Value;
+                }
             }
 
             return result;
