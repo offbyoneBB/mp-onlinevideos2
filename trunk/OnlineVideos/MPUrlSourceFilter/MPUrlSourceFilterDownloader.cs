6dab0e84c41b5bd77eb6a50eaa0bca3e7bc45172
bborgsdorf@gmail.com <bborgsdorf@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-02-07 13:32:10 +0000

    made new splitter the default filter to use in the code

--- trunk/OnlineVideos/MPUrlSourceFilter/MPUrlSourceFilterDownloader.cs
+++ trunk/OnlineVideos/MPUrlSourceFilter/MPUrlSourceFilterDownloader.cs
@@ -126,8 +126,8 @@ namespace OnlineVideos.MPUrlSourceFilter
         #endregion
 
         #region Constants
-		public const string FilterName = "MediaPortal Url Source Filter";
-        //public const string FilterName = "MediaPortal Url Source Splitter";
+		//public const string FilterName = "MediaPortal Url Source Filter";
+        public const string FilterName = "MediaPortal Url Source Splitter";
         #endregion
 
         #region Internals
@@ -135,8 +135,8 @@ namespace OnlineVideos.MPUrlSourceFilter
         /// <summary>
         /// Defines MediaPortal Url Source Filter.
         /// </summary>
-        [ComImport, Guid("87DD67C7-5D13-4CD5-819B-586FFCE8650F")]
-        //[ComImport, Guid("59ED045A-A938-4A09-A8A6-8231F5834259")]
+        //[ComImport, Guid("87DD67C7-5D13-4CD5-819B-586FFCE8650F")]
+        [ComImport, Guid("59ED045A-A938-4A09-A8A6-8231F5834259")]
         private class MPUrlSourceFilter { } ;
 
         #endregion
