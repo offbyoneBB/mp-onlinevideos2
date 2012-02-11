52f956f5cd0b0a88d38f8c0ba6a8c2e590a83187
bborgsdorf@gmail.com <bborgsdorf@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-02-11 14:14:45 +0000

    Configuration: simplified CreateEditSite form

--- trunk/OnlineVideos.MediaPortal1/Configuration/CreateEditSite.cs
+++ trunk/OnlineVideos.MediaPortal1/Configuration/CreateEditSite.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace OnlineVideos.MediaPortal1
@@ -46,210 +43,143 @@ namespace OnlineVideos.MediaPortal1
 
         private void CreateEditSite_Load(object sender, EventArgs e)
         {
-            SiteSettings site = SiteSettingsBindingSource.Current as SiteSettings;
-            BindingList<RssLink> rssLinks = new BindingList<RssLink>();
+            cbSiteUtil.SelectedItem = (SiteSettingsBindingSource.Current as SiteSettings).UtilName;
+            RebuildTreeView();
+        }
 
-            tvGroups_AfterSelect(tvGroups, new TreeViewEventArgs(null, TreeViewAction.Unknown));
+        private void RebuildTreeView(object select = null)
+        {
             tvGroups.Nodes.Clear();
-
+            SiteSettings site = SiteSettingsBindingSource.Current as SiteSettings;
             if (site.Categories != null)
             {
                 foreach (Category aCat in site.Categories)
                 {
-                    if (aCat is RssLink) rssLinks.Add(aCat as RssLink);
+                    if (aCat is RssLink)
+                    {
+                        TreeNode aRssNode = new TreeNode(aCat.Name);
+                        aRssNode.ImageIndex = 0;
+                        aRssNode.SelectedImageIndex = 0;
+                        aRssNode.Tag = aCat;
+                        tvGroups.Nodes.Add(aRssNode);
+                        if (aCat == select) tvGroups.SelectedNode = aRssNode;
+                    }
                     else if (aCat is Group)
                     {
                         TreeNode aGroupNode = new TreeNode(aCat.Name);
+                        aGroupNode.ImageIndex = 2;
+                        aGroupNode.SelectedImageIndex = 2;
                         aGroupNode.Tag = aCat;
+                        tvGroups.Nodes.Add(aGroupNode);
+                        if (aCat == select) tvGroups.SelectedNode = aGroupNode;
                         if ((aCat as Group).Channels != null)
                         {
                             foreach (Channel aChannel in (aCat as Group).Channels)
                             {
-                                TreeNode node = new TreeNode(aChannel.StreamName);
-                                node.Tag = aChannel;
-                                aGroupNode.Nodes.Add(node);
+                                TreeNode aChannelNode = new TreeNode(aChannel.StreamName);
+                                aChannelNode.ImageIndex = 1;
+                                aChannelNode.SelectedImageIndex = 1;
+                                aChannelNode.Tag = aChannel;
+                                aGroupNode.Nodes.Add(aChannelNode);
+                                if (!aGroupNode.IsExpanded) aGroupNode.Expand();
+                                if (aChannel == select) tvGroups.SelectedNode = aChannelNode;
                             }
                         }
-                        tvGroups.Nodes.Add(aGroupNode);
                     }
                 }
-                tvGroups.ExpandAll();
+                if (tvGroups.SelectedNode != null) tvGroups.SelectedNode.EnsureVisible();
             }
-
-            SiteUtil = SiteUtilFactory.CreateFromShortName(site.UtilName, site);
-            propertyGridSiteSettings.SelectedObject = SiteUtil;
-
-            if (SiteUtil == null) cbSiteUtil.SelectedIndex = -1;
-            else cbSiteUtil.SelectedItem = site.UtilName;
-
-            bindingSourceRssLink.DataSource = rssLinks;
-            RssLinkListSelectedIndexChanged(this, EventArgs.Empty);
         }
 
         private void tvGroups_AfterSelect(object sender, TreeViewEventArgs e)
         {
-            if (e.Node != null)
+            if (e.Node == null || e.Node.Tag == null)
             {
-                btnSaveChannel.Enabled = true;
-                btnDeleteChannel.Enabled = true;
-                if (e.Node.Parent == null)
-                {
-                    tbxChannelName.Enabled = true;
-                    tbxChannelName.Text = e.Node.Text;
-                    tbxChannelThumb.Enabled = true;
-                    tbxChannelThumb.Text = ((Group)e.Node.Tag).Thumb;
-                    tbxStreamName.Text = "";
-                    tbxStreamName.Enabled = false;
-                    tbxStreamUrl.Text = "";
-                    tbxStreamUrl.Enabled = false;
-                    tbxStreamThumb.Text = "";
-                    tbxStreamThumb.Enabled = false;
-                    btnAddChannel.Enabled = true;
-                }
-                else
-                {
-                    tbxChannelName.Text = "";
-                    tbxChannelName.Enabled = false;
-                    tbxChannelThumb.Text = "";
-                    tbxChannelThumb.Enabled = false;
-                    tbxStreamName.Text = e.Node.Text;
-                    tbxStreamName.Enabled = true;
-                    tbxStreamUrl.Text = ((Channel)e.Node.Tag).Url;
-                    tbxStreamUrl.Enabled = true;
-                    tbxStreamThumb.Text = ((Channel)e.Node.Tag).Thumb;
-                    tbxStreamThumb.Enabled = true;
-                    btnAddChannel.Enabled = false;
-                }
+                btnAddStream.Enabled = false;
+                btnDelete.Enabled = false;
+                tablessTabControl1.SelectedTab = tabPageEmpty;
             }
-            else
+            else if (e.Node.Tag is Group)
             {
-                tbxChannelName.Text = "";
-                tbxChannelName.Enabled = false;
-                tbxChannelThumb.Text = "";
-                tbxChannelThumb.Enabled = false;
-                tbxStreamName.Text = "";
-                tbxStreamName.Enabled = false;
-                tbxStreamUrl.Text = "";
-                tbxStreamUrl.Enabled = false;
-                tbxStreamThumb.Text = "";
-                tbxStreamThumb.Enabled = false;
-                btnSaveChannel.Enabled = false;
-                btnDeleteChannel.Enabled = false;
-                btnAddChannel.Enabled = false;
+                btnAddStream.Enabled = true;
+                btnDelete.Enabled = true;
+                groupBindingSource.DataSource = e.Node.Tag;
+                tablessTabControl1.SelectedTab = tabPageGroup;
             }
-        }
-
-        void RssLinkListSelectedIndexChanged(object sender, EventArgs e)
-        {
-            // enable/disable all TextBoxes and Buttons for RssLink if one/none is selected
-            txtRssUrl.Enabled = RssLinkList.SelectedIndex > -1;
-            txtRssName.Enabled = RssLinkList.SelectedIndex > -1;
-            txtRssThumb.Enabled = RssLinkList.SelectedIndex > -1;
-            btnDeleteRss.Enabled = RssLinkList.SelectedIndex > -1;
+            else if (e.Node.Tag is Channel)
+            {
+                btnAddStream.Enabled = false;
+                btnDelete.Enabled = true;
+                channelBindingSource.DataSource = e.Node.Tag;
+                tablessTabControl1.SelectedTab = tabPageChannel;
+            }
+            else if (e.Node.Tag is RssLink)
+            {
+                btnAddStream.Enabled = false;
+                btnDelete.Enabled = true;
+                bindingSourceRssLink.DataSource = e.Node.Tag;
+                tablessTabControl1.SelectedTab = tabPageRssLink;
+            }
         }
 
         void SelectedSiteUtilChanged(object sender, EventArgs e)
         {
             if (cbSiteUtil.SelectedIndex != -1)
             {
-                (SiteSettingsBindingSource.Current as SiteSettings).UtilName = (string)cbSiteUtil.SelectedItem;
-            }
-        }
-
-        void BtnAddRssClick(object sender, EventArgs e)
-        {
-            RssLink link = new RssLink() { Name = "new", Url = "http://" };
-            (SiteSettingsBindingSource.Current as SiteSettings).AddCategoryForSerialization(link);
-            ((CurrencyManager)BindingContext[bindingSourceRssLink]).List.Add(link);
-            RssLinkList.SelectedIndex = RssLinkList.Items.Count - 1;
-            RssLinkListSelectedIndexChanged(this, EventArgs.Empty);
-            txtRssName.Focus();
-        }
-
-        void BtnDeleteRssClick(object sender, EventArgs e)
-        {
-            if (RssLinkList.SelectedIndex > -1)
-            {
                 SiteSettings site = SiteSettingsBindingSource.Current as SiteSettings;
-                RssLink link = RssLinkList.SelectedItem as RssLink;
-                ((CurrencyManager)BindingContext[bindingSourceRssLink]).RemoveAt(RssLinkList.SelectedIndex);
-                site.Categories.Remove(link);
-            }
-        }
-
-        private void btnSaveChannel_Click(object sender, EventArgs e)
-        {
-            if (tvGroups.SelectedNode != null)
-            {
-                if (tvGroups.SelectedNode.Parent == null)
+                site.UtilName = (string)cbSiteUtil.SelectedItem;
+                try
                 {
-                    SiteSettings site = SiteSettingsBindingSource.Current as SiteSettings;
-                    Group group = tvGroups.SelectedNode.Tag as Group;
-                    site.Categories.Remove(group);
-                    group.Name = tbxChannelName.Text;
-                    group.Thumb = tbxChannelThumb.Text != "" ? tbxChannelThumb.Text : null;
-                    tvGroups.SelectedNode.Text = tbxChannelName.Text;
-                    site.Categories.Add(group);
+                    propertyGridSiteSettings.SelectedObject = SiteUtilFactory.CreateFromShortName(site.UtilName, site);
                 }
-                else
+                catch
                 {
-                    Channel channel = tvGroups.SelectedNode.Tag as Channel;
-                    channel.StreamName = tbxStreamName.Text;
-                    tvGroups.SelectedNode.Text = tbxStreamName.Text;
-                    channel.Url = tbxStreamUrl.Text;
-                    channel.Thumb = tbxStreamThumb.Text != "" ? tbxStreamThumb.Text : null;
+                    propertyGridSiteSettings.SelectedObject = null;
                 }
             }
         }
 
-        private void btnDeleteChannel_Click(object sender, EventArgs e)
+        private void btnDelete_Click(object sender, EventArgs e)
         {
-            if (tvGroups.SelectedNode != null)
+            if (tvGroups.SelectedNode == null || tvGroups.SelectedNode.Tag == null) return;
+            else if (tvGroups.SelectedNode.Tag is Group)
             {
-                if (tvGroups.SelectedNode.Parent == null)
-                {
-                    Group group = tvGroups.SelectedNode.Tag as Group;
-                    SiteSettings site = SiteSettingsBindingSource.Current as SiteSettings;
-                    site.Categories.Remove(group);
-                    tvGroups.Nodes.Remove(tvGroups.SelectedNode);
-                }
-                else
-                {
-                    Channel channel = tvGroups.SelectedNode.Tag as Channel;
-                    Group group = tvGroups.SelectedNode.Parent.Tag as Group;
-                    group.Channels.Remove(channel);
-                    tvGroups.Nodes.Remove(tvGroups.SelectedNode);
-                }
-                tvGroups.SelectedNode = null;
-                tvGroups_AfterSelect(tvGroups, new TreeViewEventArgs(null));
+                (SiteSettingsBindingSource.Current as SiteSettings).Categories.Remove(tvGroups.SelectedNode.Tag as Group);
+                RebuildTreeView();
+            }
+            else if (tvGroups.SelectedNode.Tag is Channel)
+            {
+                (tvGroups.SelectedNode.Parent.Tag as Group).Channels.Remove(tvGroups.SelectedNode.Tag as Channel);
+                RebuildTreeView();
+            }
+            else if (tvGroups.SelectedNode.Tag is RssLink)
+            {
+                (SiteSettingsBindingSource.Current as SiteSettings).Categories.Remove(tvGroups.SelectedNode.Tag as RssLink);
+                RebuildTreeView();
             }
         }
 
         private void btnAddGroup_Click(object sender, EventArgs e)
         {
-            SiteSettings site = SiteSettingsBindingSource.Current as SiteSettings;
-            Group g = new Group();
-            g.Name = "New";
-            site.AddCategoryForSerialization(g);
-            TreeNode node = new TreeNode("New");
-            node.Tag = g;
-            tvGroups.Nodes.Add(node);
-            tvGroups.SelectedNode = node;
-            tbxChannelName.Focus();
+            Group group = new Group() { Name = "New" };
+            (SiteSettingsBindingSource.Current as SiteSettings).AddCategoryForSerialization(group);
+            RebuildTreeView(group);
         }
 
-        private void btnAddChannel_Click(object sender, EventArgs e)
+        private void btnAddStream_Click(object sender, EventArgs e)
         {
             Group group = tvGroups.SelectedNode.Tag as Group;
-            Channel c = new Channel();
-            c.StreamName = "New";
+            Channel channel = new Channel() { StreamName = "New" };
             if (group.Channels == null) group.Channels = new BindingList<Channel>();
-            group.Channels.Add(c);
-            TreeNode node = new TreeNode("New");
-            node.Tag = c;
-            tvGroups.SelectedNode.Nodes.Add(node);
-            tvGroups.SelectedNode = node;
-            tbxStreamName.Focus();
+            group.Channels.Add(channel);
+            RebuildTreeView(channel);
+        }
+
+        private void btnAddRss_Click(object sender, EventArgs e)
+        {
+            RssLink link = new RssLink() { Name = "new", Url = "http://" };
+            (SiteSettingsBindingSource.Current as SiteSettings).AddCategoryForSerialization(link);
+            RebuildTreeView(link);
         }
     }
 }
