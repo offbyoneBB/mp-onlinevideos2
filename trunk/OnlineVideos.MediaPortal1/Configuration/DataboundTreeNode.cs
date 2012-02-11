4506ca962b773b986dada4a0ef6f9883631a5e55
bborgsdorf@gmail.com <bborgsdorf@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-02-11 15:09:02 +0000

    Configuration: fixed Category TreeView not updating Node Text when Name is changed in TextBox

---
+++ trunk/OnlineVideos.MediaPortal1/Configuration/DataboundTreeNode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace OnlineVideos.MediaPortal1
+{
+    public class DataboundTreeNode : TreeNode
+    {
+        public DataboundTreeNode(string text) : base(text) { }
+
+        object tag;
+        public new object Tag
+        {
+            get
+            {
+                return tag;
+            }
+            set
+            {
+                if (tag != null && tag is INotifyPropertyChanged) (tag as INotifyPropertyChanged).PropertyChanged -= TagPropertyChanged;
+                tag = value;
+                if (tag != null && tag is INotifyPropertyChanged) (tag as INotifyPropertyChanged).PropertyChanged += TagPropertyChanged;
+            }
+        }
+
+        public string TagPropertyBoundToText { get; set; }
+
+        void TagPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (TreeView == null) (sender as INotifyPropertyChanged).PropertyChanged -= TagPropertyChanged;
+            else
+            {
+                if (e.PropertyName == TagPropertyBoundToText)
+                {
+                    Text = sender.GetType().GetProperty(TagPropertyBoundToText).GetValue(sender, null) as string;
+                    TreeView.Refresh();
+                }
+            }
+        }
+    }
+}
