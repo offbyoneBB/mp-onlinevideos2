d3e7275674bc2f1db66201d11ddfd54c516489fd
bborgsdorf <bborgsdorf@gmail.com>
2012-02-12 13:00:40 +0000

    Configuration: added sorting on header click in dialog Import from Global List Configuration: fixed Site Icon and Banner downloaded to (and published from) default directory, not the configured one

--- OnlineVideos.MediaPortal1/Configuration/ImportGlobalSite.cs
+++ OnlineVideos.MediaPortal1/Configuration/ImportGlobalSite.cs
@@ -14,5 +14,64 @@ namespace OnlineVideos.MediaPortal1
         {
             InitializeComponent();
         }
+
+        private void dgvSites_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            string strColumnName = dgvSites.Columns[e.ColumnIndex].DataPropertyName;
+            SortOrder strSortOrder = getSortOrder(e.ColumnIndex);
+            List<OnlineVideosWebservice.Site> sites = dgvSites.DataSource as List<OnlineVideosWebservice.Site>;
+            sites.Sort(new SiteComparer(strColumnName, strSortOrder));
+            dgvSites.DataSource = null;
+            dgvSites.DataSource = sites;
+            dgvSites.Columns[e.ColumnIndex].HeaderCell.SortGlyphDirection = strSortOrder;
+        }
+
+        private SortOrder getSortOrder(int columnIndex)
+        {
+            if (dgvSites.Columns[columnIndex].HeaderCell.SortGlyphDirection == SortOrder.None ||
+                dgvSites.Columns[columnIndex].HeaderCell.SortGlyphDirection == SortOrder.Descending)
+            {
+                dgvSites.Columns[columnIndex].HeaderCell.SortGlyphDirection = SortOrder.Ascending;
+                return SortOrder.Ascending;
+            }
+            else
+            {
+                dgvSites.Columns[columnIndex].HeaderCell.SortGlyphDirection = SortOrder.Descending;
+                return SortOrder.Descending;
+            }
+        }
+
+        class SiteComparer : IComparer<OnlineVideosWebservice.Site>
+        {
+            string memberName = string.Empty; // specifies the member name to be sorted
+            SortOrder sortOrder = SortOrder.None; // Specifies the SortOrder.
+            Type siteType = typeof(OnlineVideosWebservice.Site);
+            System.Reflection.PropertyInfo propInfo = null;
+
+            /// <summary>
+            /// constructor to set the sort column and sort order.
+            /// </summary>
+            /// <param name="strMemberName"></param>
+            /// <param name="sortingOrder"></param>
+            public SiteComparer(string strMemberName, SortOrder sortingOrder)
+            {
+                memberName = strMemberName;
+                propInfo = siteType.GetProperty(memberName);
+                sortOrder = sortingOrder;
+            }
+
+            public int Compare(OnlineVideosWebservice.Site site1, OnlineVideosWebservice.Site site2)
+            {
+                if (sortOrder == SortOrder.Ascending)
+                    return propInfo.GetValue(site1, null).ToString().CompareTo(propInfo.GetValue(site2, null).ToString());
+                else
+                    return propInfo.GetValue(site2, null).ToString().CompareTo(propInfo.GetValue(site1, null).ToString());
+            }
+        }
+
+        private void dgvSites_SelectionChanged(object sender, EventArgs e)
+        {
+            btnOK.Enabled = dgvSites.SelectedRows.Count > 0;
+        }
     }
 }
