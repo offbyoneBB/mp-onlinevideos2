5f0522e05a25defe5f46cb64c375416e18d1ad7a
bborgsdorf <bborgsdorf@gmail.com>
2012-03-11 18:45:35 +0000

    fixed Sat1 missing some shows and videos

--- SiteUtilProjects/OnlineVideos.Sites.hioctane/Sat1Util.cs
+++ SiteUtilProjects/OnlineVideos.Sites.hioctane/Sat1Util.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text.RegularExpressions;
@@ -10,8 +11,10 @@ namespace OnlineVideos.Sites
     {
         [Category("OnlineVideosConfiguration"), Description("Url used for category generation.")]
         protected string baseUrl;
-        [Category("OnlineVideosConfiguration"), Description("Url used to parse Category Content from Page")]
+        [Category("OnlineVideosConfiguration"), Description("RegEx used to parse Categories from baseUrl")]
 		protected string dynamicCategoriesRegEx;
+        [Category("OnlineVideosConfiguration"), Description("Second RegEx used to parse Categories from baseUrl")]
+        protected string dynamicCategoriesRegEx2;
 		[Category("OnlineVideosConfiguration"), Description("Regular Expression used to parse a html page for videos. Group names: 'VideoUrl', 'ImageUrl', 'Title', 'Duration', 'Description', 'Airdate'.")]
 		protected string videoListRegEx;
         [Category("OnlineVideosConfiguration"), Description("Url to rtmp Server")]
@@ -26,28 +29,52 @@ namespace OnlineVideos.Sites
 
 		public override int DiscoverDynamicCategories()
 		{
+            if (Settings.Categories == null) Settings.Categories = new BindingList<Category>();
 			string page = GetWebData(baseUrl);
 			Match m = Regex.Match(page, dynamicCategoriesRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture);
-			Settings.Categories.Clear();
+			List<RssLink> cats = new List<RssLink>();
 			while (m.Success)
 			{
-				RssLink cat = new RssLink();
-				cat.Url = m.Groups["url"].Value;
-				if (!Uri.IsWellFormedUriString(cat.Url, System.UriKind.Absolute)) cat.Url = new Uri(new Uri(baseUrl), cat.Url).AbsoluteUri;
-				cat.Thumb = m.Groups["thumb"].Value;
-				if (!String.IsNullOrEmpty(cat.Thumb) && !Uri.IsWellFormedUriString(cat.Thumb, System.UriKind.Absolute)) cat.Thumb = new Uri(new Uri(baseUrl), cat.Thumb).AbsoluteUri;
-				cat.Description = Utils.PlainTextFromHtml(m.Groups["description"].Value).Replace('\n', ' ');
-				if (m.Groups["title"].Success && !string.IsNullOrEmpty(m.Groups["title"].Value)) 
-					cat.Name = HttpUtility.HtmlDecode(m.Groups["title"].Value);
-				else
-					cat.Name = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(cat.Url.Substring(cat.Url.LastIndexOf('/')+1).Replace('-', ' '));
-				if (!Regex.Match(cat.Url, "/videos?$").Success) cat.Url += "/video";
-				Settings.Categories.Add(cat);
+                cats.Add(CategoryFromMatch(m));
 				m = m.NextMatch();
 			}
-			Settings.DynamicCategoriesDiscovered = true;
+            if (!string.IsNullOrEmpty(dynamicCategoriesRegEx2))
+            {
+                m = Regex.Match(page, dynamicCategoriesRegEx2, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture);
+                while (m.Success)
+                {
+                    var c2 = CategoryFromMatch(m);
+                    var c1 = cats.FirstOrDefault(cat => ((RssLink)cat).Url == c2.Url);
+                    if (c1 == null) cats.Add(c2);
+                    else c1.Name = c2.Name;
+                    m = m.NextMatch();
+                }
+                cats.Sort((c1, c2) => { return c1.Name.CompareTo(c2.Name); });
+            }
+            if (cats.Count > 0)
+            {
+                Settings.Categories.Clear();
+                cats.ForEach(c => Settings.Categories.Add(c));
+                Settings.DynamicCategoriesDiscovered = true;
+            }
 			return Settings.Categories.Count;
 		}
+
+        private RssLink CategoryFromMatch(Match m)
+        {
+            RssLink cat = new RssLink();
+            cat.Url = m.Groups["url"].Value;
+            if (!Uri.IsWellFormedUriString(cat.Url, System.UriKind.Absolute)) cat.Url = new Uri(new Uri(baseUrl), cat.Url).AbsoluteUri;
+            cat.Thumb = m.Groups["thumb"].Value;
+            if (!String.IsNullOrEmpty(cat.Thumb) && !Uri.IsWellFormedUriString(cat.Thumb, System.UriKind.Absolute)) cat.Thumb = new Uri(new Uri(baseUrl), cat.Thumb).AbsoluteUri;
+            cat.Description = Utils.PlainTextFromHtml(m.Groups["description"].Value).Replace('\n', ' ');
+            if (m.Groups["title"].Success && !string.IsNullOrEmpty(m.Groups["title"].Value))
+                cat.Name = HttpUtility.HtmlDecode(m.Groups["title"].Value);
+            else
+                cat.Name = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(cat.Url.Substring(cat.Url.LastIndexOf('/') + 1).Replace('-', ' '));
+            if (!Regex.Match(cat.Url, "/videos?$").Success) cat.Url += "/video";
+            return cat;
+        }
 
 		public override List<VideoInfo> getVideoList(Category category)
 		{
@@ -81,8 +108,8 @@ namespace OnlineVideos.Sites
             {
                 url = Regex.Match(webData, @"flashdrm_url"":""(?<Value>[^""]+)""").Groups["Value"].Value;
                 url = url.Replace("\\/", "/");
-                url = url.Replace("rtmpte", "rtmpe");
-                url = url.Replace(".net", ".net:1935");
+                //url = url.Replace("rtmpte", "rtmpe");
+                //url = url.Replace(".net", ".net:1935");
 				url = new MPUrlSourceFilter.RtmpUrl(url) { SwfUrl = "http://www.sat1.de/imperia/moveplayer/HybridPlayer.swf", SwfVerify = true }.ToString();
             }
             else
