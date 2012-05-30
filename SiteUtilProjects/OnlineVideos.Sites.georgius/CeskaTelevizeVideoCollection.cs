1de8fd5bf3a9baf008a4867b274cebeffcbf2064
juraj.horvath <juraj.horvath@inmail.sk>
2012-05-30 19:59:41 +0000

    enhanced CeskaTelevize (user can choose video quality on each video part)

---
+++ SiteUtilProjects/OnlineVideos.Sites.georgius/CeskaTelevizeVideoCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace OnlineVideos.Sites.georgius
+{
+    public class CeskaTelevizeVideoCollection : KeyedCollection<String, CeskaTelevizeVideo>
+    {
+        protected override string GetKeyForItem(CeskaTelevizeVideo item)
+        {
+            return item.Url;
+        }
+    }
+}
