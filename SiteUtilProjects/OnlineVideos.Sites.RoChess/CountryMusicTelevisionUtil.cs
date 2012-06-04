82a21b5f55e8aa88dd7006c3e0fb03ab83aea1ec
bborgsdorf <bborgsdorf@gmail.com>
2012-06-04 21:46:27 +0000

    added new SiteUtilProject for RoChess (starting with CountryMusicTelevisionUtil)

---
+++ SiteUtilProjects/OnlineVideos.Sites.RoChess/CountryMusicTelevisionUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace OnlineVideos.Sites.RoChess
+{
+	public class CountryMusicTelevisionUtil : GenericSiteUtil
+	{
+		[Category("OnlineVideosConfiguration"), Description("regex used to parse the playlist file url from the config file of the flash player")]
+		string configFileRegEx;
+
+		public override Dictionary<string, string> GetPlaybackOptions(string playlistUrl)
+		{
+			string resolvedUrl = GetRedirectedUrl(playlistUrl);
+			string page = HttpUtility.ParseQueryString(new Uri(resolvedUrl).Query)["CONFIG_URL"];
+			string config = GetWebData(page, GetCookie(), forceUTF8: forceUTF8Encoding, allowUnsafeHeader: allowUnsafeHeaders, encoding: encodingOverride);
+			playlistUrl = Regex.Match(config, configFileRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture).Groups["url"].Value;
+			return base.GetPlaybackOptions(HttpUtility.HtmlDecode(playlistUrl));
+		}
+	}
+}
