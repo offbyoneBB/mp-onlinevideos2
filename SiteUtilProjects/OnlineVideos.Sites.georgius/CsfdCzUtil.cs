0e2a4c06aedbf36e0f136f9e1bb2ad0f6a99af44
juraj.horvath <juraj.horvath@inmail.sk>
2012-06-25 14:51:42 +0000

    fixed CSFD

--- SiteUtilProjects/OnlineVideos.Sites.georgius/CsfdCzUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.georgius/CsfdCzUtil.cs
@@ -38,7 +38,7 @@ namespace OnlineVideos.Sites.georgius
 
         private static String showEpisodeUrlAndTitleRegex = @"<option value=""(?<showUrl>[^""]+)"">(?<showTitle>[^<]+)";
 
-        private static String videoUrlBlockStart = @"if (!readCookie('videoPlayer_' + ""videoPlayer1""))";
+        private static String videoUrlBlockStart = @"Vyber video:";
         private static String videoUrlBlockEnd = @"$(clip)";
         private static String videoUrlRegex = @"player.addClip\(""(?<videoUrl>[^""]+)";
         private static String subtitleUrlRegex = @"""subtitles"":""(?<subtitleUrl>[^""]+)";
