a3ec079077d0a87805718287faa3b0932f5c68de
brownard@live.co.uk <brownard@live.co.uk@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-07-21 17:41:56 +0000

    Add missing file

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.brownard/Utils/SubCatHolder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineVideos.Sites.Utils
+{
+    class SubCatHolder
+    {
+        public List<Category> SubCategories { get; set; }
+        public Dictionary<string, string> SearchableCategories { get; set; }
+        public object Other { get; set; }
+    }
+}
