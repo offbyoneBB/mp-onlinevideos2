0145e92ae42f2a33030c2afa272c478b4e015dd5
juraj.horvath <juraj.horvath@inmail.sk>
2012-08-07 21:33:04 +0000

    added library for MSHS (Microsoft HTTP streaming) added parser for MSHS fixed F4M parser

--- MPUrlSourceSplitter/MPUrlSourceSplitter/MPUrlSourceSplitter_libmshsmanifest/MSHS_Elements.h
+++ MPUrlSourceSplitter/MPUrlSourceSplitter/MPUrlSourceSplitter_libmshsmanifest/MSHS_Elements.h
@@ -18,23 +18,13 @@
     along with MediaPortal 2.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// dllmain.cpp : Defines the entry point for the DLL application.
+#pragma once
 
-#include "stdafx.h"
+#ifndef __MSHS_ELEMENTS_DEFINED
+#define __MSHS_ELEMENTS_DEFINED
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
-{
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH:
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
-	}
-	return TRUE;
-}
+// The root element in the document is <SmoothStreamingMedia>.
+#define MSHS_ELEMENT_MANIFEST                                                 "SmoothStreamingMedia"
 
+
+#endif
--- MPUrlSourceSplitter/MPUrlSourceSplitter/MPUrlSourceSplitter_libmshsmanifest/targetver.h
+++ MPUrlSourceSplitter/MPUrlSourceSplitter/MPUrlSourceSplitter_libmshsmanifest/targetver.h
@@ -18,23 +18,11 @@
     along with MediaPortal 2.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// dllmain.cpp : Defines the entry point for the DLL application.
+#pragma once
 
-#include "stdafx.h"
+// Including SDKDDKVer.h defines the highest available Windows platform.
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
-{
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH:
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
-	}
-	return TRUE;
-}
+// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
 
+#include <SDKDDKVer.h>
