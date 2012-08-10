a447daf2ad5cfa8e92e4b37c5a104ceff702d61b
haroon.rafique <haroon.rafique@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-08-10 00:02:40 +0000

    less is more - condensed a bunch of useless utils into a single utility with configuration. * CTVUtilBase got refactored to CTVUtil to act as the base for all these deleted sites (via configuration) * deleted CTV Local News and CTV News as they are not working

--- SiteUtilProjects/OnlineVideos.Sites.corporategadfly/FashionUtil.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace OnlineVideos.Sites
-{
-    public class FashionUtil : CTVUtilBase
-    {
-        public override string BaseUrl { get { return @"http://watch.fashiontelevision.com"; } }
-        public override string Swf { get { return @"http://watch.fashiontelevision.com/Flash/player.swf?themeURL=http://watch.fashiontelevision.com/themes/FashionTelevision/player/theme.aspx"; } }
-    }
-}
--- SiteUtilProjects/OnlineVideos.Sites.corporategadfly/SpaceCastUtil.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace OnlineVideos.Sites
-{
-    public class SpaceCastUtil : CTVUtilBase
-    {
-        public override string BaseUrl { get { return @"http://watch.spacecast.com"; } }
-        public override string Swf { get { return @"http://watch.spacecast.com/Flash/player.swf?themeURL=http://watch.spacecast.com/themes/Space/player/theme.aspx"; } }
-    }
-}
