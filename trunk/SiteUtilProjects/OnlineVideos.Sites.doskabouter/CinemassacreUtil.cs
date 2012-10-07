540da95d8a3bf6e90764b1c53999b13c6b64122b
paul.doskabouter@gmail.com <paul.doskabouter@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-10-07 17:02:35 +0000

    improved Cinemassacre

--- trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/CinemassacreUtil.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/CinemassacreUtil.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Xml;
+using System.Web;
 
 namespace OnlineVideos.Sites
 {
@@ -126,7 +127,36 @@ namespace OnlineVideos.Sites
 
             if (thisUrl.StartsWith("http://www.gametrailers.com"))
             {
-                //http://www.gametrailers.com/video/angry-video-screwattack/60232
+                string webData = GetWebData(thisUrl);
+                Match m = Regex.Match(webData, @"<meta\sproperty=""og:video""\scontent=""http://media.mtvnservices.com/fb/(?<id>[^""]*)""");
+                if (m.Success)
+                {
+                    webData = GetWebData(String.Format(@"http://www.gametrailers.com/feeds/mediagen?uri={0}",
+                        HttpUtility.UrlEncode(m.Groups["id"].Value.Replace(".swf", ""))));
+                    //http://www.gametrailers.com/feeds/mediagen/?uri=mgid%3Aarc%3Avideo%3Agametrailers.com%3Adb2b0e67-9546-421b-b57d-227c7f23c8ac
+
+                    XmlDocument doc2 = new XmlDocument();
+                    doc2.LoadXml(webData);
+                    video.PlaybackOptions = new Dictionary<string, string>();
+                    foreach (XmlNode rendition in doc2.SelectNodes("//rendition"))
+                    {
+                        video.PlaybackOptions.Add(rendition.Attributes["bitrate"].Value,
+                            rendition.SelectSingleNode("src").InnerText);
+                    }
+                    string resultUrl;
+                    if (video.PlaybackOptions.Count == 0) return String.Empty;
+                    else
+                    {
+                        var enumer = video.PlaybackOptions.GetEnumerator();
+                        enumer.MoveNext();
+                        resultUrl = enumer.Current.Value;
+                    }
+                    if (video.PlaybackOptions.Count == 1) video.PlaybackOptions = null;
+
+                    return resultUrl;
+                }
+
+                //old gametrailers:
                 int i = thisUrl.LastIndexOf('/');
                 data = thisUrl.Substring(i + 1);
                 string url = String.Format(@"http://www.gametrailers.com/neo/?page=xml.mediaplayer.Mediagen&movieId={0}", data);
@@ -134,7 +164,7 @@ namespace OnlineVideos.Sites
 
                 XmlDocument doc = new XmlDocument();
                 doc.LoadXml(data3);
-                return url = doc.SelectSingleNode("//rendition/src").InnerText;
+                return doc.SelectSingleNode("//rendition/src").InnerText;
             }
 
             if (thisUrl.StartsWith("http://www.spike.com"))
@@ -165,6 +195,14 @@ namespace OnlineVideos.Sites
                 nsmgr.AddNamespace("a", "http://search.yahoo.com/mrss/");
                 XmlNode node = doc.SelectSingleNode("rss/channel/item/a:content", nsmgr);
                 return node.Attributes["url"].Value;
+            }
+            if (thisUrl.IndexOf("bit.ly/") >= 0)
+            {
+                string newUrl = GetRedirectedUrl(thisUrl);
+                string webData = GetWebData(newUrl);
+                Match m = Regex.Match(webData, @"<meta\sproperty=""og:video""\scontent=""(?<url>[^""]*)""");
+                if (m.Success)
+                    return m.Groups["url"].Value;
             }
             return null;
         }
