5d1c9d49f3d18efcf13f0393fc82287eb199dd7b
paul.doskabouter@gmail.com <paul.doskabouter@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-10-28 16:32:21 +0000

    fixed southparkworld

--- trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/SouthParkUtil.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/SouthParkUtil.cs
@@ -139,8 +139,8 @@ namespace OnlineVideos.Sites
                 string url = list[i].InnerText;
 
                 string swfUrl = null;
-                if (spc == SouthParkCountry.Nl)
-                    url = url.Replace(@"viacomccstrmfs.fplive.net/viacomccstrm", @"cp10740.edgefcs.net/ondemand/mtvnorigin");
+                if (spc == SouthParkCountry.World)
+                    url = url.Replace(@"viacomspstrmfs.fplive.net/viacomspstrm", @"cp10740.edgefcs.net/ondemand/mtvnorigin");
                 /*switch (spc)
                 {
                     case SouthParkCountry.World:
