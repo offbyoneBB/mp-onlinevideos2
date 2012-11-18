b4fe9d9d6d84cec79f9f5fe868b9ffde9b1e7b71
paul.doskabouter@gmail.com <paul.doskabouter@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-11-18 15:59:57 +0000

    fixed WatchSeries search

--- trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/WatchSeriesUtil.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/WatchSeriesUtil.cs
@@ -301,7 +301,7 @@ namespace OnlineVideos.Sites
         {
             List<ISearchResultItem> cats = new List<ISearchResultItem>();
 
-            Regex r = new Regex(@"<tr><td\svalign=""top"">\s*<a\shref=""(?<url>[^""]*)""[^>]*>\s<img\ssrc=""(?<thumb>[^""]*)""[^>]*></a>\s*</td>\s*<td\svalign=""top"">\s*<a[^>]*><b>(?<title>[^<]*)</b></a>\s*<br\s/>\s*<b>Description:</b>\s*(?<description>[^<]*)<br\s/>",
+            Regex r = new Regex(@"<tr><td\svalign=""top"">\s*<a\shref=""(?<url>[^""]*)""[^>]*>\s<img\ssrc=""(?<thumb>[^""]*)""[^>]*></a>\s*</td>\s*<td\svalign=""top"">\s*<a[^>]*><b>(?<title>[^<]*)</b></a>\s*<br\s/>\s*<b>Description:</b>\s*(?<description>[^<]*)</td>",
                 RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture);
 
             string webData = GetWebData(baseUrl + "/search/" + query);
