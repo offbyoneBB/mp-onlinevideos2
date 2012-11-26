5ed083b7b69f859e3eb352ba56facffbd45383d3
josumoreno@gmail.com <josumoreno@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2012-11-26 21:09:02 +0000

    forgotten files for uploading of mitele

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.Dj_Moren/Utils/Flowplayer/Commercial/V3_1_5_17_002/Base64.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Flowplayer.Commercial.V3_1_5_17_002
+{
+    internal class Base64
+    {
+        public static String Decode(String content, Boolean force)
+        {
+            StringBuilder builder = new StringBuilder();
+            content = force ? Utf8.Decode(content) : content;
+            int i = 0;
+            List<String> strings = new List<String>(content.Length);
+
+            while (i < content.Length)
+            {
+                uint a = (uint)Base64.Code.IndexOf(content[i]);
+                uint b = (uint)Base64.Code.IndexOf(content[i + 1]);
+                uint c = (uint)Base64.Code.IndexOf(content[i + 2]);
+                uint d = (uint)Base64.Code.IndexOf(content[i + 3]);
+
+                uint e = ((a << 18) | (b << 12) | (c << 6) | d);
+
+                uint f = ((e >> 16) & 0xFF);
+                uint g = ((e >> 8) & 0xFF);
+                uint h = (e & 0xFF);
+
+                strings.Add(new String(new Char[] { (Char)f, (Char)g, (Char)h }));
+
+                if (d == 64)
+                {
+                    strings[i / 4] = new String(new Char[] { (Char)f, (Char)g });
+                }
+
+                if (c == 64)
+                {
+                    strings[i / 4] = new String(new Char[] { (Char)f });
+                }
+
+                i += 4;
+            }
+
+            foreach (var str in strings)
+            {
+                builder.Append(str);
+            }
+
+            return (force) ? Utf8.Decode(builder.ToString()) : builder.ToString();
+        }
+
+        public static String Encode(String content, Boolean force)
+        {
+            StringBuilder builder = new StringBuilder();
+            StringBuilder temp = new StringBuilder();
+            StringBuilder temp2 = new StringBuilder();
+            content = (force) ? Utf8.Encode(content) : content;
+            
+            int i = content.Length % 3;
+
+            if (i > 0)
+            {
+                while (i++ < 3)
+                {
+                    content += "0";
+                    temp.Append("=");
+                }
+            }
+            i = 0;
+            while (i < content.Length)
+            {
+                uint a = content[i];
+                uint b = content[i + 1];
+                uint c = content[i + 2];
+
+                uint d = ((a << 16) | (b << 8) | c);
+
+                uint e = (d >> 18) & 0x3F;
+                uint f = (d >> 12) & 0x3F;
+                uint g = (d >> 6) & 0x3F;
+                uint h = d & 0x3F;
+
+                temp2.Append(new String(new Char[] { Base64.Code[(int)e], Base64.Code[(int)f], Base64.Code[(int)g], Base64.Code[(int)h] }));
+
+                i += 3;
+            }
+
+            builder.Append(temp2.ToString().Substring(0, temp2.Length - temp.Length));
+            builder.Append(temp);
+
+            return builder.ToString();
+        }
+
+        public const String Code = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    }
+}
---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.Dj_Moren/Utils/Flowplayer/Commercial/V3_1_5_17_002/Utf8.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Flowplayer.Commercial.V3_1_5_17_002
+{
+    internal class Utf8
+    {
+        public static String Decode(String content)
+        {
+            StringBuilder builder = new StringBuilder();
+            int i = 0;
+
+            while (i < content.Length)
+            {
+                uint characterCode = content[i];
+
+                if (characterCode < 128)
+                {
+                    builder.Append((Char)characterCode);
+                    ++i;
+                }
+                else if ((characterCode > 191) && (characterCode < 224))
+                {
+                    uint characterCode2 = content[i + 1];
+                    builder.Append((Char)(((characterCode & 0x1F) << 6) | (characterCode2 & 0x3F)));
+                    i += 2;
+                }
+                else
+                {
+                    uint characterCode2 = content[i + 1];
+                    uint characterCode3 = content[i + 2];
+                    builder.Append((Char)(((characterCode & 0x0F) << 12) | ((characterCode2 & 0x3F) << 6) | (characterCode3 & 0x3F)));
+                    i += 3;
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        public static String Encode(String content)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                uint characterCode = content[i];
+
+                if (characterCode < 128)
+                {
+                    builder.Append((Char)characterCode);
+                }
+                else if ((characterCode > 127) && (characterCode < 2048))
+                {
+                    builder.Append((Char)((characterCode >> 6) | 0xC0));
+                    builder.Append((Char)((characterCode & 0x3F) | 0x80));
+                }
+                else
+                {
+                    builder.Append((Char)((characterCode >> 12) | 0xE0));
+                    builder.Append((Char)(((characterCode >> 6) & 0x3F) | 0x80));
+                    builder.Append((Char)((characterCode & 0x3F) | 0x80));
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
