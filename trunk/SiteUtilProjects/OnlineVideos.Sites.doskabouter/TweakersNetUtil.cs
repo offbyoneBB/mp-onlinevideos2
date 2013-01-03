e75b59edc18fb7f8dd5f21981d9a7b3874fa2fc5
paul.doskabouter@gmail.com <paul.doskabouter@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-01-03 18:51:46 +0000

    Fixed Tweakers.net

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/TweakersNetUtil.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Web;
+using System.IO;
+
+namespace OnlineVideos.Sites
+{
+    public class TweakersNetUtil : GenericSiteUtil
+    {
+        CookieContainer cc;
+
+        public override int DiscoverDynamicCategories()
+        {
+            cc = new CookieContainer();
+            string data = GetWebData(baseUrl, cc);
+            Match m = Regex.Match(data, @"<input\stype=""hidden""\sname=""tweakers_token""\svalue=""(?<tweakerstoken>[^""]+)"">");
+            if (m.Success)
+            {
+                string postData = @"decision=accept&returnTo=http%3A%2F%2Ftweakers.net%2F&tweakers_token=" +
+                    HttpUtility.UrlEncode(m.Groups["tweakerstoken"].Value);
+
+                cc = MyGetWebData(@"https://secure.tweakers.net/my.tnet/cookies/", postData, cc);
+            }
+            return base.DiscoverDynamicCategories();
+        }
+
+        private CookieContainer MyGetWebData(string url, string postData, CookieContainer cc)
+        {
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate; // turn on automatic decompression of both formats so we can say we accept them
+            request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate"); // we accept compressed content
+            request.CookieContainer = cc;
+
+            request.Accept = "*/*";
+            request.UserAgent = OnlineVideoSettings.Instance.UserAgent;
+
+            byte[] data = Encoding.UTF8.GetBytes(postData);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+            request.ProtocolVersion = HttpVersion.Version10;
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(data, 0, data.Length);
+            requestStream.Close();
+
+            request.AllowAutoRedirect = false;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse()) { };//just to get the cookies
+
+            CookieContainer result = new CookieContainer();
+            foreach (Cookie cookie in cc.GetCookies(new Uri("http://tmp.tweakers.net")))
+            {
+                cookie.Domain = new Uri(baseUrl).Host;
+                result.Add(cookie);
+            }
+            return result;
+        }
+
+
+        protected override CookieContainer GetCookie()
+        {
+            return cc;
+        }
+    }
+}
