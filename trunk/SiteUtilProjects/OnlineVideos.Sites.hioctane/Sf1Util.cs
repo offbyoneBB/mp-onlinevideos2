2b469f806cc48d50a39310c33ff74937d2172b85
bborgsdorf@gmail.com <bborgsdorf@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-01-04 16:00:17 +0000

    fixed SF1

--- trunk/SiteUtilProjects/OnlineVideos.Sites.hioctane/Sf1Util.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.hioctane/Sf1Util.cs
@@ -1,95 +1,143 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Text.RegularExpressions;
-using RssToolkit.Rss;
+using System.Web;
 
 namespace OnlineVideos.Sites
 {
     public class Sf1Util : SiteUtilBase
     {
-        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to parse the baseUrl for dynamic categories. Group names: 'url', 'title', 'thumb', 'desc'.")]
-        string dynamicCategoriesRegEx = @"<img\sclass=""az_thumb""\ssrc=""(?<thumb>[^""]+)""\swidth=""\d*""\sheight=""\d*""\salt=""(?<alt>[^""]+)""\s/></a><a\sclass=""sendung_name""\shref=""(?<url>[^""]+)"">(?<title>[^<]+)</a><p\sclass=""az_description"">(?<desc>[^<]+)</p>";
-        [Category("OnlineVideosConfiguration"), Description("Format string applied to the 'url' match retrieved from the dynamicCategoriesRegEx.")]
-        string dynamicCategoryUrlFormatString = "http://videoportal.sf.tv/rss{0}";
-        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to parse the video 'title' and 'url' from a category rss page.")]
-        string videoItemRegex = @"a\shref=""/video\?id=(?<url>[^;]+);DCSext.zugang=videoportal_sendungsuebersicht"">(?<title>[^<]+)</a>";
-        [Category("OnlineVideosConfiguration"), Description("Format string applied to the video url of an item that was found in the rss.")]
-        string videoUrlFormatString = "http://videoportal.sf.tv/cvis/segment/{0}/.json";
+        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to parse a html page for dynamic categories. Group names: 'url', 'title', 'thumb', 'description'. Will be used on the web pages resulting from the links from the dynamicCategoriesRegEx. Will not be used if not set.")]
+        protected string dynamicSubCategoriesRegEx;
+        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to parse a html page for videos. Group names: 'VideoUrl', 'ImageUrl', 'Title', 'Duration', 'Description', 'Airdate'.")]
+        protected string videoListRegEx;
         [Category("OnlineVideosConfiguration"), Description("Regular Expression used to parse the actual playback urls with some info ('audio', 'video', 'bitrate', 'url').")]
-        string videoUrlRegex = @"""codec_video"":""(?<video>[^""]+)"",""codec_audio"":""(?<audio>[^""]+)"",""bitrate"":(?<bitrate>[^,]+),""frame_width"":\d*,""frame_height"":\d*,""url"":""(?<url>[^""]+)""";
-        [Category("OnlineVideosConfiguration"), Description("Url used for to get the categories.")]
-        string baseUrl = "http://videoportal.sf.tv/sendungen";
+        protected string videoUrlRegex;
+        [Category("OnlineVideosConfiguration"), Description("Format string applied to the video url of an item that was found in the rss.")]
+        protected string videoUrlFormatString;
+        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to split the categories html for subcategories")]
+        protected string subCategoriesSplit;
+        [Category("OnlineVideosConfiguration"), Description("Regular Expression used to get the title for a subcategory")]
+        protected string subCategoriesName;
 
-        Regex regEx_dynamicCategories, regEx_VideoItem, regEx_VideoUrl;
+        Regex regEx_dynamicSubCategories, regEx_VideoList, regEx_VideoUrl;
 
         public override void Initialize(SiteSettings siteSettings)
         {
             base.Initialize(siteSettings);
 
-            regEx_dynamicCategories = new Regex(dynamicCategoriesRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
-            regEx_VideoItem = new Regex(videoItemRegex, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
+            regEx_dynamicSubCategories = new Regex(dynamicSubCategoriesRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture);
+            regEx_VideoList = new Regex(videoListRegEx, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture);
             regEx_VideoUrl = new Regex(videoUrlRegex, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
         }
 
         public override int DiscoverDynamicCategories()
         {
-            Settings.Categories.Clear();
-
-            string data = GetWebData(baseUrl);
-            string xmlUrl = String.Empty;
-
-            if (!string.IsNullOrEmpty(data))
+            foreach (var cat in Settings.Categories)
             {
-                Match m = regEx_dynamicCategories.Match(data);
+                cat.HasSubCategories = true;
+                cat.SubCategories = new List<Category>();
+            }
+            Settings.DynamicCategoriesDiscovered = true;
+            return 0;
+        }
+
+        public override int DiscoverSubCategories(Category parentCategory)
+        {
+            string data = GetWebData((parentCategory as RssLink).Url);
+            var splits = Regex.Split(data, subCategoriesSplit);
+            bool first = true;
+            foreach (string split in splits)
+            {
+                if (first)
+                {
+                    first = false;
+                    continue;
+                }
+                string groupTitle = Regex.Match(split, subCategoriesName).Groups["title"].Value;
+                Category group = new Category() { Name = groupTitle, ParentCategory = parentCategory, HasSubCategories = true, SubCategories = new List<Category>(), SubCategoriesDiscovered = true };
+                Match m = regEx_dynamicSubCategories.Match(split);
                 while (m.Success)
                 {
-                    RssLink cat = new RssLink();
-                    cat.Name = m.Groups["title"].Value;
-                    cat.Url = m.Groups["url"].Value;
-                    cat.Thumb = m.Groups["thumb"].Value.Replace("?width=80", "");
-                    cat.Description = m.Groups["desc"].Value;
-
-                    Settings.Categories.Add(cat);
+                    RssLink subCat = new RssLink();
+                    subCat.Url = m.Groups["url"].Value;
+                    if (!String.IsNullOrEmpty(subCat.Url) && !Uri.IsWellFormedUriString(subCat.Url, System.UriKind.Absolute))
+                        subCat.Url = new Uri(new Uri((parentCategory as RssLink).Url), subCat.Url).AbsoluteUri;
+                    subCat.Name = HttpUtility.HtmlDecode(m.Groups["title"].Value.Trim().Replace('\n', ' '));
+                    subCat.Thumb = m.Groups["thumb"].Value;
+                    subCat.Thumb = subCat.Thumb.Substring(0, subCat.Thumb.IndexOf("?"));
+                    if (!String.IsNullOrEmpty(subCat.Thumb) && !Uri.IsWellFormedUriString(subCat.Thumb, System.UriKind.Absolute))
+                        subCat.Thumb = new Uri(new Uri((parentCategory as RssLink).Url), subCat.Thumb).AbsoluteUri;
+                    subCat.Description = m.Groups["description"].Value;
+                    subCat.ParentCategory = group;
+                    group.SubCategories.Add(subCat);
                     m = m.NextMatch();
                 }
-                Settings.DynamicCategoriesDiscovered = true;
+                parentCategory.SubCategories.Add(group);
+                parentCategory.SubCategoriesDiscovered = true;
             }
-            return Settings.Categories.Count;
+            return parentCategory.SubCategories.Count;
         }
         
+        public override List<VideoInfo> getVideoList(Category category)
+        {
+            List<VideoInfo> videoList = new List<VideoInfo>();
+
+            string data = GetWebData((category as RssLink).Url);
+            if (data.Length > 0)
+            {
+                if (regEx_VideoList != null)
+                {
+                    Match m = regEx_VideoList.Match(data);
+                    while (m.Success)
+                    {
+                        VideoInfo videoInfo = new VideoInfo();
+                        videoInfo.Title = HttpUtility.HtmlDecode(m.Groups["Title"].Value);
+                        videoInfo.VideoUrl = m.Groups["VideoUrl"].Value;
+                        if (!String.IsNullOrEmpty(videoInfo.VideoUrl) && !Uri.IsWellFormedUriString(videoInfo.VideoUrl, System.UriKind.Absolute))
+                            videoInfo.VideoUrl = new Uri(new Uri((category as RssLink).Url), videoInfo.VideoUrl).AbsoluteUri;
+                        videoInfo.ImageUrl = m.Groups["ImageUrl"].Value;
+                        if (!String.IsNullOrEmpty(videoInfo.ImageUrl) && !Uri.IsWellFormedUriString(videoInfo.ImageUrl, System.UriKind.Absolute))
+                            videoInfo.ImageUrl = new Uri(new Uri((category as RssLink).Url), videoInfo.ImageUrl).AbsoluteUri;
+                        videoInfo.Length = Utils.PlainTextFromHtml(m.Groups["Duration"].Value);
+                        videoList.Add(videoInfo);
+                        m = m.NextMatch();
+                    }
+                }
+            }
+
+            return videoList;
+        }
+
         public override String getUrl(VideoInfo video)
         {
-            if (video.VideoUrl.StartsWith("http")) return video.VideoUrl;
-
-            if (video.PlaybackOptions == null)
+            string data = GetWebData(string.Format(videoUrlFormatString, HttpUtility.ParseQueryString(new Uri(video.VideoUrl).Query)["id"]));
+            Match m = regEx_VideoUrl.Match(data);
+            video.PlaybackOptions = new Dictionary<string, string>();
+            while (m.Success)
             {
-                string data = GetWebData(string.Format(videoUrlFormatString, video.VideoUrl));
-                Match m = regEx_VideoUrl.Match(data);
-                video.PlaybackOptions = new Dictionary<string, string>();
-                while (m.Success)
+                if (!m.Groups["url"].Value.Contains("no streaming"))
                 {
-                    if (!m.Groups["url"].Value.Contains("no streaming"))
+                    if (m.Groups["video"].Value.Contains("wmv3"))
                     {
-                        if (m.Groups["video"].Value.Contains("wmv3"))
-                        {
-                            string title = "WMV (" + m.Groups["bitrate"].Value + "K)";
-                            string url = m.Groups["url"].Value.Substring(0, m.Groups["url"].Value.IndexOf("?")).Replace("\\/", "/");
-                            url = ParseASX(url)[0];
-                            video.PlaybackOptions.Add(title, url);
-                        }
-                        else
-                        {
-                            string title = "FLV (" + m.Groups["bitrate"].Value + "K)";
-                            string url = m.Groups["url"].Value.Replace("\\/", "/");
-							url = new MPUrlSourceFilter.RtmpUrl(url) { SwfUrl = "http://videoportal.sf.tv/flash/videoplayer.swf", SwfVerify = true }.ToString();
-                            video.PlaybackOptions.Add(title, url);
-                        }
+                        string title = "WMV (" + m.Groups["bitrate"].Value + "K)";
+                        string url = m.Groups["url"].Value.Substring(0, m.Groups["url"].Value.IndexOf("?")).Replace("\\/", "/");
+                        url = ParseASX(url)[0];
+                        video.PlaybackOptions.Add(title, url);
                     }
-                    m = m.NextMatch();
-                }                
+                    else
+                    {
+                        string title = "FLV (" + m.Groups["bitrate"].Value + "K)";
+                        string url = m.Groups["url"].Value.Replace("\\/", "/");
+                        url = new MPUrlSourceFilter.RtmpUrl(url) { SwfUrl = "http://videoportal.sf.tv/flash/videoplayer.swf", SwfVerify = true }.ToString();
+                        video.PlaybackOptions.Add(title, url);
+                    }
+                }
+                m = m.NextMatch();
             }
+
             if (video.PlaybackOptions != null && video.PlaybackOptions.Count > 0)
             {
                 return video.PlaybackOptions.First().Value;
@@ -97,36 +145,5 @@ namespace OnlineVideos.Sites
             return "";
         }
 
-        public override List<VideoInfo> getVideoList(Category category)
-        {
-            List<VideoInfo> videoList = new List<VideoInfo>();
-
-            foreach (RssItem rssItem in GetWebData<RssDocument>(string.Format(dynamicCategoryUrlFormatString, ((RssLink)category).Url)).Channel.Items)
-            {
-                Match m = regEx_VideoItem.Match(rssItem.Description);
-                if (m.Success) videoList.Add(new VideoInfo() { Title = rssItem.Title, VideoUrl = m.Groups["url"].Value });
-            }
-
-            // try podcast if no vidoes found in rss
-            if (videoList.Count == 0)
-            {
-                try
-                {
-                    foreach (RssItem rssItem in GetWebData<RssDocument>("http://feeds.sf.tv/podcast/" + category.Name.ToLower().Replace(" ", "").Replace("-", "").Replace("ö","oe").Replace("ü","ue").Replace("ä","ae"), forceUTF8: true).Channel.Items)
-                    {
-                        VideoInfo video = VideoInfo.FromRssItem(rssItem, false, new Predicate<string>(isPossibleVideo));
-                        // only if a video url was set, add this Video to the list
-                        if (!string.IsNullOrEmpty(video.VideoUrl))
-                        {
-                            //video.VideoUrl = System.Web.HttpUtility.UrlDecode(video.VideoUrl);
-                            videoList.Add(video);
-                        }
-                    }
-                }
-                catch { }
-            }
-
-            return videoList;
-        }
     }
 }
