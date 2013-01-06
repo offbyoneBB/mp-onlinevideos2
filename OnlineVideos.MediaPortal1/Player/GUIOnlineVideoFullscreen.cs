9652c6769093a09c9941a1f35b1a654aedf97e7e
bborgsdorf <bborgsdorf@gmail.com>
2013-01-06 20:54:38 +0000

    fixed: VideoScreen switching between Fullscreen and MiniWindow not working in MP1.3 fixed: VolumeOverlay shown incorrectly in MP1.3 in FullScreenVideo

--- OnlineVideos.MediaPortal1/Player/GUIOnlineVideoFullscreen.cs
+++ OnlineVideos.MediaPortal1/Player/GUIOnlineVideoFullscreen.cs
@@ -21,34 +21,44 @@ namespace OnlineVideos.MediaPortal1.Player
             return base.Load(GUIGraphicsContext.Skin + @"\myonlinevideosFullScreen.xml");
         }
 
-        public override void OnAction(Action action)
-        {
-            if (action.wID == Action.ActionType.ACTION_VOLUME_UP ||
-                action.wID == Action.ActionType.ACTION_VOLUME_DOWN ||
-                action.wID == Action.ActionType.ACTION_VOLUME_MUTE)
-            {
-                // MediaPortal core sends this message to the Fullscreenwindow, we need to do it ourselves to make the Volume OSD show up
-                base.OnAction(new Action(Action.ActionType.ACTION_SHOW_VOLUME, 0, 0));
-                return;
-            }
-            else
-            {
-                // translate the action as if our window was the WINDOW_FULLSCREEN_VIDEO
-                if (ActionTranslator.GetAction((int)Window.WINDOW_FULLSCREEN_VIDEO, action.m_key, ref action))
-                {
-                    if (action.wID == Action.ActionType.ACTION_SHOW_OSD) // handle the OSD action differently - we need to show our OSD
-                    {
-                        base.OnAction(action);
-                        if (GUIWindowManager.VisibleOsd == Window.WINDOW_OSD)
-                        {
-                            GUIWindowManager.VisibleOsd = (Window)GUIOnlineVideoOSD.WINDOW_ONLINEVIDEOS_OSD;
-                        }
-                        return;
-                    }
-                }
-            }
-            base.OnAction(action);
-        }
+		public override void OnAction(Action action)
+		{
+			if (action.wID == Action.ActionType.ACTION_KEY_PRESSED)
+			{
+				// translate the action as if our window was the WINDOW_FULLSCREEN_VIDEO
+				if (ActionTranslator.GetAction((int)Window.WINDOW_FULLSCREEN_VIDEO, action.m_key, ref action))
+				{
+					if (action.wID == Action.ActionType.ACTION_SHOW_OSD) // handle the OSD action differently - we need to show our OSD
+					{
+						base.OnAction(action);
+						if (GUIWindowManager.VisibleOsd == Window.WINDOW_OSD)
+						{
+							GUIWindowManager.VisibleOsd = (Window)GUIOnlineVideoOSD.WINDOW_ONLINEVIDEOS_OSD;
+						}
+						return;
+					}
+					else if (action.wID == Action.ActionType.ACTION_SHOW_GUI)
+					{
+						return;
+					}
+					else if (action.wID == Action.ActionType.ACTION_VOLUME_UP ||
+							 action.wID == Action.ActionType.ACTION_VOLUME_DOWN ||
+							 action.wID == Action.ActionType.ACTION_VOLUME_MUTE)
+					{
+						// MediaPortal core sends this message to the Fullscreenwindow, we need to do it ourselves to make the Volume OSD show up
+						GUIWindowManager.SendThreadCallback((p1, p2, o) => 
+						{
+							Action showVolume = new Action(Action.ActionType.ACTION_SHOW_VOLUME, 0, 0);
+							GUIWindowManager.OnAction(showVolume);
+							return 0; 
+						}, 0, 0, null);
+						return;
+					}
+				}
+			}
+
+			base.OnAction(action);
+		}
 
         public override bool OnMessage(GUIMessage message)
         {
