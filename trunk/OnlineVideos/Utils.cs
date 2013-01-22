bdd4973056ada854885d5a50e0143c9a2ef2f9de
bborgsdorf@gmail.com <bborgsdorf@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-01-22 14:41:42 +0000

    conflict resolution when loading of SiteUtils and Hosters with equal names: Use the Type defined in the assembly with the latest compile time

--- trunk/OnlineVideos/Utils.cs
+++ trunk/OnlineVideos/Utils.cs
@@ -413,5 +413,33 @@ namespace OnlineVideos
 				list[n] = value;
 			}
 		}
+
+		internal static DateTime RetrieveLinkerTimestamp(string filePath)
+		{
+			const int c_PeHeaderOffset = 60;
+			const int c_LinkerTimestampOffset = 8;
+			byte[] b = new byte[2048];
+			System.IO.Stream s = null;
+
+			try
+			{
+				s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+				s.Read(b, 0, 2048);
+			}
+			finally
+			{
+				if (s != null)
+				{
+					s.Close();
+				}
+			}
+
+			int i = System.BitConverter.ToInt32(b, c_PeHeaderOffset);
+			int secondsSince1970 = System.BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
+			DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0);
+			dt = dt.AddSeconds(secondsSince1970);
+			dt = dt.AddHours(TimeZone.CurrentTimeZone.GetUtcOffset(dt).Hours);
+			return dt;
+		}
     }
 }
