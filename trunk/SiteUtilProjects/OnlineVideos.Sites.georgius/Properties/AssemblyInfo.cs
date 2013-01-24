e4f69abf0e5be838a4f7def9b5298d7d43d2639f
bborgsdorf@gmail.com <bborgsdorf@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-01-24 16:16:07 +0000

    fixed duplicate version attribute

--- trunk/SiteUtilProjects/OnlineVideos.Sites.georgius/Properties/AssemblyInfo.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.georgius/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b788588d-0eee-4e5b-86f2-737332606658")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.1")]
-[assembly: AssemblyFileVersion("1.4.0.1")]
