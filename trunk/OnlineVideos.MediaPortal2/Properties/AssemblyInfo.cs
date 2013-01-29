105ec84eb2b1707b25552501cf0a94a03ac72e56
bborgsdorf@gmail.com <bborgsdorf@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-01-29 20:33:11 +0000

    OnlineVideos for MediaPortal2: - added automatic assembly versioning from SVN revision via PreBuild event to project - added shortcut (O for OnlineVideos) - made player load our sourcefilter from file, so no need to register anymore ("unzip" deployment now almost possible)

--- trunk/OnlineVideos.MediaPortal2/Properties/AssemblyInfo.cs
+++ trunk/OnlineVideos.MediaPortal2/Properties/AssemblyInfo.cs
@@ -2,35 +2,17 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("OnlineVideos.MediaPortal2")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("OnlineVideos.MediaPortal2")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OnlineVideos MediaPortal2 GUI")]
+[assembly: AssemblyDescription("OnlineVideos GUI FrontEnd for MediaPortal 2")]
+[assembly: AssemblyProduct("OnlineVideos")]
 
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2976a058-f20b-4628-b317-aef5da5c0d0a")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
