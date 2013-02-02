6373579722924926e96d5fdf33cce5098d13fbb7
bborgsdorf@gmail.com <bborgsdorf@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-02-02 13:39:06 +0000

    added Sorting capabilities to Favorites Site (supported by Name, Airdate, Duration)

--- trunk/OnlineVideos/Sites/FavoriteUtil.cs
+++ trunk/OnlineVideos/Sites/FavoriteUtil.cs
@@ -7,7 +7,7 @@ namespace OnlineVideos.Sites
     /// <summary>
     /// Description of FavoriteUtil.
     /// </summary>
-    public class FavoriteUtil : SiteUtilBase
+    public class FavoriteUtil : SiteUtilBase, IFilter
     {
         public class FavoriteCategory : Category
         {
@@ -69,7 +69,7 @@ namespace OnlineVideos.Sites
         {
             if (category is RssLink)
             {
-                return OnlineVideoSettings.Instance.FavDB.getFavoriteVideos(((RssLink)category).Url, null);
+                return OrderVideos(OnlineVideoSettings.Instance.FavDB.getFavoriteVideos(((RssLink)category).Url, null));
             }
             return null;
         }
@@ -162,7 +162,7 @@ namespace OnlineVideos.Sites
 
         public override List<VideoInfo> Search(string query)
         {
-            return OnlineVideoSettings.Instance.FavDB.getFavoriteVideos(null, query);
+            return OrderVideos(OnlineVideoSettings.Instance.FavDB.getFavoriteVideos(null, query));
         }
 
         #endregion
@@ -212,5 +212,100 @@ namespace OnlineVideos.Sites
         }
 
         #endregion
+
+		#region IFilter Member
+
+		string lastSort = "default";
+
+		public List<VideoInfo> filterVideoList(Category category, int maxResult, string orderBy, string timeFrame)
+		{
+			lastSort = orderBy;
+			return getVideoList(category);
+		}
+
+		public List<VideoInfo> filterSearchResultList(string query, int maxResult, string orderBy, string timeFrame)
+		{
+			lastSort = orderBy;
+			return Search(query);
+		}
+
+		public List<VideoInfo> filterSearchResultList(string query, string category, int maxResult, string orderBy, string timeFrame)
+		{
+			return null;
+		}
+
+		public List<int> getResultSteps()
+		{
+			return new List<int>();
+		}
+
+		public Dictionary<string, string> getOrderbyList()
+		{
+			Dictionary<string, string> options = new Dictionary<string, string>();
+			options.Add(Translation.Instance.Default, "default");
+			options.Add(Translation.Instance.Name, "name");
+			options.Add(Translation.Instance.Airdate, "airdate");
+			options.Add(Translation.Instance.Runtime, "runtime");
+			return options;
+		}
+
+		public Dictionary<string, string> getTimeFrameList()
+		{
+			return new Dictionary<string, string>();
+		}
+
+		List<VideoInfo> OrderVideos(List<VideoInfo> videos)
+		{
+			switch (lastSort)
+			{
+				case "name":
+					videos.Sort((Comparison<VideoInfo>)delegate(VideoInfo v1, VideoInfo v2)
+					{
+						return v1.Title.CompareTo(v2.Title);
+					});
+					break;
+				case "airdate":
+					videos.Sort((Comparison<VideoInfo>)delegate(VideoInfo v1, VideoInfo v2)
+					{
+						DateTime airdate_v1;
+						DateTime airdate_v2;
+						if (DateTime.TryParse(v1.Airdate, out airdate_v1) && DateTime.TryParse(v2.Airdate, out airdate_v2))
+							return airdate_v2.CompareTo(airdate_v1);
+						else
+							return v2.Airdate.CompareTo(v1.Airdate); // string compare as fallback
+					});
+					break;
+				case "runtime":
+					videos.Sort((Comparison<VideoInfo>)delegate(VideoInfo v1, VideoInfo v2)
+					{
+						double seconds_v1 = TryGetSeconds(v1.Length);
+						double seconds_v2 = TryGetSeconds(v2.Length);
+						if (seconds_v1 != 0.0f && seconds_v2 != 0.0f)
+							return seconds_v2.CompareTo(seconds_v1);
+						else
+							return v2.Length.CompareTo(v1.Length); // string compare as fallback
+					});
+					break;
+			}
+			return videos;
+		}
+
+		double TryGetSeconds(string length)
+		{
+			// might be a single number
+			double seconds;
+			if (!double.TryParse(length, System.Globalization.NumberStyles.None | System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.CultureInfo.CreateSpecificCulture("en-US"), out seconds))
+			{
+				// might be in HH:mm:ss notation
+				TimeSpan timespan;
+				if (TimeSpan.TryParse(length, out timespan))
+				{
+					seconds = timespan.TotalSeconds;
+				}
+			}
+			return seconds;
+		}
+
+		#endregion
     }
 }
