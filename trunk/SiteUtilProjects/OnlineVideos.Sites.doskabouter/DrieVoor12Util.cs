33db7241819dd9e8b4a8b5a2175af0eec427347f
paul.doskabouter@gmail.com <paul.doskabouter@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-02-06 21:58:18 +0000

    Added 3voor12

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/DrieVoor12Util.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace OnlineVideos.Sites
+{
+    public class DrieVoor12Util : GenericSiteUtil
+    {
+        public override List<VideoInfo> getVideoList(Category category)
+        {
+            List<VideoInfo> result = base.getVideoList(category);
+            result.ForEach(item => item.Other = ((RssLink)category).Url);
+            return result;
+        }
+
+        public override string getUrl(VideoInfo video)
+        {
+            int p = video.VideoUrl.LastIndexOf('.');
+            Match m = Regex.Match(video.VideoUrl, @"program\.(?<id>\d*)\.html");
+            if (m.Success)
+            {
+                string webData = GetWebDataFromPost(@"http://3voor12.vpro.nl/odi/",
+                    "locations=" + HttpUtility.UrlEncode(
+                    String.Format(@"[{{""urn"":""urn:vpro:media:program:{0}"",""extension"":""mp4""}}]", m.Groups["id"].Value)
+                    ),
+                    cc: GetCookie(), referer: (string)video.Other);
+                Match matchFileUrl = regEx_FileUrl.Match(webData);
+                if (matchFileUrl.Success)
+                    return Newtonsoft.Json.JsonConvert.DeserializeObject<string>(matchFileUrl.Groups["m0"].Value);
+            }
+            return String.Empty;
+        }
+    }
+}
