8bb6ec827bf502eab2852256c6444f133a753c4f
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-03-18 21:16:51 +0000

    Added Rockpalast

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/RockPalastUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineVideos.Sites
+{
+    public class RockpalastUtil : GenericSiteUtil
+    {
+        public override int DiscoverDynamicCategories()
+        {
+            int res = base.DiscoverDynamicCategories();
+            RssLink extra = new RssLink() { Url = baseUrl, Name = "Nur noch kurz online:", Other = 1 };
+            Settings.Categories.Insert(0, extra);
+            extra = new RssLink() { Url = baseUrl, Name = "Zuletzt hinzugekommen:", Other = 2 };
+            Settings.Categories.Insert(1, extra);
+            return res + 2;
+        }
+
+        public override List<VideoInfo> getVideoList(Category category)
+        {
+            string url = ((RssLink)category).Url;
+            string data = GetWebData(url);
+            string[] parts = data.Split(new[] { "wsArticleUnit" }, StringSplitOptions.RemoveEmptyEntries);
+            int? n = category.Other as int?;
+            if (n.HasValue)
+                return Parse(url, parts[n.Value]);
+            else
+                return Parse(url, parts[3]);
+        }
+    }
+}
