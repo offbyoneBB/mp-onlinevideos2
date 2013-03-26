e8b5e2e7b6c35df220b25aa98e3f168b87d447c6
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-03-26 20:14:32 +0000

    Added EventCaster

---
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/EventCasterUtil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Web;
+using OnlineVideos.MPUrlSourceFilter;
+
+namespace OnlineVideos.Sites
+{
+    public class EventCasterUtil : GenericSiteUtil
+    {
+
+        protected override void ExtraVideoMatch(VideoInfo video, GroupCollection matchGroups)
+        {
+            video.Airdate = matchGroups["date"].Value + ' ' + matchGroups["time"].Value;
+        }
+
+        public override string getUrl(VideoInfo video)
+        {
+            string webData = GetWebData(video.VideoUrl);
+
+            Match m = regEx_FileUrl.Match(webData);
+            if (m.Success)
+            {
+                string newUrl = m.Groups["m0"].Value + "?authenticity_token=" + HttpUtility.UrlEncode(m.Groups["m1"].Value);
+                webData = GetWebData(newUrl);
+                m = Regex.Match(webData, @"url:\s\\""(?<url>[^\\]*)\\[^{]*{[^{]*{\\n\s*url:\s\\""[^""]*"",\\n\s*netConnectionUrl:\s\\""(?<netConnectionUrl>[^\\]*)\\""", defaultRegexOptions);
+                if (m.Success)
+                {
+                    string ncUrl = m.Groups["netConnectionUrl"].Value;
+                    string[] parts = ncUrl.Split('/');
+                    string playPath = parts[4];
+                    bool live = webData.Contains("live = true");
+                    if (live)
+                    {
+                        int p = playPath.IndexOf("stream");
+                        if (p >= 0) playPath = playPath.Insert(p, "/");
+                    }
+                    RtmpUrl result = new RtmpUrl(ncUrl)
+                    {
+                        PlayPath = m.Groups["url"].Value,
+                        App = String.Format("{0}/{1}{2}", parts[3], parts[4], m.Groups["m1"].Value),
+                        Live = live
+                    };
+                    return result.ToString();
+                }
+            }
+            return String.Empty;
+
+        }
+    }
+}
