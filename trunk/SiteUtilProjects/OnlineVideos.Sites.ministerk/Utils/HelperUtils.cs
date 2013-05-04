8928e6f0da2941d7a7c4458ed57efc0f3da9e9f8
ministerkrister@gmail.com <ministerkrister@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-05-04 22:32:12 +0000

    Fixed TV4Play Util Parsing of site, added f4m streams, subtitles and filtering of premium videos A Helper Utils class for project common methods. First commit!

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.ministerk/Utils/HelperUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineVideos.Sites.Utils
+{
+    class HelperUtils
+    {
+        public static string GetRandomChars(int amount)
+        {
+            var random = new Random();
+            var sb = new System.Text.StringBuilder(amount);
+            for (int i = 0; i < amount; i++) sb.Append(System.Text.Encoding.ASCII.GetString(new byte[] { (byte)random.Next(65, 90) }));
+            return sb.ToString();
+        }
+    }
+}
