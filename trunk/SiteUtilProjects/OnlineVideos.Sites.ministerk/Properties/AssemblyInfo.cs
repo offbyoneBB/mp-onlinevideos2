8a32e376c5dbac3645fcd00a58c32ce0c482adc2
bborgsdorf@gmail.com <bborgsdorf@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-05-04 10:50:17 +0000

    added new SiteUtil project for Swedish sites to be maintained by ministerk moved all Swedish sites from offbyone to ministerk project added new dll to MPEI installer script

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.ministerk/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OnlineVideos.Sites.ministerk")]
+[assembly: AssemblyDescription("SiteUtils for OnlineVideos by ministerk")]
+[assembly: AssemblyProduct("OnlineVideos")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("EA0C065D-39AC-42E2-AEB5-C426F1925B69")]
