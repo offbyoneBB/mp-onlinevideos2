12bbd42fad88b86334ed211dd7f2732c3b7a6327
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-06-15 20:54:15 +0000

    Fixed De Redactie

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/DeRedactieUtil.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using HtmlAgilityPack;
+
+namespace OnlineVideos.Sites
+{
+    public class DeRedactieUtil : GenericSiteUtil
+    {
+        public override int DiscoverDynamicCategories()
+        {
+            string data = GetWebData(baseUrl);
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(data);
+            HtmlNode ul = htmlDoc.DocumentNode.SelectSingleNode("//ul[@class = 'hnav nav2']");
+            HtmlNodeCollection mainMenu = ul.SelectNodes("li");
+            foreach (HtmlNode node in mainMenu)
+            {
+                RssLink category = new RssLink()
+                {
+                    Name = HttpUtility.HtmlDecode(node.SelectSingleNode("a/span").InnerText)
+                };
+                HtmlAttribute classAtt = node.Attributes["class"];
+                if (classAtt != null && classAtt.Value == "hasSubMenu")
+                    AddSubcats(category, node.SelectNodes(".//ul/li"));
+                else
+                    category.Url = FormatDecodeAbsolutifyUrl(baseUrl, node.SelectSingleNode("a").Attributes["href"].Value, null, UrlDecoding.None);
+                Settings.Categories.Add(category);
+            }
+            Settings.DynamicCategoriesDiscovered = true;
+            return Settings.Categories.Count;
+        }
+
+        private void AddSubcats(Category category, HtmlNodeCollection subcats)
+        {
+            category.HasSubCategories = true;
+            category.SubCategories = new List<Category>();
+            foreach (HtmlNode sub in subcats)
+            {
+                HtmlNode aNode = sub.SelectSingleNode("a");
+                RssLink subcat = new RssLink()
+                {
+                    Name = HttpUtility.HtmlDecode(aNode.ChildNodes[1].InnerText),
+                    Url = FormatDecodeAbsolutifyUrl(baseUrl, aNode.Attributes["href"].Value, null, UrlDecoding.None),
+                    ParentCategory = category
+                };
+                category.SubCategories.Add(subcat);
+            }
+            category.SubCategoriesDiscovered = true;
+        }
+
+        public override List<VideoInfo> getVideoList(Category category)
+        {
+            string url = ((RssLink)category).Url;
+            string data = GetWebData(url);
+            int p = data.IndexOf(@"class=""splitter split12-12");
+            if (p != -1)
+                data = data.Substring(0, p);
+
+            return Parse(url, data);
+        }
+    }
+}
