2890ce5853159007f4a30c2572e4e916300c5e9a
bborgsdorf@gmail.com <bborgsdorf@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-07-28 14:37:58 +0000

    fixed NDR

--- trunk/SiteUtilProjects/OnlineVideos.Sites.hioctane/NdrUtil.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.hioctane/NdrUtil.cs
@@ -1,44 +1,113 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
-using System.Xml;
+using System.Web;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace OnlineVideos.Sites
 {
-    public class NdrUtil : GenericSiteUtil
+    public class NdrUtil : SiteUtilBase
     {
-        string rtmpBaseLink = "rtmp://cp160844.edgefcs.net/ondemand/flashmedia/streams/ndr/";
+		const string baseUrl = "http://www.ndr.de";
+		const string category_list_url = "http://www.ndr.de/mediathek/dropdown101-extapponly.html";
+		const string search_url = "http://www.ndr.de/mediathek/mediatheksuche101.html?search_video=true&query={0}";
+
+		string nextPageUrl = "";
+
+		public override int DiscoverDynamicCategories()
+		{
+			Settings.Categories.Clear();
+			HtmlDocument htmlDoc = new HtmlDocument();
+			htmlDoc.LoadHtml(GetWebData(category_list_url));
+			var list = htmlDoc.DocumentNode.Descendants("ul").First(ul => ul.GetAttributeValue("class", "") == "m_az_key");
+			foreach (var h5 in list.Descendants("h5"))
+			{
+				var a = h5.Element("a");
+				Settings.Categories.Add(new RssLink() { Name = a.InnerText, Url = baseUrl + a.GetAttributeValue("href", "").Replace("103", "105") });
+			}
+			Settings.DynamicCategoriesDiscovered = Settings.Categories.Count > 0;
+			return Settings.Categories.Count;
+		}
+
+		public override List<VideoInfo> getVideoList(Category category)
+		{
+			HasNextPage = false;
+			var result = new List<VideoInfo>();
+
+			var htmlDoc = new HtmlDocument();
+			htmlDoc.LoadHtml(GetWebData((category as RssLink).Url));
+			foreach (var item in htmlDoc.DocumentNode.Descendants("div").Where(div => div.GetAttributeValue("class", "") == "m_teaser"))
+			{
+				string videoUrl = item.Descendants("a").First().GetAttributeValue("href", "");
+				if (!string.IsNullOrEmpty(videoUrl))
+				{
+					if (!Uri.IsWellFormedUriString(videoUrl, UriKind.Absolute))
+						videoUrl = new Uri(new Uri((category as RssLink).Url), videoUrl).AbsoluteUri;
+					var length = item.Descendants("span").FirstOrDefault(s => s.GetAttributeValue("class", "") == "runtime");
+					var video = new VideoInfo()
+					{
+						Title = item.Descendants("h4").First().InnerText,
+						Description = item.Descendants("p").First().FirstChild.InnerText,
+						VideoUrl = videoUrl,
+						ImageUrl = baseUrl + item.Descendants("img").First().GetAttributeValue("src", ""),
+						Length = length != null ? length.InnerText : "",
+						Airdate = HttpUtility.HtmlDecode(item.Descendants("div").First(d => d.GetAttributeValue("class", "") == "subline").InnerText)
+					};
+					video.Airdate = video.Airdate.Substring(video.Airdate.LastIndexOf('-') + 1).Trim();
+					result.Add(video);
+				}
+			}
+			var weiterSpan = htmlDoc.DocumentNode.Descendants("span").FirstOrDefault(s => s.InnerText == "weiter");
+			if (weiterSpan != null && weiterSpan.ParentNode.Name == "a")
+			{
+				nextPageUrl = weiterSpan.ParentNode.GetAttributeValue("href", "");
+				if (!Uri.IsWellFormedUriString(nextPageUrl, UriKind.Absolute))
+					nextPageUrl = new Uri(new Uri((category as RssLink).Url), nextPageUrl).AbsoluteUri;
+				HasNextPage = true;
+			}
+			return result;
+		}
+
+		public override List<VideoInfo> getNextPageVideos()
+		{
+			return getVideoList(new RssLink() { Url = nextPageUrl });
+		}
 
         public override String getUrl(VideoInfo video)
         {
-            if (video.PlaybackOptions == null)
-            {
-                string data = GetWebData(video.VideoUrl);
-                video.PlaybackOptions = new Dictionary<string, string>();
-                
-                if (!string.IsNullOrEmpty(data))
-                {
-                    XmlDocument doc = new XmlDocument();
-                    doc.LoadXml(data);
-                    XmlElement root = doc.DocumentElement;
-                    XmlNodeList list;
-                    list = root.SelectNodes("./sources/source");
-                    foreach (XmlNode node in list)
-                    {
-                        string title = node.SelectSingleNode("@mimetype").InnerText + " - " + node.SelectSingleNode("@format").InnerText;
-                        string url = node.InnerText;
-                        int index = url.IndexOf("ndr/");
-                        if (index >= 0) url = rtmpBaseLink + url.Substring(index + 4);
-                        video.PlaybackOptions.Add(title, url);
-                    }
-                }
-            }
-            if (video.PlaybackOptions != null && video.PlaybackOptions.Count > 0)
-            {
-                var enumer = video.PlaybackOptions.GetEnumerator();
-                enumer.MoveNext();
-                return enumer.Current.Value;
-            }
-            return "";
+			video.PlaybackOptions = new Dictionary<string, string>();
+			string data = GetWebData(video.VideoUrl);
+			var match = Regex.Match(data, @"playlist:\s*(?<playlist>\[.*?\])", RegexOptions.Singleline);
+			if (match.Success)
+			{
+				var json = JsonConvert.DeserializeObject<JArray>(match.Groups["playlist"].Value);
+				foreach (var item in json.First.Children())
+				{
+					string url = item.First.Value<string>("src");
+					if (!string.IsNullOrEmpty(url))
+					{
+						if (url.EndsWith("f4m"))
+							video.PlaybackOptions.Add("HD", url = url + "?hdcore=2.8.2&g=AAAAAAAAAAAA");
+						else if (url.EndsWith("mp4"))
+							video.PlaybackOptions.Add("SD", url);
+					}
+				}
+			}
+			else
+			{
+				throw new OnlineVideosException("Keinen Stream gefunden.");
+			}
+			return video.PlaybackOptions.Select(d => d.Value).FirstOrDefault();
         }
+
+		public override bool CanSearch { get { return true; } }
+
+		public override List<ISearchResultItem> DoSearch(string query)
+		{
+			return getVideoList(new RssLink() { Url = string.Format(search_url, query) }).ConvertAll<ISearchResultItem>(v => (ISearchResultItem)v);
+		}
     }
 }
