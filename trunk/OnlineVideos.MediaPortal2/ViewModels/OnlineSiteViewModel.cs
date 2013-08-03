60e9a514eb82b6eeb053b768958461e3fd1bab9c
bborgsdorf@gmail.com <bborgsdorf@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-08-03 17:40:23 +0000

    MP2: Show local Time for LastUpdated in Site Manager (same as in MP1 now)

--- trunk/OnlineVideos.MediaPortal2/ViewModels/OnlineSiteViewModel.cs
+++ trunk/OnlineVideos.MediaPortal2/ViewModels/OnlineSiteViewModel.cs
@@ -1,4 +1,6 @@
-﻿using MediaPortal.UI.Presentation.DataObjects;
+﻿using System;
+using MediaPortal.Common.General;
+using MediaPortal.UI.Presentation.DataObjects;
 using MediaPortal.UiComponents.Media.General;
 
 namespace OnlineVideos.MediaPortal2
@@ -9,12 +11,23 @@ namespace OnlineVideos.MediaPortal2
 		public string Owner { get; protected set; }
 		public SiteSettings LocalSite { get; protected set; }
 
+		protected AbstractProperty _lastUpdatedProperty;
+		public AbstractProperty LastUpdatedProperty { get { return _lastUpdatedProperty; } }
+		public DateTime LastUpdated
+		{
+			get { return (DateTime)_lastUpdatedProperty.GetValue(); }
+			set { _lastUpdatedProperty.SetValue(value); }
+		}
+
 		public OnlineSiteViewModel(OnlineVideosWebservice.Site site, SiteSettings localSite)
 			: base(Consts.KEY_NAME, site.Name)
         {
+			_lastUpdatedProperty = new WProperty(typeof(DateTime), default(DateTime));
+
 			Site = site;
 			LocalSite = localSite;
 			Owner = !string.IsNullOrEmpty(site.Owner_FK) ? site.Owner_FK.Substring(0, site.Owner_FK.IndexOf('@')) : string.Empty;
+			LastUpdated = site.LastUpdated.ToLocalTime();
 		}
 	}
 }
