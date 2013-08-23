37c79f8cec4dfb739762f17063763c63e35ed845
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-08-23 21:34:37 +0000

    Added ipla.tv

---
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/IplaUtil.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml;
+
+namespace OnlineVideos.Sites
+{
+    public class IplaUtil : GenericSiteUtil
+    {
+        private XmlDocument doc = null;
+        private DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+
+        public override int DiscoverDynamicCategories()
+        {
+            String webData = GetWebData(baseUrl, forceUTF8: true);
+            doc = new XmlDocument();
+            doc.LoadXml(webData);
+
+            if (Settings.Categories == null) Settings.Categories = new BindingList<Category>();
+
+            foreach (Category cat in AddCats("0", doc, null))
+            {
+                Settings.Categories.Add(cat);
+            }
+            Settings.DynamicCategoriesDiscovered = true;
+            return Settings.Categories.Count;
+        }
+
+        public override int DiscoverSubCategories(Category parentCategory)
+        {
+            parentCategory.SubCategories = AddCats((string)parentCategory.Other, doc, parentCategory);
+            parentCategory.SubCategoriesDiscovered = true;
+            return parentCategory.SubCategories.Count;
+        }
+
+        public override List<VideoInfo> getVideoList(Category category)
+        {
+            String webData = GetWebData(((RssLink)category).Url, forceUTF8: true);
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(webData);
+
+            List<VideoInfo> result = new List<VideoInfo>();
+
+            foreach (XmlNode vidNode in doc.SelectNodes("//resp/VoDs/vod"))
+            {
+                VideoInfo video = new VideoInfo();
+                video.Title = vidNode.Attributes["title"].Value;
+                video.Airdate = epoch.AddSeconds(float.Parse(vidNode.Attributes["timestamp"].Value)).ToString();
+                video.Length = VideoInfo.GetDuration(vidNode.Attributes["dur"].Value);
+                video.ImageUrl = getThumb(vidNode);
+
+                video.PlaybackOptions = new Dictionary<string, string>();
+                foreach (XmlNode urlNode in vidNode.SelectNodes("srcreq[@drmtype=\"0\" and @format!=\"2\"]"))
+                {
+                    string url = urlNode.Attributes["url"].Value;
+                    string key = "quality " + urlNode.Attributes["quality"].Value +
+                        " format: " + urlNode.Attributes["format"].Value;
+                    if (!video.PlaybackOptions.ContainsKey(key))
+                        video.PlaybackOptions.Add(key, url);
+                }
+                if (video.PlaybackOptions.Count > 0)
+                    result.Add(video);
+            }
+
+            return result;
+        }
+
+        public override String getUrl(VideoInfo video)
+        {
+            if (video.PlaybackOptions == null)
+                return String.Empty;
+            if (video.PlaybackOptions.Count == 1)
+            {
+                string url = video.PlaybackOptions.First().Value;
+                video.PlaybackOptions = null;
+                return url;
+            }
+            return video.PlaybackOptions.Last().Value;
+        }
+
+        private List<Category> AddCats(string pid, XmlDocument doc, Category parentCat)
+        {
+            SortedList<int, XmlNode> catNodes = new SortedList<int, XmlNode>();
+
+            foreach (XmlNode catNode in doc.SelectNodes("//resp/cat[@pid=\"" + pid + "\" and @id!=\"5000296\"]"))
+                catNodes.Add(Int32.Parse(catNode.Attributes["seq"].Value), catNode);
+
+            List<Category> result = new List<Category>();
+            foreach (XmlNode catNode in catNodes.Values)
+            {
+                RssLink cat = new RssLink()
+                {
+                    ParentCategory = parentCat,
+                    Name = catNode.Attributes["title"].Value,
+                    Description = catNode.Attributes["descr"].Value,
+                    Other = catNode.Attributes["id"].Value,
+                    HasSubCategories = catNode.Attributes["contentupdatets"] == null
+                };
+                cat.Thumb = getThumb(catNode);
+
+                if (!cat.HasSubCategories)
+                {
+                    cat.Url = FormatDecodeAbsolutifyUrl(baseUrl, catNode.Attributes["id"].Value, dynamicSubCategoryUrlFormatString, dynamicSubCategoryUrlDecoding);
+                }
+
+                result.Add(cat);
+            }
+            return result;
+        }
+
+
+        private string getThumb(XmlNode node)
+        {
+            if (node.Attributes["thumbnail_big"] != null)
+                return node.Attributes["thumbnail_big"].Value;
+            else
+                if (node.Attributes["thumbnail"] != null)
+                    return node.Attributes["thumbnail"].Value;
+            return String.Empty;
+        }
+
+    }
+}
