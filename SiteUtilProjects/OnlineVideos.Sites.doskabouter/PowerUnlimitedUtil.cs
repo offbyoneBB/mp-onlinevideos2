c376912c28dce82a2ce9c3983e4a4da97be18275
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-09-14 19:34:11 +0000

    Added PowerUnlimited

---
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/PowerUnlimitedUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineVideos.Sites
+{
+    public class PowerUnlimitedUtil : GenericSiteUtil
+    {
+        protected override void ExtraVideoMatch(VideoInfo video, System.Text.RegularExpressions.GroupCollection matchGroups)
+        {
+            video.Airdate = String.Format("{0}-{1} {2}", matchGroups["AirdateDay"].Value.Trim(),
+                matchGroups["AirdateMonth"].Value.Trim(), matchGroups["AirdateTime"].Value.Trim());
+        }
+    }
+}
