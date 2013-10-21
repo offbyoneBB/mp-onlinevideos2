d15234a4cf56b33bd6ec8696db91c3c49080a5f7
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-10-21 18:54:31 +0000

    Fixed DivxStage

--- SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/DivxStage.cs
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/DivxStage.cs
@@ -17,6 +17,11 @@ namespace OnlineVideos.Hoster
             string page = SiteUtilBase.GetWebData(url);
             if (!string.IsNullOrEmpty(page))
             {
+                //Even newer:
+                string newMethod = Dumbify(page);
+                if (!String.IsNullOrEmpty(newMethod))
+                    page = newMethod;
+
                 //new method:
                 Match m = Regex.Match(page, @"flashvars\.domain=""(?<domain>[^""]*)"";\s*flashvars\.file=""(?<file>[^""]*)"";\s*flashvars\.filekey=""(?<filekey>[^""]*)"";");
                 if (m.Success)
@@ -57,6 +62,86 @@ namespace OnlineVideos.Hoster
             }
             return String.Empty;
         }
+
+        private string Dumbify(string webdata)
+        {
+            Match m = Regex.Match(webdata, @"\('(?<w>[^']{5,})','(?<i>[^']{5,})','(?<s>[^']{5,})'");
+            while (m.Success)
+            {
+                webdata = Dumb(m.Groups["w"].Value, m.Groups["i"].Value, m.Groups["s"].Value);
+                if (webdata.Contains("flashvars"))
+                    return webdata;
+                string s = Dumbify(webdata);
+                if (!String.IsNullOrEmpty(s))
+                    return s;
+
+                m = m.NextMatch();
+            }
+            return null;
+        }
+
+        private int FromBase36(char c)
+        {
+            if (c >= '0' && c <= '9')
+                return (int)c - 0x30;
+            if (c >= 'a' && c <= 'z')
+                return (int)c - 0x60 + 9;
+            return -1;
+
+        }
+        private int FromBase36(string s)
+        {
+            int result = 0;
+            for (int i = 0; i < s.Length; i++)
+                result = result * 36 + FromBase36(s[i]);
+            return result;
+        }
+
+        private string Dumb(string w, string i, string s)
+        {
+            string arr1 = "";
+            string arr2 = "";
+            int v1 = 0;
+            int v2 = 0;
+            int v3 = 0;
+            while (true)
+            {
+                if (v1 < 5) arr2 += w[v1];
+                else
+                    if (v1 < w.Length)
+                        arr1 += w[v1];
+                v1++;
+
+                if (v2 < 5) arr2 += i[v2];
+                else
+                    if (v2 < i.Length)
+                        arr1 += i[v2];
+                v2++;
+                if (v3 < 5) arr2 += s[v3];
+                else
+                    if (v3 < s.Length)
+                        arr1 += s[v3];
+                v3++;
+                if (w.Length + i.Length + s.Length == arr1.Length + arr2.Length)
+                    break;
+            }
+
+            v2 = 0;
+            string res = "";
+            for (v1 = 0; v1 < arr1.Length; v1 += 2)
+            {
+                int tt = -1;
+                if (arr2[v2] % 2 == 1)
+                    tt = 1;
+                int b = FromBase36(arr1.Substring(v1, 2)) - tt;
+                res += Convert.ToChar(b);
+                v2++;
+                if (v2 >= arr2.Length) v2 = 0;
+            }
+            return res;
+        }
+
+
     }
 
     public class DivxStageNet : DivxStage
