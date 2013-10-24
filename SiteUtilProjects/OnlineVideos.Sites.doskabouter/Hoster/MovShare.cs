039504948349d612bb8799cfcb0bbc084e47af7b
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-10-24 21:09:55 +0000

    Fixed Movshare, refactor last 2 commits

--- SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/MovShare.cs
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/MovShare.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using OnlineVideos.Hoster.Base;
 using OnlineVideos.Sites;
 using System.Text.RegularExpressions;
-using System.Web;
 
 namespace OnlineVideos.Hoster
 {
-    public class MovShare : HosterBase
+    public class MovShare : MyHosterBase
     {
         public override string getHosterUrl()
         {
@@ -18,18 +16,43 @@ namespace OnlineVideos.Hoster
             string page = SiteUtilBase.GetWebData(url);
             if (!string.IsNullOrEmpty(page))
             {
-                Match m = Regex.Match(page, @"flashvars\.file=""(?<fileid>[^""]*)"";\s*flashvars\.filekey=""(?<filekey>[^""]*)"";");
-                if (m.Success)
-                {
-                    page = SiteUtilBase.GetWebData(
-                        String.Format("http://www.movshare.net/api/player.api.php?pass=undefined&codes=undefined&user=undefined&file={0}&key={1}",
-                        m.Groups["fileid"].Value, HttpUtility.UrlEncode(m.Groups["filekey"].Value)));
-                    m = Regex.Match(page, @"url=(?<url>[^&]*)&");
-                    if (m.Success)
-                        return m.Groups["url"].Value;
-                }
+                return ParseData(page);
             }
             return String.Empty;
         }
+
+        public string ParseData(string data)
+        {
+            string step1 = WiseCrack(data);
+            Match m = Regex.Match(step1, @"\}\('(?<p>.+?)[^\\]',(?<a>[^,]*),(?<c>[^,]*),[^']*'(?<k>\|[^']*)'.split\('\|'\),(?<e>[^,]*),(?<d>[^\)]*)\)");
+            if (m.Success)
+            {
+                data = Unpack(m.Groups["p"].Value, Int32.Parse(m.Groups["a"].Value), Int32.Parse(m.Groups["c"].Value), m.Groups["k"].Value.Split('|'),
+                    Int32.Parse(m.Groups["e"].Value), m.Groups["d"].Value);
+            }
+
+
+            m = Regex.Match(data, @"flashvars\.file=""(?<fileid>[^""]*)"";\s*flashvars\.filekey=(?<filekey>[^;]*);");
+            if (m.Success)
+            {
+                string fileKey = m.Groups["filekey"].Value;
+                string fileId = m.Groups["fileid"].Value;
+                while (m.Success && !fileKey.Contains("."))
+                {
+                    m = Regex.Match(data, String.Format(@"var\s{0}=(?<newval>[^;]*);", fileKey));
+                    if (m.Success)
+                        fileKey = m.Groups["newval"].Value;
+                }
+                fileKey = fileKey.Trim('"').Replace(".", "%2E").Replace("-", "%2D");
+                data = SiteUtilBase.GetWebData(
+                    String.Format("http://www.movshare.net/api/player.api.php?pass=undefined&codes=undefined&user=undefined&file={0}&key={1}",
+                    fileId, fileKey));
+                m = Regex.Match(data, @"url=(?<url>[^&]*)&");
+                if (m.Success)
+                    return m.Groups["url"].Value;
+            }
+            return String.Empty;
+        }
+
     }
 }
---
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/MyHosterBase.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using OnlineVideos.Hoster.Base;
+
+namespace OnlineVideos.Hoster
+{
+    public abstract class MyHosterBase : HosterBase
+    {
+        protected string WiseCrack(string webdata)
+        {
+            Match m = Regex.Match(webdata, @"\('(?<w>[^']{5,})','(?<i>[^']{5,})','(?<s>[^']{5,})'");
+            while (m.Success)
+            {
+                webdata = UnWise(m.Groups["w"].Value, m.Groups["i"].Value, m.Groups["s"].Value);
+                if (webdata.Contains("flashvars"))
+                    return webdata;
+                string s = WiseCrack(webdata);
+                if (!String.IsNullOrEmpty(s))
+                    return s;
+
+                m = m.NextMatch();
+            }
+            return null;
+        }
+
+        private int FromBase36(char c)
+        {
+            if (c >= '0' && c <= '9')
+                return (int)c - 0x30;
+            if (c >= 'a' && c <= 'z')
+                return (int)c - 0x60 + 9;
+            return -1;
+
+        }
+        private int FromBase36(string s)
+        {
+            int result = 0;
+            for (int i = 0; i < s.Length; i++)
+                result = result * 36 + FromBase36(s[i]);
+            return result;
+        }
+
+        private string UnWise(string w, string i, string s)
+        {
+            StringBuilder arr1 = new StringBuilder();
+            StringBuilder arr2 = new StringBuilder();
+            int v1 = 0;
+            int v2 = 0;
+            int v3 = 0;
+            int endLength = w.Length + i.Length + s.Length;
+
+            while (endLength != arr1.Length + arr2.Length)
+            {
+                if (v1 < 5) arr2.Append(w[v1]);
+                else
+                    if (v1 < w.Length)
+                        arr1.Append(w[v1]);
+                v1++;
+
+                if (v2 < 5) arr2.Append(i[v2]);
+                else
+                    if (v2 < i.Length)
+                        arr1.Append(i[v2]);
+                v2++;
+                if (v3 < 5) arr2.Append(s[v3]);
+                else
+                    if (v3 < s.Length)
+                        arr1.Append(s[v3]);
+                v3++;
+            }
+
+            v2 = 0;
+            string arr1S = arr1.ToString();
+            string res = "";
+            for (v1 = 0; v1 < arr1S.Length; v1 += 2)
+            {
+                int tt = -1;
+                if (arr2[v2] % 2 == 1)
+                    tt = 1;
+                int b = FromBase36(arr1S.Substring(v1, 2)) - tt;
+                res += Convert.ToChar(b);
+                v2++;
+                if (v2 >= arr2.Length) v2 = 0;
+            }
+            return res;
+        }
+
+        private string ToBase36(int i)
+        {
+            string chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+            string res = "";
+            do
+            {
+                res += chars[i % 36];
+                i = i / 36;
+            } while (i > 0);
+            return res;
+        }
+
+        private string ToBase(int c, int a)
+        {
+            string res = (c < a ? "" : ToBase(c / a, a)) + ((c % a) > 35 ? ((char)(c % a + 29)).ToString() : ToBase36(c % a));
+            return res;
+        }
+
+        protected string Unpack(string p, int a, int c, string[] k, int e, string d)
+        {
+            for (int i = c - 1; i >= 0; i--)
+                if (i < k.Length && !String.IsNullOrEmpty(k[i]))
+                    p = Regex.Replace(p, @"\b" + ToBase(i, a) + @"\b", k[i]);
+            return p;
+        }
+
+
+    }
+}
