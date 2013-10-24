bce5e57472b7af8e5a696512504722f0e69371ed
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-10-24 21:09:55 +0000

    Fixed Movshare, refactor last 2 commits

--- trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/DivxStage.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/DivxStage.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Text;
 using OnlineVideos.Hoster.Base;
 using OnlineVideos.Sites;
 using System.Text.RegularExpressions;
 
 namespace OnlineVideos.Hoster
 {
-    public class DivxStage : HosterBase
+    public class DivxStage : MyHosterBase
     {
         public override string getHosterUrl()
         {
@@ -19,7 +18,7 @@ namespace OnlineVideos.Hoster
             if (!string.IsNullOrEmpty(page))
             {
                 //Even newer:
-                string newMethod = Dumbify(page);
+                string newMethod = WiseCrack(page);
                 if (!String.IsNullOrEmpty(newMethod))
                     page = newMethod;
 
@@ -63,86 +62,6 @@ namespace OnlineVideos.Hoster
             }
             return String.Empty;
         }
-
-        private string Dumbify(string webdata)
-        {
-            Match m = Regex.Match(webdata, @"\('(?<w>[^']{5,})','(?<i>[^']{5,})','(?<s>[^']{5,})'");
-            while (m.Success)
-            {
-                webdata = Dumb(m.Groups["w"].Value, m.Groups["i"].Value, m.Groups["s"].Value);
-                if (webdata.Contains("flashvars"))
-                    return webdata;
-                string s = Dumbify(webdata);
-                if (!String.IsNullOrEmpty(s))
-                    return s;
-
-                m = m.NextMatch();
-            }
-            return null;
-        }
-
-        private int FromBase36(char c)
-        {
-            if (c >= '0' && c <= '9')
-                return (int)c - 0x30;
-            if (c >= 'a' && c <= 'z')
-                return (int)c - 0x60 + 9;
-            return -1;
-
-        }
-        private int FromBase36(string s)
-        {
-            int result = 0;
-            for (int i = 0; i < s.Length; i++)
-                result = result * 36 + FromBase36(s[i]);
-            return result;
-        }
-
-        private string Dumb(string w, string i, string s)
-        {
-            StringBuilder arr1 = new StringBuilder();
-            StringBuilder arr2 = new StringBuilder();
-            int v1 = 0;
-            int v2 = 0;
-            int v3 = 0;
-            int endLength = w.Length + i.Length + s.Length;
-
-            while (endLength != arr1.Length + arr2.Length)
-            {
-                if (v1 < 5) arr2.Append(w[v1]);
-                else
-                    if (v1 < w.Length)
-                        arr1.Append(w[v1]);
-                v1++;
-
-                if (v2 < 5) arr2.Append(i[v2]);
-                else
-                    if (v2 < i.Length)
-                        arr1.Append(i[v2]);
-                v2++;
-                if (v3 < 5) arr2.Append(s[v3]);
-                else
-                    if (v3 < s.Length)
-                        arr1.Append(s[v3]);
-                v3++;
-            }
-
-            v2 = 0;
-            string arr1S = arr1.ToString();
-            string res = "";
-            for (v1 = 0; v1 < arr1S.Length; v1 += 2)
-            {
-                int tt = -1;
-                if (arr2[v2] % 2 == 1)
-                    tt = 1;
-                int b = FromBase36(arr1S.Substring(v1, 2)) - tt;
-                res += Convert.ToChar(b);
-                v2++;
-                if (v2 >= arr2.Length) v2 = 0;
-            }
-            return res;
-        }
-
 
     }
 
