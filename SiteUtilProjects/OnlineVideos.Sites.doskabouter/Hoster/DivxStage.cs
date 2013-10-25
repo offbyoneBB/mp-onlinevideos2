8cb5a558f2e2cf9673bed1313642cb07bf6f66bd
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2013-10-25 20:13:20 +0000

    Fixed NovaMov

--- SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/DivxStage.cs
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/DivxStage.cs
@@ -23,35 +23,21 @@ namespace OnlineVideos.Hoster
                     page = newMethod;
 
                 //new method:
-                Match m = Regex.Match(page, @"flashvars\.domain=""(?<domain>[^""]*)"";\s*flashvars\.file=""(?<file>[^""]*)"";\s*flashvars\.filekey=""(?<filekey>[^""]*)"";");
-                if (m.Success)
+                string link = FlashProvider(page);
+                if (!String.IsNullOrEmpty(link))
                 {
-                    string fileKey = m.Groups["filekey"].Value.Replace(".", "%2E").Replace("-", "%2D");
-                    string url2 = String.Format(@"{0}/api/player.api.php?key={1}&user=undefined&codes=1&pass=undefined&file={2}",
-                        m.Groups["domain"].Value, fileKey, m.Groups["file"].Value);
-                    page = SiteUtilBase.GetWebData(url2);
-                    m = Regex.Match(page, @"url=(?<url>[^&]*)&");
-                    if (m.Success)
-                        return m.Groups["url"].Value;
+                    videoType = VideoType.flv;
+                    return link;
                 }
+
                 //divx
-                string link = DivxProvider(url, page);
+                link = DivxProvider(url, page);
                 if (!string.IsNullOrEmpty(link))
                 {
                     videoType = VideoType.divx;
                     return link;
                 }
-                //flv
-                link = FlashProvider(url, page);
-                if (!string.IsNullOrEmpty(link))
-                {
-                    int index = link.IndexOf(".flv", StringComparison.CurrentCultureIgnoreCase);
-                    if (index >= 0)
-                        link = link.Substring(0, index + 4);
 
-                    videoType = VideoType.flv;
-                    return link;
-                }
                 //other
                 Match n = Regex.Match(page, @"src""\svalue=""(?<url>.*?)""");
                 if (n.Success)
--- SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/NovaMov.cs
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/Hoster/NovaMov.cs
@@ -1,11 +1,11 @@
-﻿using OnlineVideos.Hoster.Base;
+﻿using System;
+using OnlineVideos.Hoster.Base;
 using OnlineVideos.Sites;
-using System.Text.RegularExpressions;
 using System.Web;
 
 namespace OnlineVideos.Hoster
 {
-    public class NovaMov : HosterBase
+    public class NovaMov : MyHosterBase
     {
         public override string getHosterUrl()
         {
@@ -15,22 +15,22 @@ namespace OnlineVideos.Hoster
         public override string getVideoUrls(string url)
         {
             string page = SiteUtilBase.GetWebData(url);
+            return ParseData(page);
+        }
 
-            Match n = Regex.Match(page, @"flashvars.file=""(?<file>[^""]+)"";\s*flashvars.filekey=""(?<key>[^""]+)"";\s*flashvars.advURL=""[^""]*""");
-            if (n.Success)
+        public string ParseData(string webData)
+        {
+            string step1 = WiseCrack(webData);
+
+            string link = HttpUtility.UrlDecode(FlashProvider(step1));
+            if (!String.IsNullOrEmpty(link))
             {
-                string tmpUrl = string.Format(@"http://www.novamov.com/api/player.api.php?key={0}&user=undefined&codes={1}&file={2}&pass=undefined",
-                    HttpUtility.UrlEncode(n.Groups["key"].Value),
-                    n.Groups["id"].Value,
-                    n.Groups["file"].Value);
-                page = SiteUtilBase.GetWebData(tmpUrl);
-                n = Regex.Match(page, @"url=(?<url>.+?)&\w+=");
-                if (n.Success && Utils.IsValidUri(n.Groups["url"].Value))
-                    return n.Groups["url"].Value;
+                videoType = VideoType.flv;
+                return link;
             }
 
-            videoType = VideoType.flv;
-            return FlashProvider(url, page);
+            return String.Empty;
+
         }
     }
 }
