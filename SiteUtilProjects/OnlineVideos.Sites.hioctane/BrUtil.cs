3711fb213efcf964f8773688e8f4c70716e0da08
bborgsdorf <bborgsdorf@gmail.com>
2013-12-22 20:26:14 +0000

    fixed BR

--- SiteUtilProjects/OnlineVideos.Sites.hioctane/BrUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.hioctane/BrUtil.cs
@@ -1,74 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
-using System.Text.RegularExpressions;
-using System.Xml;
-using Ionic.Zip;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
 namespace OnlineVideos.Sites
 {
-    public class BrUtil : SiteUtilBase
+    public class BrUtil : GenericSiteUtil
     {
-		XmlDocument masterXmlDoc;
-
-		public override int DiscoverDynamicCategories()
-		{
-			Settings.Categories.Clear();
-
-			string js = GetWebData("http://mediathek-video.br.de/js/config.js");
-			string baseUrl = Regex.Match(js, @"http://.*/archive/archive\.xml\.zip\.adler32").Value;
-			WebClient Client = new WebClient();
-			byte[] downloadBuffer = Client.DownloadData(baseUrl);
-			ZipFile zipFile = ZipFile.Read(downloadBuffer);
-			ZipEntry zipEntry = zipFile[0];
-			masterXmlDoc = new XmlDocument();
-			using (MemoryStream ms = new MemoryStream())
-			{
-				zipEntry.Extract(ms);
-				ms.Position = 0;
-				masterXmlDoc.Load(ms);
-			}
-			foreach (XmlElement sendung in masterXmlDoc.DocumentElement.SelectNodes("../archiv/sendungen/sendung"))
-			{
-				RssLink cat = new RssLink() { Name = sendung.GetAttribute("name"), Thumb = sendung.GetAttribute("bild"), Url = sendung.GetAttribute("id") };
-				if (!String.IsNullOrEmpty(cat.Name) && !String.IsNullOrEmpty(cat.Url))
-				{
-					cat.EstimatedVideoCount = (uint)masterXmlDoc.DocumentElement.SelectNodes("../archiv/ausstrahlungen/ausstrahlung[sendung='" + cat.Url + "' and count(videos/video)>0]").Count;
-					if (cat.EstimatedVideoCount > 0)
-					{
-						Settings.Categories.Add(cat);
-					}
-				}
-			}
-
-			Settings.DynamicCategoriesDiscovered = Settings.Categories.Count > 0;
-			return Settings.Categories.Count;
-		}
-
-        public override List<VideoInfo> getVideoList(Category category)
+        public override int DiscoverDynamicCategories()
         {
-            List<VideoInfo> videos = new List<VideoInfo>();
-            foreach (XmlNode ausstrahlung in masterXmlDoc.DocumentElement.SelectNodes("../archiv/ausstrahlungen/ausstrahlung[sendung='" + ((RssLink)category).Url + "' and count(videos/video)>0]"))
+            var result = base.DiscoverDynamicCategories();
+            foreach (RssLink cat in Settings.Categories)
+                cat.Url = "http://www.br.de/mediathek/video/suche/suche-104.html?broadcast=" + HttpUtility.UrlEncode(cat.Name) + "&period=year";
+            return result;
+        }
+
+        public override Dictionary<string, string> GetPlaybackOptions(string playlistUrl)
+        {
+            var result = new Dictionary<string, string>();
+            var xdoc = GetWebData<System.Xml.Linq.XDocument>(playlistUrl);
+            foreach (var asset in xdoc.Descendants("asset"))
             {
-                if ((category as RssLink).Url.CompareTo(ausstrahlung.SelectSingleNode("sendung").InnerText) == 0)
+                if (!asset.Attribute("type").Value.StartsWith("MOBILE"))
                 {
-                    VideoInfo video = new VideoInfo();
-					DateTime airdate = DateTime.MinValue;
-					if (ausstrahlung.SelectSingleNode("beginnPlan") != null) if (DateTime.TryParse(ausstrahlung.SelectSingleNode("beginnPlan").InnerText, out airdate)) video.Airdate = airdate.ToString("g", OnlineVideoSettings.Instance.Locale);
-                    video.Title = ausstrahlung.SelectSingleNode("titel").InnerText;
-					string titleAppend = ausstrahlung.SelectSingleNode("nebentitel").InnerText;
-					if (string.IsNullOrEmpty(titleAppend) && airdate != DateTime.MinValue) titleAppend = airdate.ToShortDateString();
-					if (!string.IsNullOrEmpty(titleAppend)) video.Title += " - " + titleAppend;
-                    if(ausstrahlung.SelectSingleNode("bild") != null)video.ImageUrl = ausstrahlung.SelectSingleNode("bild").InnerText;
-                    video.Description = ausstrahlung.SelectSingleNode("beschreibung").InnerText;
-                    if (string.IsNullOrEmpty(video.Description)) video.Description = ausstrahlung.SelectSingleNode("kurzbeschreibung").InnerText;
-					video.VideoUrl = ausstrahlung.SelectSingleNode("hdslink").InnerText + "?hdcore=2.11.3&g=AAAAAAAAAAAA";
-                    if(!string.IsNullOrEmpty(video.VideoUrl))
-                        videos.Add(video);
+                    var info = string.Format("{0} ({1})", asset.Descendants("dimensions").First().Value, asset.Descendants("readableSize").First().Value);
+                    var url = asset.Descendants("downloadUrl").First().Value;
+                    result.Add(info, url);
                 }
             }
-            return videos;
+            return result;
         }
 
     }
