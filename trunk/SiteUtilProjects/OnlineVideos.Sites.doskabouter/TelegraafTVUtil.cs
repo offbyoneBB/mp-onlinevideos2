3b0825342c779d38608faa29ef868f90c05d2919
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2014-01-09 21:50:51 +0000

    Added TelegraafTV

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/TelegraafTVUtil.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace OnlineVideos.Sites
+{
+    public class TelegraafTVUtil : BrightCoveUtil
+    {
+        protected override List<VideoInfo> Parse(string url, string data)
+        {
+            int p = url.LastIndexOf('=');
+            int currPage;
+            nextPageAvailable = false;
+            if (int.TryParse(url.Substring(p + 1), out currPage))
+            {
+                nextPageAvailable = currPage < 10;
+                nextPageUrl = url.Substring(0, p + 1) + (currPage + 1).ToString();
+            }
+
+            return base.Parse(url, data);
+        }
+    }
+}
