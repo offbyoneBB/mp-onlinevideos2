1790d319e00ef4457da246e5b5e1232b19184cac
bborgsdorf@gmail.com <bborgsdorf@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2014-02-08 17:08:46 +0000

    added missing files

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.DavidCalder/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OnlineVideos.Sites.DavidCalder")]
+[assembly: AssemblyDescription("SiteUtils for OnlineVideos by DavidCalder")]
+[assembly: AssemblyConfiguration("OnlineVideos")]
+[assembly: AssemblyProduct("OnlineVideos.Sites.DavidCalder")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ffdb5e4e-28d1-4bcc-9fb2-ebb363c76a3a")]
+
---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.DavidCalder/WatchOnlineSeriesUtil.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using System.Text.RegularExpressions;
+
+namespace OnlineVideos.Sites.DavidCalder
+{
+    public class WatchOnlineSeriesUtil : DeferredResolveUtil
+    {
+
+        public override List<VideoInfo> getVideoList(Category category)
+        {
+            List<VideoInfo> vidInfo = new List<VideoInfo>(base.getVideoList(category));
+
+            foreach (VideoInfo vid in vidInfo)
+            {
+                string newName = vid.Title.Replace(".", "");
+                vid.Title = string.Format("{0} – {1} – TBA", category.ParentCategory.Name, newName);
+            }
+            return vidInfo;
+        }
+
+        public override string FormatHosterUrl(string name)
+        {
+            string videoUrl = name;
+            if (videoUrl.Contains("adf.ly/254271/"))
+            {
+                videoUrl = videoUrl.Replace("adf.ly/254271/", "");
+            }
+            if (!videoUrl.Contains("www."))
+            {
+                videoUrl = videoUrl.Replace("http://", "http://www.");
+            }
+            return videoUrl;
+        }
+
+
+        public override List<ISearchResultItem> DoSearch(string query)
+        {
+            List < ISearchResultItem > searchResults = new List<ISearchResultItem>(base.DoSearch(query.Replace(" ", "-")));
+            
+            return searchResults;
+        }
+
+    }
+
+   
+}
