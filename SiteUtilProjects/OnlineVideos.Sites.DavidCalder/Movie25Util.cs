962deb07a5c6f29630e1b84113a9de41479cc836
Boogles21 <Boogles21@gmail.com>
2014-04-15 10:16:18 +0000

    Added search to Movie25

--- SiteUtilProjects/OnlineVideos.Sites.DavidCalder/Movie25Util.cs
+++ SiteUtilProjects/OnlineVideos.Sites.DavidCalder/Movie25Util.cs
@@ -42,6 +42,40 @@ namespace OnlineVideos.Sites.DavidCalder
       return GetVideoUrl(newUrl);
     }
 
+    public override bool CanSearch
+    {
+      get
+      {
+        return true;
+      }
+    }
+    public override List<ISearchResultItem> DoSearch(string query)
+    {
+      List<ISearchResultItem> cats = new List<ISearchResultItem>();
+
+      Regex r = new Regex(@"<div\sclass=""movie_pic""><a\shref=""(?<url>[^""]*)""\starget=""_blank"">\s*<img\ssrc=""(?<thumb>[^""]*)""\swidth=""101""\sheight=""150""\s/>\s*</a></div>\s*<div\sclass=""movie_about"">\s*<div\sclass=""movie_about_text"">\s*<h1><a\shref=""[^""]*""\starget=""_blank"">(?<title>[^<]*)</a></h1>", defaultRegexOptions);
+
+      query = query.Replace(" ", "+");
+      string webData = GetWebData(string.Format(baseUrl + "/search.php?key={0}&submit=", query), forceUTF8: true);
+      Match m = r.Match(webData);
+      while (m.Success)
+      {
+        RssLink cat = new RssLink();
+        cat.Url = baseUrl + m.Groups["url"].Value;
+        if (!string.IsNullOrEmpty(dynamicSubCategoryUrlFormatString)) cat.Url = string.Format(dynamicSubCategoryUrlFormatString, cat.Url);
+        cat.Url = ApplyUrlDecoding(cat.Url, dynamicSubCategoryUrlDecoding);
+        if (!Uri.IsWellFormedUriString(cat.Url, System.UriKind.Absolute)) cat.Url = new Uri(new Uri(baseUrl), cat.Url).AbsoluteUri;
+        cat.Name = m.Groups["title"].Value.Trim();
+        cat.Thumb = m.Groups["thumb"].Value;
+        if (!String.IsNullOrEmpty(cat.Thumb) && !Uri.IsWellFormedUriString(cat.Thumb, System.UriKind.Absolute)) cat.Thumb = new Uri(new Uri(baseUrl), cat.Thumb).AbsoluteUri;
+        cat.Description = m.Groups["description"].Value;
+        cat.HasSubCategories = false;
+        cats.Add(cat);
+        m = m.NextMatch();
+      }
+      return cats;
+    }
+
   }
 
 }
