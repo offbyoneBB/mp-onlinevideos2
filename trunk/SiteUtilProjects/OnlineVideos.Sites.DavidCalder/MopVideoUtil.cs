f06ae9280f3c67a1e35c92446c32f6ec90c40702
Boogles21@gmail.com <Boogles21@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2014-04-16 04:00:59 +0000

    Updated MopVideoUtil

--- trunk/SiteUtilProjects/OnlineVideos.Sites.DavidCalder/MopVideoUtil.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.DavidCalder/MopVideoUtil.cs
@@ -1,10 +1,9 @@
- using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
-
 
 namespace OnlineVideos.Sites.DavidCalder
 {
