89bcdea8ee45471d5a8486bce2dbffafb52b8c51
ministerkrister@gmail.com <ministerkrister@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2014-04-21 09:57:14 +0000

    New site(s) HBO Nordic.

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.ministerk/BrowserUtilConnectors/HboNordicConnector.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OnlineVideos.Sites.Base;
+using OnlineVideos.Sites.Entities;
+using System.Windows.Forms;
+using System.Threading;
+using OnlineVideos.Helpers;
+using System.Diagnostics;
+using OnlineVideos.Sites.Utils;
+
+namespace OnlineVideos.Sites.BrowserUtilConnectors
+{
+    public class HboNordicConnector : BrowserUtilConnector
+    {
+        protected enum State
+        {
+            None,
+            LoadLogIn,
+            LogIn,
+            DoLogIn,
+            Play,
+            Playing
+        }
+        bool debug = false;
+        protected bool _isPlayingOrPausing = false;
+        protected bool _hdEnabled;
+        protected bool _doEnableHd;
+        protected State _currentState = State.None;
+
+        public const string baseUrl = "http://hbonordic.com";
+        private string _password;
+        private string _username;
+
+
+        public string LoginUrl
+        {
+            get { return baseUrl + "/home"; }
+        }
+
+        public override EventResult PerformLogin(string username, string password)
+        {
+            _password = password;
+            _username = username;
+            _currentState = State.None;
+            Url = LoginUrl;
+            ProcessComplete.Finished = true;
+            ProcessComplete.Success = true;
+            return EventResult.Complete();
+        }
+
+        public override EventResult PlayVideo(string videoToPlay)
+        {
+            _doEnableHd = videoToPlay.Contains("DOENABLEHD");
+            _hdEnabled = false;
+            videoToPlay = videoToPlay.Replace("DOENABLEHD", "");
+            ProcessComplete.Finished = true;
+            ProcessComplete.Success = true;
+            Url = baseUrl + videoToPlay;
+            _currentState = State.LoadLogIn;
+            return EventResult.Complete();
+        }
+
+        public override EventResult Play()
+        {
+
+            return PlayPause();
+        }
+
+        public override EventResult Pause()
+        {
+            return PlayPause();
+        }
+
+        private EventResult PlayPause()
+        {
+            if (_currentState != State.Playing || _isPlayingOrPausing || Browser.Document == null || Browser.Document.Body == null) return EventResult.Complete();
+            _isPlayingOrPausing = true;
+            int x = Browser.FindForm().Left + 30;
+            if (_doEnableHd && !_hdEnabled)
+            {
+                x = Browser.FindForm().Right - 235;
+                _hdEnabled = true;
+            }
+            var y = Browser.FindForm().Bottom - 10;
+            // Need to move the cursor a lot, slow animations.
+            Cursor.Position = new System.Drawing.Point(x, Browser.FindForm().Bottom - 200);
+            Application.DoEvents();
+            // We have to move the cursor to show the play/pause button
+            while (Cursor.Position.Y < y)
+            {
+                Cursor.Position = new System.Drawing.Point(x, Cursor.Position.Y + 2);
+                Application.DoEvents();
+            }
+            Cursor.Position = new System.Drawing.Point(x, y);
+            Application.DoEvents();
+            CursorHelper.DoLeftMouseClick();
+            Application.DoEvents();
+            _isPlayingOrPausing = false;
+            return EventResult.Complete();
+        }
+
+        private void runJs()
+        {
+            var js = "var tryToPlayTimer;";
+            js += "var playArray = ['Play','Toista'];";
+            js += "function fn(){";
+            js += @"if (playArray.indexOf(document.elementFromPoint(window.innerWidth/2,342).innerText.replace(/^\s+|\s+$/g, '')) === -1 && playArray.indexOf(document.elementFromPoint(window.innerWidth/2,322).innerText.replace(/^\s+|\s+$/g, '')) === -1) return;";
+            js += "clearInterval(tryToPlayTimer);";
+            js += "setTimeout(\"fn2()\",750);";
+            js += "};";
+            js += "function fn2(){";
+            js += "var i342 = document.elementFromPoint(window.innerWidth/2,342);";
+            js += @"if (playArray.indexOf(i342.innerText.replace(/^\s+|\s+$/g, '')) > -1){";
+            js += "i342.click();";
+            js += "} else {";
+            js += "document.elementFromPoint(window.innerWidth/2,322).click();";
+            js += "};";
+            js += "setTimeout(\"fn3()\",500);";
+            js += "};";
+            js += "function fn3(){";
+            js += "var fset = document.getElementsByTagName('fieldset');";
+            js += "if (fset.length > 0){";
+            js += "fset[0].querySelectorAll('input[type=email]')[0].value = '" + _username + "';";
+            js += "fset[0].querySelectorAll('input[type=password]')[0].value = '" + _password + "';";
+            js += "fset[0].querySelectorAll('input[type=submit]')[0].click();";
+            js += "} else {";
+            js += "document.getElementsByTagName('html')[0].style.overflow = 'hidden';";
+            js += "var container = document.getElementsByClassName('hbo_video_container')[0];";
+            js += "container.style.height = window.innerHeight + 'px';";
+            js += "container.className = \"\";";
+            js += "container.className = \"js-hbo_video_container hbo_video_container fixed topleft full_width clip_container row z_9\";";
+            js += "var obj = document.getElementsByTagName('object')[0];";
+            js += "obj.style.height = window.innerHeight + 'px';";
+            js += "obj.style.width = window.innerWidth + 'px';";
+            js += "obj.height = window.innerHeight + 'px';";
+            js += "obj.width = window.innerWidth + 'px';";
+            js += "}};";
+            js += "$(document).ready(function() {tryToPlayTimer = setInterval(\"fn()\",250);});";
+            InvokeScript(js);
+        }
+
+        public override EventResult BrowserDocumentComplete()
+        {
+            if (debug)
+            {
+                InvokeScript("alert('Debug Time!')");
+                debug = false;
+            }
+            switch (_currentState)
+            {
+                case State.LoadLogIn:
+                    ProcessComplete.Finished = true;
+                    ProcessComplete.Success = true;
+                    _currentState = State.LogIn;
+                    break;
+                case State.LogIn:
+                    runJs();
+                    ProcessComplete.Finished = true;
+                    ProcessComplete.Success = true;
+                    _currentState = State.DoLogIn;
+                    break;
+                case State.DoLogIn:
+                    ProcessComplete.Finished = true;
+                    ProcessComplete.Success = true;
+                    _currentState = State.Play;
+                    break;
+                case State.Play:
+                    runJs();
+                    _currentState = State.Playing;
+                    ProcessComplete.Finished = true;
+                    ProcessComplete.Success = true;
+                    break;
+                case State.Playing:
+                    ProcessComplete.Finished = true;
+                    ProcessComplete.Success = true;
+                    break;
+            }
+            return EventResult.Complete();
+        }
+    }
+}
---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.ministerk/HboNordicWebUtil.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Collections.Specialized;
+using System.Net;
+using OnlineVideos.Sites.Utils;
+
+namespace OnlineVideos.Sites
+{
+    public class HboNordicWebUtil : SiteUtilBase, IBrowserSiteUtil
+    {
+        #region Config
+
+        [Category("OnlineVideosUserConfiguration"), LocalizableDisplayName("E-mail"), Description("HBO Nordic username e-mail")]
+        protected string username = null;
+        [Category("OnlineVideosUserConfiguration"), LocalizableDisplayName("Password"), Description("HBO Nordic password")]
+        protected string password = null;
+        [Category("OnlineVideosUserConfiguration"), LocalizableDisplayName("In English please"), Description("Get titles and descriptions in english (does not affect subtitles).")]
+        protected bool useEnglish = false;
+        [Category("OnlineVideosUserConfiguration"), LocalizableDisplayName("Enable HD on 1st play/pause"), Description("(Try to) Enable HD the first time play/pause is pressed when playing video, let the video play for 20-30 seconds first.")]
+        protected bool enableHd = false;
+        [Category("OnlineVideosUserConfiguration"), LocalizableDisplayName("Show forum category"), Description("Enable or disable forum category (Link to forum - http://tinyurl.com/olv-hbonordic)")]
+        protected bool showHelpCategory = true;
+
+        #endregion
+
+        #region Vars
+
+        private const string baseUrl = "http://hbonordic.com/rest-services-hook";
+        private const string moviesUrl = "/thin/movies";
+        private const string seriesUrl = "/series";
+
+        #endregion
+
+        #region BrowserSiteUtil
+
+        public string UserName
+        {
+            get
+            {
+                return username;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+        }
+
+        public string ConnectorEntityTypeName
+        {
+            get
+            {
+                return "OnlineVideos.Sites.BrowserUtilConnectors.HboNordicConnector";
+            }
+        }
+
+        #endregion
+
+        #region Localization
+
+        protected string SeriesCategoryName
+        {
+            get
+            {
+                string series = "Series";
+                if (!useEnglish)
+                {
+                    switch (Settings.Language)
+                    {
+                        case "sv":
+                            series = "Serier";
+                            break;
+                        case "da":
+                            series = "Serier";
+                            break;
+                        case "fi":
+                            series = "TV-Sarjat";
+                            break;
+                        case "no":
+                            series = "Serier";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                return series;
+            }
+        }
+
+        protected string SeasonCategoryNamePrefix
+        {
+            get
+            {
+                string series = "Season";
+                if (!useEnglish)
+                {
+                    switch (Settings.Language)
+                    {
+                        case "sv":
+                            series = "Säsong";
+                            break;
+                        case "da":
+                            series = "Sæson";
+                            break;
+                        case "fi":
+                            series = "Kausi";
+                            break;
+                        case "no":
+                            series = "Sesong";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                return series;
+            }
+        }
+
+        protected string MoviesCategoryName
+        {
+            get
+            {
+                string movies = "Movies";
+                if (!useEnglish)
+                {
+                    switch (Settings.Language)
+                    {
+                        case "sv":
+                            movies = "Film";
+                            break;
+                        case "da":
+                            movies = "Film";
+                            break;
+                        case "fi":
+                            movies = "Elokuvat";
+                            break;
+                        case "no":
+                            movies = "Filmer";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                return movies;
+            }
+        }
+
+        protected string Language
+        {
+            get
+            {
+                string lang = "en_US";
+                if (!useEnglish)
+                {
+                    switch (Settings.Language)
+                    {
+                        case "sv":
+                            lang = "sv_SE";
+                            break;
+                        case "da":
+                            lang = "da_DK";
+                            break;
+                        case "fi":
+                            lang = "fi_FI";
+                            break;
+                        case "no":
+                            lang = "nb_NO";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                return lang;
+            }
+        }
+
+        #endregion
+
+        #region Helpers
+
+        protected T MyGetWebData<T>(string url)
+        {
+            CookieContainer cc = new CookieContainer();
+            CookieCollection ccol = new CookieCollection();
+            Cookie langcookie = new Cookie("GUEST_LANGUAGE_ID", Language, "/", "hbonordic.com");
+            cc.Add(langcookie);
+            return GetWebData<T>(url, cc);
+        }
+
+        #endregion
+
+        #region SiteUtil
+
+        #region Category
+
+        public override int DiscoverDynamicCategories()
+        {
+            Settings.Categories.Clear();
+
+            RssLink seriesCat = new RssLink()
+            {
+                Name = SeriesCategoryName,
+                HasSubCategories = true,
+                SubCategories = new List<Category>()
+            };
+
+            string data = MyGetWebData<string>(baseUrl + seriesUrl);
+            JArray series = (JArray)JsonConvert.DeserializeObject(data);
+            foreach (JToken serie in series)
+            {
+                seriesCat.SubCategories.Add(new RssLink()
+                {
+                    Name = serie["title"].Value<string>(),
+                    Thumb = serie["poster"].Value<string>(),
+                    Description = serie["info"].Value<string>(),
+                    HasSubCategories = true,
+                    SubCategories = new List<Category>(),
+                    ParentCategory = seriesCat,
+                    Url = baseUrl + seriesUrl + "/" + serie["id"].Value<string>()
+                });
+            }
+            seriesCat.EstimatedVideoCount = (uint)seriesCat.SubCategories.Count();
+            seriesCat.SubCategoriesDiscovered = seriesCat.EstimatedVideoCount > 0;
+            Settings.Categories.Add(seriesCat);
+
+            RssLink moviesCat = new RssLink()
+            {
+                Name = MoviesCategoryName,
+                HasSubCategories = false
+            };
+            Settings.Categories.Add(moviesCat);
+
+            if (showHelpCategory)
+            {
+                Settings.Categories.Add(new RssLink()
+                {
+                    Name = "Forum",
+                    Other = "Forum"
+                });
+            }
+
+            Settings.DynamicCategoriesDiscovered = Settings.Categories.Count > 0;
+            return Settings.Categories.Count();
+        }
+
+        public override int DiscoverSubCategories(Category parentCategory)
+        {
+            JObject series = MyGetWebData<JObject>((parentCategory as RssLink).Url);
+            foreach (JToken seasonInfo in series["season_info"])
+            {
+                var season = seasonInfo.First().Value<JToken>();
+                parentCategory.SubCategories.Add(new RssLink()
+                {
+                    Name = SeasonCategoryNamePrefix + " " + season["season_number"].Value<string>(),
+                    Thumb = season["season_image_url"].Value<string>(),
+                    ParentCategory = parentCategory,
+                    HasSubCategories = false,
+                    Other = new List<VideoInfo>()
+                });
+            }
+            (parentCategory as RssLink).EstimatedVideoCount = (uint)parentCategory.SubCategories.Count();
+            foreach (JToken episode in series["episodes"])
+            {
+                (parentCategory.SubCategories.First(c => c.Name == SeasonCategoryNamePrefix + " " + episode["season"].Value<string>()).Other as List<VideoInfo>).Add(new VideoInfo()
+                {
+                    Title = string.Format("{0}. {1}", episode["episode_number"].Value<string>(), episode["title"].Value<string>()),
+                    ImageUrl = episode["thumb"].Value<string>(),
+                    Description = episode["info"].Value<string>(),
+                    VideoUrl = episode["url"].Value<string>()
+                });
+            }
+            parentCategory.SubCategories.ForEach(c => (c as RssLink).EstimatedVideoCount = (uint)(c.Other as List<VideoInfo>).Count());
+            parentCategory.SubCategoriesDiscovered = parentCategory.SubCategories.Count() > 0;
+            return parentCategory.SubCategories.Count();
+        }
+        #endregion
+
+        #region Videos
+
+        public override List<VideoInfo> getVideoList(Category category)
+        {
+            if (category.Other is string && (category.Other as string) == "Forum")
+                throw new OnlineVideosException("Forum http://tinyurl.com/olv-hbonordic");
+            List<VideoInfo> videos = new List<VideoInfo>();
+            if (category.Name == MoviesCategoryName)
+            {
+                JObject movieGroups = MyGetWebData<JObject>(baseUrl + moviesUrl);
+                foreach (JToken movieGroup in movieGroups["entry"].First())
+                {
+                    foreach (JToken movie in movieGroup.First())
+                    {
+                        videos.Add(new VideoInfo()
+                        {
+                            Title = movie["title"].Value<string>(),
+                            ImageUrl = movie["thumb"].Value<string>(),
+                            Description = movie["description"].Value<string>(),
+                            VideoUrl = movie["uri"].Value<string>()
+                        });
+                    }
+                }
+                (category as RssLink).EstimatedVideoCount = (uint)videos.Count;
+            }
+            else if (category.Other != null && category.Other is List<VideoInfo>)
+            {
+                videos = category.Other as List<VideoInfo>;
+            }
+            return videos;
+        }
+
+        public override string getUrl(VideoInfo video)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || !HelperUtils.IsValidEmail(username))
+                throw new OnlineVideosException("Please enter username and password");
+            return video.VideoUrl + ((enableHd) ? "DOENABLEHD" : "");
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
