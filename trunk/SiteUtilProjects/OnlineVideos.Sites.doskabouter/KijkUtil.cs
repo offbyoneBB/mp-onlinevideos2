e6a302a8658e5a1345502bc5e8ba01faadcc0ab3
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2014-04-25 19:17:54 +0000

    Fixed kijk.nl

--- trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/KijkUtil.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/KijkUtil.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Reflection;
 using System.Text.RegularExpressions;
 using OnlineVideos.AMF;
 
@@ -10,89 +8,41 @@ namespace OnlineVideos.Sites
 
     public class KijkUtil : BrightCoveUtil
     {
-        public override int DiscoverDynamicCategories()
-        {
-            if (Settings.Categories == null) Settings.Categories = new BindingList<Category>();
-            Settings.Categories.Add(GetGemist(baseUrl));
-            return 1 + base.DiscoverDynamicCategories();
-        }
-
         public override int DiscoverSubCategories(Category parentCategory)
         {
             if (parentCategory.SubCategories == null) parentCategory.SubCategories = new List<Category>();
             Category gemist = GetGemist(((RssLink)parentCategory).Url);
             gemist.ParentCategory = parentCategory;
             parentCategory.SubCategories.Add(gemist);
-
-            int res = base.DiscoverSubCategories(parentCategory);
-            foreach (Category cat in parentCategory.SubCategories)
-                if (!cat.HasSubCategories) //skip gemist
+            string webdata = GetWebData(((RssLink)parentCategory).Url);
+            string[] parts = webdata.Split(new[] { "all-shows-a\">" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string part in parts)
+            {
+                if (!part.StartsWith("<"))
                 {
-                    cat.HasSubCategories = true;
-                    cat.SubCategories = new List<Category>();
-                    cat.SubCategories.Add(new RssLink()
+                    Category cat = new RssLink()
                     {
-                        ParentCategory = cat,
-                        HasSubCategories = false,
-                        Name = "Clips",
-                        Url = ((RssLink)cat).Url,
-                        Other = 0
-                    });
-                    cat.SubCategories.Add(new RssLink()
-                    {
-                        ParentCategory = cat,
-                        HasSubCategories = false,
-                        Name = "Afleveringen",
-                        Url = ((RssLink)cat).Url,
-                        Other = 1
-                    });
-                    cat.SubCategoriesDiscovered = true;
+                        ParentCategory = parentCategory,
+                        Name = part.Substring(0, 1),
+                        HasSubCategories = true
+                    };
+                    parentCategory.SubCategories.Add(cat);
+                    base.ParseSubCategories(cat, part);
                 }
+            }
+
             parentCategory.SubCategoriesDiscovered = true;
-            return 1 + res;
+            return parentCategory.SubCategories.Count;
         }
 
         private Category GetGemist(string url)
         {
-            string webData = GetWebData(url, forceUTF8: true);
-            string[] days = webData.Split(new[] { "<li id=" }, StringSplitOptions.RemoveEmptyEntries);
-            Category result = new Category()
+            return new RssLink()
             {
                 Name = "Gemist",
-                HasSubCategories = true,
-                SubCategoriesDiscovered = true,
-                SubCategories = new List<Category>()
+                HasSubCategories = false,
+                Url = url
             };
-            for (int i = days.Length - 1; i > 0; i--)
-            {
-                Category subcat = new Category()
-                {
-                    ParentCategory = result,
-                    HasSubCategories = false
-                };
-                int p = days[i].IndexOf('"', 1);
-                subcat.Name = days[i].Substring(1, p - 1);
-                subcat.Other = Parse(baseUrl, days[i]);
-                result.SubCategories.Add(subcat);
-
-            }
-            return result;
-        }
-
-        public override List<VideoInfo> getVideoList(Category category)
-        {
-            List<VideoInfo> res = category.Other as List<VideoInfo>;
-            if (res != null)
-                return res;
-            int? nr = category.Other as int?;
-            if (nr.HasValue)
-            {
-                string webdata = GetWebData(((RssLink)category).Url, forceUTF8: true);
-                string[] parts = webdata.Split(new[] { @"<ul class=""horizontal-scroll-holder"">" }, StringSplitOptions.RemoveEmptyEntries);
-                if (parts.Length > 2)
-                    return Parse(baseUrl, parts[nr.Value + 1]);
-            }
-            return base.getVideoList(category);
         }
 
         public override string getUrl(VideoInfo video)
