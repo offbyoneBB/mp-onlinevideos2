2d69a2abcedcb98792a55281499c4f718c96e804
jlbpyke@googlemail.com <jlbpyke@googlemail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2014-04-25 16:27:31 +0000

    Fix 4OD Web Thumbnails

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.MrP/Entities/ExtendedCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineVideos.Sites.WebAutomation.Entities
+{
+    /// <summary>
+    /// Add a sort column to category
+    /// </summary>
+    public class ExtendedCategory : Category
+    {
+        public string SortValue { get; set; }
+    }
+}
