01766d98599dc8b0de9c4fdd6f72697d43aa9d80
haroon.rafique <haroon.rafique@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2014-05-27 22:45:15 +0000

    implemented video listing for CBS Classics and Originals

--- trunk/SiteUtilProjects/OnlineVideos.Sites.corporategadfly/CBSUtil.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.corporategadfly/CBSUtil.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Xml;
 
+using HtmlAgilityPack;
 using Newtonsoft.Json.Linq;
 
 namespace OnlineVideos.Sites
 {
-    enum CBSItemType { Movie, Show, ShowCarousels, Classic, ClassicSeasons };
+    enum CBSItemType { Movie, Show, ShowCarousels, Classic, ClassicSeasons, ClassicEpisodes };
     /// <summary>
     /// Site utility for CBS.
     /// </summary>
@@ -19,6 +21,10 @@ namespace OnlineVideos.Sites
                                                            RegexOptions.Compiled);
         private static Regex showIdRegex = new Regex(@"var\sshow\s=\snew\sCBS\.Show\({id:(?<showId>[^}]*)}",
                                                      RegexOptions.Compiled);
+        private static Regex classicSeasonRegex = new Regex(@"(?<seasonInfo>.*?)<br>",
+                                                            RegexOptions.Compiled);
+        private static Regex classicLengthRegex = new Regex(@"(Clip|Episode)\s\((?<length>.*?)\)",
+                                                            RegexOptions.Compiled);
         private static Regex pidRegex = new Regex(@"video\.settings\.pid\s=\s'(?<pid>[^']*)';",
                                                   RegexOptions.Compiled);
 
@@ -26,6 +32,8 @@ namespace OnlineVideos.Sites
         private static string videoSectionUrlFormat = @"http://www.cbs.com/carousels/videosBySection/{0}/offset/0/limit/1/xs/0/";
         private static string videoListUrlFormat = @"http://www.cbs.com/carousels/videosByWindow/{0}/offset/0/limit/40/xs/0/{1}/";
         private static string thePlatformUrlFormat = @"http://link.theplatform.com/s/dJ5BDC/{0}?format=SMIL&Tracking=true&mbr=true";
+        
+        private Category currentCategory = null;
 
         public override int DiscoverDynamicCategories()
         {
@@ -68,7 +76,10 @@ namespace OnlineVideos.Sites
                             new RssLink() {
                                 ParentCategory = parentCategory,
                                 Name = (string) item["title"],
-                                Url = string.Format(@"{0}video", (string) item["link"]),
+                                Url =
+                                    itemType.Equals(CBSItemType.Classic)
+                                    ? (string) item["link"]
+                                    : string.Format(@"{0}video", (string) item["link"]),
                                 Thumb = (string) item["filepath_nav_logo"],
                                 HasSubCategories = true,
                                 Other =
@@ -81,10 +92,24 @@ namespace OnlineVideos.Sites
             }
             else if (itemType.Equals(CBSItemType.ClassicSeasons))
             {
-                string webData = GetWebData(parentRssLink.Url);
-                if (!string.IsNullOrEmpty(webData))
-                {
-                    throw new OnlineVideosException("Not implemented yet");
+                HtmlDocument document = GetWebData<HtmlDocument>(parentRssLink.Url);
+                if (document != null) {
+                    foreach (HtmlNode season in document.DocumentNode.SelectNodes(@"//div[@id = 'season-nav-wrapper']//div[@class = 'season-nav-text']"))
+                    {
+                        string seasonText = season.InnerText;
+                        if (@"Full Episodes".Equals(seasonText) || @"Clips".Equals(seasonText))
+                        {
+                            continue;
+                        }
+                        parentCategory.SubCategories.Add(
+                            new RssLink() {
+                                ParentCategory = parentCategory,
+                                Name = seasonText,
+                                Url = string.Format(@"{0}filter/?season={1}&start=0", parentRssLink.Url, seasonText.Replace(@"Season ", string.Empty)),
+                                HasSubCategories = false,
+                                Other = CBSItemType.ClassicEpisodes
+                            });
+                    }
                 }
             }
             else if (itemType.Equals(CBSItemType.ShowCarousels))
@@ -122,39 +147,116 @@ namespace OnlineVideos.Sites
         
         public override List<VideoInfo> getVideoList(Category category)
         {
+            return getVideoListForSinglePage(category, (category as RssLink).Url);
+        }
+        
+        private List<VideoInfo> getVideoListForSinglePage(Category category, string url)
+        {
             List<VideoInfo> result = new List<VideoInfo>();
+            currentCategory = category;
+            nextPageUrl = string.Empty;
 
-            JObject json = GetWebData<JObject>((category as RssLink).Url);
-            if (json != null)
+            if (category.Other != null && CBSItemType.ClassicEpisodes.Equals((CBSItemType) category.Other))
             {
-                foreach (JToken item in json["result"]["data"] as JArray)
+                string webData = GetWebDataFromPost(url, string.Empty);
+                if (!string.IsNullOrEmpty(webData))
                 {
-                    string description = string.Empty;
-                    if (!string.IsNullOrEmpty((string) item["season_number"]))
+                    JObject json = JObject.Parse(webData);
+                    if (json != null)
                     {
-                        description = string.Format(@"Season {0}", item["season_number"]);
+                        HtmlDocument document = new HtmlDocument();
+                        document.LoadHtml((string) json["html"]);
+                        
+                        foreach (HtmlNode div in document.DocumentNode.SelectNodes(@"//div[@class = 'video-content-item']"))
+                        {
+                            HtmlNode title = div.SelectSingleNode(@"./div[@class = 'video-content-title']");
+                            HtmlNode info = div.SelectSingleNode(@".//div[@class = 'video-content-info']");
+                            HtmlNode anchor = div.SelectSingleNode(@".//a[img]");
+                            
+                            string seasonInfo = string.Empty;
+                            Match seasonInfoMatch = classicSeasonRegex.Match(
+                                info.SelectSingleNode(@"./div[@class = 'video-content-season-info']").InnerHtml);
+                            if (seasonInfoMatch.Success)
+                            {
+                                seasonInfo = seasonInfoMatch.Groups["seasonInfo"].Value;
+                            }
+                            
+                            string length = string.Empty;
+                            Match lengthMatch = classicLengthRegex.Match(
+                                info.SelectSingleNode(@"./div[@class = 'video-content-duration']").InnerText);
+                            if (lengthMatch.Success)
+                            {
+                                length = lengthMatch.Groups["length"].Value;
+                            }
+
+                            result.Add(
+                                new VideoInfo() {
+                                    Title = title.InnerText,
+                                    VideoUrl = string.Format(@"{0}{1}", baseUrl, anchor.GetAttributeValue(@"href", string.Empty)),
+                                    ImageUrl = anchor.Element("img").GetAttributeValue(@"src", string.Empty),
+                                    Airdate = info.SelectSingleNode(@"./div[@class = 'video-content-air-date']").InnerText,
+                                    Description =
+                                        string.Format(@"{0}: {1}",
+                                                      seasonInfo,
+                                                      info.SelectSingleNode(@"./div[@class = 'video-content-description']").InnerText),
+                                    Length = length
+                                });
+                        }
+                        
+                        if ((bool) json["page"]["more"])
+                        {
+                            UriBuilder builder = new UriBuilder(url);
+                            NameValueCollection parameters = HttpUtility.ParseQueryString(builder.Query);
+                            parameters["start"] = Convert.ToString(json["page"]["next"]);
+                            builder.Query = parameters.ToString();
+                            nextPageUrl = builder.Uri.ToString();
+                        }
                     }
-                    if (!string.IsNullOrEmpty((string) item["episode_number"]))
+                }
+            }
+            else
+            {
+                JObject json = GetWebData<JObject>(url);
+                if (json != null)
+                {
+                    foreach (JToken item in json["result"]["data"] as JArray)
                     {
-                        description = string.Format(@"{0}, Episode {1}", description, item["episode_number"]);
+                        string description = string.Empty;
+                        if (!string.IsNullOrEmpty((string) item["season_number"]))
+                        {
+                            description = string.Format(@"Season {0}", item["season_number"]);
+                        }
+                        if (!string.IsNullOrEmpty((string) item["episode_number"]))
+                        {
+                            description = string.Format(@"{0}, Episode {1}", description, item["episode_number"]);
+                        }
+                        result.Add(new VideoInfo() {
+                                       Title = (string) item["title"],
+                                       Description =
+                                           string.IsNullOrEmpty(description) ?
+                                           (string) item["description"] :
+                                           string.Format(@"{0}: {1}", description, item["description"]),
+                                       Airdate = (string) item["airdate"],
+                                       Length = (string) item["duration"],
+                                       VideoUrl = string.Format(@"{0}{1}", baseUrl, item["url"]),
+                                       ImageUrl = (string) item["thumb"]["large"]
+                                   });
                     }
-                    result.Add(new VideoInfo() {
-                                   Title = (string) item["title"],
-                                   Description =
-                                       string.IsNullOrEmpty(description) ?
-                                       (string) item["description"] :
-                                       string.Format(@"{0}: {1}", description, item["description"]),
-                                   Airdate = (string) item["airdate"],
-                                   Length = (string) item["duration"],
-                                   VideoUrl = string.Format(@"{0}{1}", baseUrl, item["url"]),
-                                   ImageUrl = (string) item["thumb"]["large"]
-                               });
                 }
             }
 
             return result;
         }
         
+        public override bool HasNextPage {
+            get { return !string.IsNullOrEmpty(nextPageUrl); }
+        }
+        
+        public override List<VideoInfo> getNextPageVideos()
+        {
+            return getVideoListForSinglePage(currentCategory, nextPageUrl);
+        }
+
         public override string getUrl(VideoInfo video)
         {
             Log.Debug(@"video: {0}", video.Title);
