31e86a8ca5df23a4d0609c7f39bd1089498d65bf
ministerkrister@gmail.com <ministerkrister@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2014-07-16 23:31:17 +0000

    MovHuter: new util to be able to add TV series category with subcategories + TrackingInfo

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.ministerk/MovHunterUtil.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace OnlineVideos.Sites
+{
+    public class MovHunterUtil : GenericSiteUtil
+    {
+        public override int DiscoverDynamicCategories()
+        {
+            int count = base.DiscoverDynamicCategories();
+            foreach (Category cat in Settings.Categories) { cat.HasSubCategories = cat.Name == "TV Series"; }
+            return count;
+        }
+
+        public override ITrackingInfo GetTrackingInfo(VideoInfo video)
+        {
+            ITrackingInfo ti = new TrackingInfo();
+            Regex rgx = new Regex(@"(.+)S(\d+)E(\d+)");
+            uint s = 0;
+            uint e = 0;
+            Match m = rgx.Match(video.Title);
+            if (m.Success)
+            {
+                ti.VideoKind = VideoKind.TvSeries;
+                ti.Title = m.Groups[1].Value.Trim();
+                uint.TryParse(m.Groups[2].Value, out s);
+                ti.Season = s;
+                uint.TryParse(m.Groups[3].Value, out e);
+                ti.Episode = e;
+            }
+            else
+            {
+                ti.VideoKind = VideoKind.Movie;
+                rgx = new Regex(@"(.+)\((\d{4})\)");
+                m = rgx.Match(video.Title);
+                uint y = 0;
+                if (m.Success) //movies with year
+                {
+                    ti.Title = m.Groups[1].Value.Trim();
+                    uint.TryParse(m.Groups[2].Value, out y);
+                    ti.Year = y;
+                }
+                else // movie no year
+                {
+                    ti.Title = video.Title;
+                }
+            }
+            return ti;
+        }
+    }
+}
