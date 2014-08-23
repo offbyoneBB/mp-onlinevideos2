7868f3e22c49c04ceda22b5404d72c4561db60a3
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2014-08-23 20:26:21 +0000

    Added GamersNet

---
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/GamersNetUtil.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineVideos.Sites
+{
+    public class GamersNetUtil : GenericSiteUtil
+    {
+        public override int DiscoverSubCategories(Category parentCategory)
+        {
+            if (parentCategory.Name == "Recent")
+                return GetDays(parentCategory);
+            else
+                return base.DiscoverSubCategories(parentCategory);
+        }
+
+        public override List<VideoInfo> getVideoList(Category category)
+        {
+            string other = category.Other as string;
+            if (other == null)
+                return base.getVideoList(category);
+            else
+                return Parse(baseUrl, other);
+        }
+
+        private int GetDays(Category parentCat)
+        {
+            string data = GetWebData(((RssLink)parentCat).Url);
+            string[] days = data.Split(new[] { "<p><b>" }, StringSplitOptions.RemoveEmptyEntries);
+            parentCat.SubCategories = new List<Category>();
+            for (int i = 1; i < days.Length; i++)
+            {
+                int p = days[i].IndexOf('<');
+                RssLink cat = new RssLink()
+                {
+                    Name = days[i].Substring(0, p),
+                    Other = days[i]
+                };
+                parentCat.SubCategories.Add(cat);
+            }
+            parentCat.SubCategoriesDiscovered = true;
+            return parentCat.SubCategories.Count;
+        }
+    }
+}
