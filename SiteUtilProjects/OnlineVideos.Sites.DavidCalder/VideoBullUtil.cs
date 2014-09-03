d85afe4a028102dca927afd11a8d663012151c13
Boogles21 <Boogles21@gmail.com>
2014-09-03 07:48:42 +0000

    Updated to latest working copy

--- SiteUtilProjects/OnlineVideos.Sites.DavidCalder/VideoBullUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.DavidCalder/VideoBullUtil.cs
@@ -8,99 +8,110 @@ using System.Text.RegularExpressions;
 
 namespace OnlineVideos.Sites.DavidCalder
 {
-    public class VideoBullUtil : DeferredResolveUtil
+  public class VideoBullUtil : DeferredResolveUtil
+  {
+    public override ITrackingInfo GetTrackingInfo(VideoInfo video)
     {
-      public override ITrackingInfo GetTrackingInfo(VideoInfo video)
+      Log.Debug("Debug Video.Title === " + video.Title);
+      try
       {
-        Log.Debug("Debug Video.Title === " + video.Title);
-        try
+        TrackingInfo tInfo = new TrackingInfo()
         {
-          TrackingInfo tInfo = new TrackingInfo()
-          {
-            //Grimm – Season 3 Episode 3 – A Dish Best Served Cold
-            Regex = Regex.Match(video.Title, @"(?<Title>.*)\s–\sSeason\s(?<Season>\d+)\sEpisode\s(?<Episode>\d+)", RegexOptions.IgnoreCase),
-            VideoKind = VideoKind.TvSeries
-          };
-          Match match = Regex.Match(video.Title, @"(?<Title>.*)\s–\sSeason\s(?<Season>\d+)\sEpisode\s(?<Episode>\d+)");
-          Log.Debug("Debug regex match === " + tInfo.Title + " Season " + tInfo.Season + " Episode " + tInfo.Episode);
-        }
-        catch (Exception e)
-        {
-          Log.Warn("Error parsing TrackingInfo data: {0}", e.ToString());
-        }
-
-        return base.GetTrackingInfo(video);
+          //Grimm – Season 3 Episode 3 – A Dish Best Served Cold
+          Regex = Regex.Match(video.Title, @"(?<Title>.*)\s–\sSeason\s(?<Season>\d+)\sEpisode\s(?<Episode>\d+)", RegexOptions.IgnoreCase),
+          VideoKind = VideoKind.TvSeries
+        };
+        Match match = Regex.Match(video.Title, @"(?<Title>.*)\s–\sSeason\s(?<Season>\d+)\sEpisode\s(?<Episode>\d+)");
+        Log.Debug("Debug regex match === " + tInfo.Title + " Season " + tInfo.Season + " Episode " + tInfo.Episode);
+      }
+      catch (Exception e)
+      {
+        Log.Warn("Error parsing TrackingInfo data: {0}", e.ToString());
       }
 
-      public override VideoInfo CreateVideoInfo()
-      {
-        return new SeriesVideoInfo();
-      }
-
-      public class SeriesVideoInfo : DeferredResolveVideoInfo
-      {
-        public override string GetPlaybackOptionUrl(string url)
-        {
-          CookieContainer cc = new CookieContainer();
-          string newUrl = base.PlaybackOptions[url];
-          string stripedurl = newUrl.Substring(newUrl.IndexOf("http://videobull"));
-          string data = SiteUtilBase.GetWebData(stripedurl, cc, newUrl);
-
-          //<a href="http://hoster/ekqiej2ito9p"
-          Match n = Regex.Match(data, @"<a\shref=""(?<url>[^""]*)""\starget=""_blank"">");
-          if (n.Success)
-            return GetVideoUrl(n.Groups["url"].Value);
-
-          Match n1 = Regex.Match(data, @"<iframe\ssrc="".(?<url>[^""]*)""");
-          if (n1.Success)
-          {
-            string linkurl = n1.Groups["url"].Value.Substring(n1.Groups["url"].Value.IndexOf("linkurl="));
-            //linkurl = linkurl.Remove(linkurl.IndexOf("&linkfrom"));
-            string[] parts = linkurl.Split(new[] { "linkurl=" }, StringSplitOptions.None);
-            if (parts.Length == 2)
-            {
-              byte[] tmp = Convert.FromBase64String(parts[1]);
-              string i = Encoding.ASCII.GetString(tmp);
-              byte[] tmp2 = Convert.FromBase64String(i);
-              string i2 = Encoding.ASCII.GetString(tmp2);
-
-              return GetVideoUrl(i2);
-            }
-          }
-          return url;
-        }
-      }
-
-
-      public override int DiscoverDynamicCategories()
-      {
-        int res = base.DiscoverDynamicCategories();
-
-        foreach (Category cat in Settings.Categories)
-        {
-          if (cat.Name.Contains("Season") == true)
-          {
-            cat.Name = cat.Name.Substring(0, cat.Name.IndexOf("Season")).Trim();
-
-          }
-        }
-        return res;
-      }
-
-
-      public override int DiscoverNextPageCategories(NextPageCategory category)
-      {
-        int res = base.DiscoverNextPageCategories(category);
-
-        foreach (RssLink cat in Settings.Categories)
-        {
-          if (cat.Name.Contains("Season") == true)
-          {
-            cat.Name = cat.Name.Substring(0, cat.Name.IndexOf("Season")).Trim();
-          }
-        }
-        return res;
-      }
-
+      return base.GetTrackingInfo(video);
     }
+
+    public override List<VideoInfo> getVideoList(Category category)
+    {
+      RssLink link = category as RssLink;
+      string page = SiteUtilBase.GetWebData(link.Url);
+      Match n = Regex.Match(page, @"<a\shref=""http://videobull\.to/tv-shows/"">TV\sShows</a>&nbsp;&gt;\s<a\shref=""(?<url>[^""]*)"">");
+      if (n.Success)
+      {
+        link.Url = n.Groups["url"].Value;
+      }
+      return base.getVideoList(link);
+    }
+
+    public override VideoInfo CreateVideoInfo()
+    {
+      return new SeriesVideoInfo();
+    }
+
+    public class SeriesVideoInfo : DeferredResolveVideoInfo
+    {
+      public override string GetPlaybackOptionUrl(string url)
+      {
+        CookieContainer cc = new CookieContainer();
+        string newUrl = base.PlaybackOptions[url];
+        string stripedurl = newUrl.Substring(newUrl.IndexOf("http://videobull"));
+        string data = SiteUtilBase.GetWebData(stripedurl, cc, newUrl);
+
+        //<a href="http://hoster/ekqiej2ito9p"
+        Match n = Regex.Match(data, @"<a\shref=""(?<url>[^""]*)""[^>]*>");
+        if (n.Success)
+          return GetVideoUrl(n.Groups["url"].Value);
+
+        Match n1 = Regex.Match(data, @"<iframe\ssrc="".(?<url>[^""]*)""");
+        if (n1.Success)
+        {
+          string linkurl = n1.Groups["url"].Value.Substring(n1.Groups["url"].Value.IndexOf("linkurl="));
+          //linkurl = linkurl.Remove(linkurl.IndexOf("&linkfrom"));
+          string[] parts = linkurl.Split(new[] { "linkurl=" }, StringSplitOptions.None);
+          if (parts.Length == 2)
+          {
+            byte[] tmp = Convert.FromBase64String(parts[1]);
+            string i = Encoding.ASCII.GetString(tmp);
+            byte[] tmp2 = Convert.FromBase64String(i);
+            string i2 = Encoding.ASCII.GetString(tmp2);
+
+            return GetVideoUrl(i2);
+          }
+        }
+        return url;
+      }
+    }
+
+
+    public override int DiscoverDynamicCategories()
+    {
+      int res = base.DiscoverDynamicCategories();
+
+      foreach (Category cat in Settings.Categories)
+      {
+        if (cat.Name.Contains("Season") == true)
+        {
+          cat.Name = cat.Name.Substring(0, cat.Name.IndexOf("Season")).Trim();
+
+        }
+      }
+      return res;
+    }
+
+
+    public override int DiscoverNextPageCategories(NextPageCategory category)
+    {
+      int res = base.DiscoverNextPageCategories(category);
+
+      foreach (RssLink cat in Settings.Categories)
+      {
+        if (cat.Name.Contains("Season") == true)
+        {
+          cat.Name = cat.Name.Substring(0, cat.Name.IndexOf("Season")).Trim();
+        }
+      }
+      return res;
+    }
+  }
 }
