6bbc4ccb412065d4a86c9747801cf492664a4883
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2014-10-19 20:37:31 +0000

    Fixed kijk.nl

--- trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/KijkUtil.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/KijkUtil.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
 using OnlineVideos.AMF;
 
 namespace OnlineVideos.Sites
@@ -8,14 +9,31 @@ namespace OnlineVideos.Sites
 
     public class KijkUtil : BrightCoveUtil
     {
+
+        public override int DiscoverDynamicCategories()
+        {
+            int res = base.DiscoverDynamicCategories();
+            foreach (Category cat in Settings.Categories)
+            {
+                if (cat.SubCategories != null && cat.SubCategories.Count > 0)
+                {
+                    cat.SubCategoriesDiscovered = true;
+                    foreach (Category subcat in cat.SubCategories)
+                    {
+                        subcat.HasSubCategories = true;
+                        subcat.Other = true;
+                    }
+                }
+            }
+            return res;
+        }
+
         public override int DiscoverSubCategories(Category parentCategory)
         {
             if (parentCategory.SubCategories == null) parentCategory.SubCategories = new List<Category>();
-            Category gemist = GetGemist(((RssLink)parentCategory).Url);
-            gemist.ParentCategory = parentCategory;
-            parentCategory.SubCategories.Add(gemist);
-            string webdata = GetWebData(((RssLink)parentCategory).Url);
-            string[] parts = webdata.Split(new[] { "all-shows-a\">" }, StringSplitOptions.RemoveEmptyEntries);
+            string url = ((RssLink)parentCategory).Url;
+            string webdata = GetWebData(url);
+            string[] parts = webdata.Split(new[] { @"showcase-heading"">" }, StringSplitOptions.RemoveEmptyEntries);
             foreach (string part in parts)
             {
                 if (!part.StartsWith("<"))
@@ -23,11 +41,19 @@ namespace OnlineVideos.Sites
                     Category cat = new RssLink()
                     {
                         ParentCategory = parentCategory,
-                        Name = part.Substring(0, 1),
-                        HasSubCategories = true
+                        Name = part.Substring(0, part.IndexOf('<')),
+                        HasSubCategories = parentCategory.Other == null
                     };
-                    parentCategory.SubCategories.Add(cat);
-                    base.ParseSubCategories(cat, part);
+                    if (!cat.HasSubCategories)
+                    {
+                        cat.Other = Parse(url, part);
+                        parentCategory.SubCategories.Add(cat);
+                    }
+                    else
+                    {
+                        if (ParseSubCategories(cat, part) > 0)
+                            parentCategory.SubCategories.Add(cat);
+                    }
                 }
             }
 
@@ -35,21 +61,25 @@ namespace OnlineVideos.Sites
             return parentCategory.SubCategories.Count;
         }
 
-        private Category GetGemist(string url)
+        public override List<VideoInfo> getVideoList(Category category)
         {
-            return new RssLink()
-            {
-                Name = "Gemist",
-                HasSubCategories = false,
-                Url = url
-            };
+            if (category.Other is List<VideoInfo>)
+                return category.Other as List<VideoInfo>;
+            return base.getVideoList(category);
         }
 
         public override string getUrl(VideoInfo video)
         {
             string[] parts = video.VideoUrl.Split('/');
-            string url = @"http://embed.kijk.nl/?width=868&height=491&video=" + parts[parts.Length - 1];
+            string url = @"http://www.kijk.nl/ajax/entitlement/" + parts[parts.Length - 2];
             string webdata = GetWebData(url, referer: video.VideoUrl);
+            JToken jt = JObject.Parse(webdata) as JToken;
+            string contentId = jt["entitlement"]["playerInfo"]["hostingkey"].Value<string>();
+
+            url = @"http://embed.kijk.nl/?width=868&height=491&video=" + parts[parts.Length - 1];
+            webdata = GetWebData(url, referer: video.VideoUrl);
+            webdata = webdata.Replace(@"<param name=\""@videoPlayer\"" value=\""\"" />", @"<param name=\""@videoPlayer\"" value=\""" + contentId + @"\"" />");
+
             Match m = regEx_FileUrl.Match(webdata);
 
             if (!m.Success)
