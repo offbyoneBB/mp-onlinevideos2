b19ee45dbec5d851a9e4d42ca705e001de3da92d
ministerkrister@gmail.com <ministerkrister@gmail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2014-11-18 00:49:02 +0000

    Netflix site changed. Fix for changing profiles, search and Netflix original series. New features: enable descriptions and Play Now in My List Category. I have tried to structure the code, but I think it's in a worse state than earlier...

---
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.ministerk/Extensions/NetflixExtensions.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OnlineVideos.Sites.Utils;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace OnlineVideos.Sites.Ministerk.Extensions
+{
+    static class NetflixExtensions
+    {
+        public static bool IsPlayNow(this Category c)
+        {
+            var other = c.Other;
+            return (other is SerializableDictionary<string, string> && (other as SerializableDictionary<string, string>).ContainsKey(NetflixUtils.PlayNow));
+        }
+
+        public static void SetPlayNow(this  Category c, bool playNow)
+        {
+            SerializableDictionary<string, string> d;
+            if (c.Other == null || !(c.Other is SerializableDictionary<string, string>))
+                d = new SerializableDictionary<string, string>();
+            else
+                d = c.Other as SerializableDictionary<string, string>;
+            if (d.ContainsKey(NetflixUtils.PlayNow))
+                d.Remove(NetflixUtils.PlayNow);
+            if (playNow)
+                d.Add(NetflixUtils.PlayNow, NetflixUtils.PlayNow);
+            c.Other = d;
+        }
+
+        public static bool RememberDiscoveredItems(this Category c)
+        {
+            var other = c.Other;
+            if (other is SerializableDictionary<string, string> && (other as SerializableDictionary<string, string>).ContainsKey(NetflixUtils.RememberDiscoveredItems))
+            {
+                return (other as SerializableDictionary<string, string>)[NetflixUtils.RememberDiscoveredItems] == true.ToString();
+            }
+            return true;
+        }
+
+        public static void SetRememberDiscoveredItems(this  Category c, bool remember)
+        {
+            SerializableDictionary<string, string> d;
+            if (c.Other == null || !(c.Other is SerializableDictionary<string, string>))
+                d = new SerializableDictionary<string, string>();
+            else
+                d = c.Other as SerializableDictionary<string, string>;
+            if (d.ContainsKey(NetflixUtils.RememberDiscoveredItems))
+                d.Remove(NetflixUtils.RememberDiscoveredItems);
+            d.Add(NetflixUtils.RememberDiscoveredItems, remember.ToString());
+            c.Other = d;
+        }
+
+        public static void SetProfile(this  Category c, JToken profile)
+        {
+            SerializableDictionary<string, string> d;
+            if (c.Other == null || !(c.Other is SerializableDictionary<string, string>))
+                d = new SerializableDictionary<string, string>();
+            else
+                d = c.Other as SerializableDictionary<string, string>;
+            if (d.ContainsKey(NetflixUtils.Profile))
+                d.Remove(NetflixUtils.Profile);
+            d.Add(NetflixUtils.Profile, profile.ToString());
+            c.Other = d;
+        }
+
+        public static JObject GetProfile(this  Category c)
+        {
+            if (c.Other == null || !(c.Other is SerializableDictionary<string, string>))
+                return null;
+            SerializableDictionary<string, string> d = c.Other as SerializableDictionary<string, string>;
+            if (!d.ContainsKey(NetflixUtils.Profile))
+                return null;
+            return (JObject)JsonConvert.DeserializeObject((string)d[NetflixUtils.Profile]);
+        }
+
+        private static string GetStringProperty(Category c, string key)
+        {
+            var other = c.Other;
+            if (other != null && other is SerializableDictionary<string, string> && (other as SerializableDictionary<string, string>).ContainsKey(key))
+                return (other as SerializableDictionary<string, string>)[key];
+            return "";
+        }
+
+        private static void SetStringProperty(Category c, string s, string key)
+        {
+            SerializableDictionary<string, string> d;
+            if (c.Other == null || !(c.Other is SerializableDictionary<string, string>))
+                d = new SerializableDictionary<string, string>();
+            else
+                d = c.Other as SerializableDictionary<string, string>;
+
+            if (d.ContainsKey(key))
+                d.Remove(key);
+            d.Add(key, s);
+            c.Other = d;
+        }
+
+        public static void SetStartIndex(this Category c, string s)
+        {
+            NetflixExtensions.SetStringProperty(c, s, NetflixUtils.StartIndex);
+        }
+
+        public static string GetStartIndex(this Category c)
+        {
+            return NetflixExtensions.GetStringProperty(c, NetflixUtils.StartIndex);
+        }
+
+        public static void SetTrackId(this Category c, string s)
+        {
+            NetflixExtensions.SetStringProperty(c, s, NetflixUtils.TrackId);
+        }
+
+        public static string GetTrackId(this Category c)
+        {
+            return NetflixExtensions.GetStringProperty(c, NetflixUtils.TrackId);
+        }
+
+        public static void SetState(this Category c, string s)
+        {
+            NetflixExtensions.SetStringProperty(c, s, NetflixUtils.State);
+        }
+
+        public static string GetState(this Category c)
+        {
+            return NetflixExtensions.GetStringProperty(c, NetflixUtils.State);
+        }
+
+        public static void SetData(this Category c, string s)
+        {
+            NetflixExtensions.SetStringProperty(c, s, NetflixUtils.Data);
+        }
+
+        public static string GetData(this Category c)
+        {
+            return NetflixExtensions.GetStringProperty(c, NetflixUtils.Data);
+        }
+
+        public static bool IsProfilesState(this Category c)
+        {
+            return c.GetState() == NetflixUtils.ProfilesState;
+        }
+
+        public static bool IsProfileState(this Category c)
+        {
+            return c.GetState() == NetflixUtils.ProfileState;
+        }
+
+        public static bool IsKidsState(this Category c)
+        {
+            return c.GetState() == NetflixUtils.KidsState;
+        }
+
+        public static bool IsSinglePageCategoriesState(this Category c)
+        {
+            return c.GetState() == NetflixUtils.SinglePageCategoriesState;
+        }
+
+        public static bool IsMultiplePageCategoriesState(this Category c)
+        {
+            return c.GetState() == NetflixUtils.MultiplePageCategoriesState;
+        }
+
+        public static bool IsTitleState(this Category c)
+        {
+            return c.GetState() == NetflixUtils.TitleState;
+        }
+
+        public static bool IsEpisodesState(this Category c)
+        {
+            return c.GetState() == NetflixUtils.EpisodesState;
+        }
+
+        public static bool IsHelpState(this Category c)
+        {
+            return c.GetState() == NetflixUtils.HelpState;
+        }
+
+        public static bool IsHomeCategoriesState(this Category c)
+        {
+            return c.GetState() == NetflixUtils.HomeCategoriesState;
+        }
+
+        public static bool IsHomeCategoryState(this Category c)
+        {
+            return c.GetState() == NetflixUtils.HomeCategoryState;
+        }
+    }
+}
--- trunk/SiteUtilProjects/OnlineVideos.Sites.ministerk/Utils/HelperUtils.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.ministerk/Utils/HelperUtils.cs
@@ -5,6 +5,34 @@ using System.Text;
 
 namespace OnlineVideos.Sites.Utils
 {
+    static class NetflixUtils
+    {
+        #region Category States
+        public static readonly string ProfilesState = "ProfilesState";
+        public static readonly string ProfileState = "ProfileState";
+        public static readonly string KidsState = "KidsState";
+        public static readonly string SinglePageCategoriesState = "SinglePageCategoriesState";
+        public static readonly string MultiplePageCategoriesState = "MultiplePageCategoriesState";
+        public static readonly string TitleState = "TitleState";
+        public static readonly string EpisodesState = "EpisodesState";
+        public static readonly string HelpState = "HelpState";
+        public static readonly string HomeCategoriesState = "HomeCategoriesState";
+        public static readonly string HomeCategoryState = "HomeCategoryState";
+
+        #endregion
+
+        #region Keys
+        public static readonly string State = "State";
+        public static readonly string PlayNow = "PlayNow";
+        public static readonly string Profile = "Profile";
+        public static readonly string TrackId = "TrackId";
+        public static readonly string StartIndex = "StartIndex";
+        public static readonly string Data = "Data";
+        public static readonly string RememberDiscoveredItems = "RememberDiscoveredItems";
+        #endregion
+
+    }
+
     class HelperUtils
     {
         public static string GetRandomChars(int amount)
