7bfeb015dedea2b3828c7311866eddf5039e84b0
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2015-01-12 21:10:07 +0000

    - Added ISubtitle interface - Copied SubtitleUtils from Brownard and added Webvtt2SRT

---
+++ trunk/OnlineVideos/Hoster/Base/ISubtitle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineVideos.Hoster.Base
+{
+    public interface ISubtitle
+    {
+        string SubtitleText { get; }
+    }
+}
