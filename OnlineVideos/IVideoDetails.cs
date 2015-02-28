080e530d5c9eef513bb6d861d90d8da6d4d4d715
bborgsdorf <bborgsdorf@gmail.com>
2015-02-28 14:45:54 +0000

    cleared up and commented the Favorite DB interface

--- OnlineVideos/IVideoDetails.cs
+++ OnlineVideos/IVideoDetails.cs
@@ -5,14 +5,12 @@ namespace OnlineVideos
 {
     /// <summary>
     /// If the object found at <see cref="VideoInfo.Other"/> property implements this interface, 
-	/// the additional info returned by <see cref="GetExtendedProperties"/> is set to the GUI.
+	/// the additional info returned by <see cref="GetExtendedProperties"/> is provided to the UI.
     /// </summary>
     public interface IVideoDetails
     {
-        /// <summary>
-        /// Gives custom information that will be provided to the GUI.
-        /// </summary>
-		/// <returns>A <see cref="Dictionary"/> with Name -> Value entries that should be exposed to the GUI.</returns>
+        /// <summary>Gives custom information that will be provided to the UI.</summary>
+		/// <returns>A <see cref="Dictionary"/> with Name -> Value entries that should be exposed to the UI.</returns>
         Dictionary<string, string> GetExtendedProperties();
     }
 }
