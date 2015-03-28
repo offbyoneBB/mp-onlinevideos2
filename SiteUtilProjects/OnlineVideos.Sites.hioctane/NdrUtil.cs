2c0e7f49b10117f99870c179d1393533c9b80f0d
bborgsdorf <bborgsdorf@gmail.com>
2015-03-28 12:18:17 +0000

    fixed NDR

--- SiteUtilProjects/OnlineVideos.Sites.hioctane/NdrUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.hioctane/NdrUtil.cs
@@ -12,8 +12,8 @@ namespace OnlineVideos.Sites
     public class NdrUtil : SiteUtilBase
     {
 		const string baseUrl = "http://www.ndr.de";
-		const string category_list_url = "http://www.ndr.de/mediathek/dropdown101-extapponly.html";
-		const string search_url = "http://www.ndr.de/mediathek/mediatheksuche101.html?search_video=true&query={0}";
+        const string category_list_url = "http://www.ndr.de/mediathek/sendungen_a-z/index.html";
+        const string search_url = "http://www.ndr.de/suche10.html?query={0}&search_mediathek=1&sort_by=date&range=unlimited&results_per_page=50#";
 
 		string nextPageUrl = "";
 
@@ -22,11 +22,11 @@ namespace OnlineVideos.Sites
 			Settings.Categories.Clear();
 			HtmlDocument htmlDoc = new HtmlDocument();
 			htmlDoc.LoadHtml(GetWebData(category_list_url));
-			var list = htmlDoc.DocumentNode.Descendants("ul").First(ul => ul.GetAttributeValue("class", "") == "m_az_key");
-			foreach (var h5 in list.Descendants("h5"))
+            var section = htmlDoc.DocumentNode.Descendants("section").FirstOrDefault(s => s.GetAttributeValue("class", "") =="columnedlist");
+			foreach (var li in section.Descendants("li"))
 			{
-				var a = h5.Element("a");
-				Settings.Categories.Add(new RssLink() { Name = a.InnerText, Url = baseUrl + a.GetAttributeValue("href", "").Replace("103", "105") });
+				var a = li.Element("a");
+				Settings.Categories.Add(new RssLink() { Name = a.InnerText, Url = baseUrl + a.GetAttributeValue("href", "") });
 			}
 			Settings.DynamicCategoriesDiscovered = Settings.Categories.Count > 0;
 			return Settings.Categories.Count;
@@ -39,31 +39,31 @@ namespace OnlineVideos.Sites
 
 			var htmlDoc = new HtmlDocument();
 			htmlDoc.LoadHtml(GetWebData((category as RssLink).Url));
-			foreach (var item in htmlDoc.DocumentNode.Descendants("div").Where(div => div.GetAttributeValue("class", "") == "m_teaser"))
+			foreach (var item in htmlDoc.DocumentNode.Descendants("div").Where(div => div.GetAttributeValue("class", "") == "teaser"))
 			{
 				string videoUrl = item.Descendants("a").First().GetAttributeValue("href", "");
 				if (!string.IsNullOrEmpty(videoUrl))
 				{
 					if (!Uri.IsWellFormedUriString(videoUrl, UriKind.Absolute))
 						videoUrl = new Uri(new Uri((category as RssLink).Url), videoUrl).AbsoluteUri;
-					var length = item.Descendants("span").FirstOrDefault(s => s.GetAttributeValue("class", "") == "runtime");
+                    var length = item.Descendants("div").FirstOrDefault(s => s.GetAttributeValue("class", "") == "textpadding");
 					var video = new VideoInfo()
 					{
-						Title = item.Descendants("h4").First().InnerText,
-						Description = item.Descendants("p").First().FirstChild.InnerText,
+						Title = item.Descendants("h2").First().InnerText.Trim(),
+						Description = item.Descendants("p").First().FirstChild.InnerText.Trim(),
 						VideoUrl = videoUrl,
 						Thumb = baseUrl + item.Descendants("img").First().GetAttributeValue("src", ""),
-						Length = length != null ? length.InnerText : "",
-						Airdate = HttpUtility.HtmlDecode(item.Descendants("div").First(d => d.GetAttributeValue("class", "") == "subline").InnerText)
+						Length = length != null ? length.InnerText.Trim() : "",
 					};
-					video.Airdate = video.Airdate.Substring(video.Airdate.LastIndexOf('-') + 1).Trim();
+                    var airDateNode = item.Descendants("div").FirstOrDefault(d => d.GetAttributeValue("class", "") == "subline");
+                    if (airDateNode != null) video.Airdate = HttpUtility.HtmlDecode(airDateNode.InnerText);
 					result.Add(video);
 				}
 			}
-			var weiterSpan = htmlDoc.DocumentNode.Descendants("span").FirstOrDefault(s => s.InnerText == "weiter");
-			if (weiterSpan != null && weiterSpan.ParentNode.Name == "a")
+			var weiterLink = htmlDoc.DocumentNode.Descendants("a").FirstOrDefault(s => s.GetAttributeValue("title", "") == "weiter");
+			if (weiterLink != null)
 			{
-				nextPageUrl = weiterSpan.ParentNode.GetAttributeValue("href", "");
+				nextPageUrl = weiterLink.GetAttributeValue("href", "");
 				if (!Uri.IsWellFormedUriString(nextPageUrl, UriKind.Absolute))
 					nextPageUrl = new Uri(new Uri((category as RssLink).Url), nextPageUrl).AbsoluteUri;
 				HasNextPage = true;
@@ -80,13 +80,27 @@ namespace OnlineVideos.Sites
         {
 			video.PlaybackOptions = new Dictionary<string, string>();
 			string data = GetWebData(video.VideoUrl);
-			var match = Regex.Match(data, @"playlist:\s*(?<playlist>\[.*?\])", RegexOptions.Singleline);
+            var match = Regex.Match(data, @"playlist\s*:\s*\[(?<inner>\s*
+{
+            [^{}]*
+            (
+                        (
+                                    (?<Open>{)
+                                    [^{}]*
+                        )+
+                        (
+                                    (?<Close-Open>})
+                                    [^{}]*
+                        )+
+            )*
+            (?(Open)(?!))
+})", RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace);
 			if (match.Success)
 			{
-				var json = JsonConvert.DeserializeObject<JArray>(match.Groups["playlist"].Value);
-				foreach (var item in json.First.Children())
+                var json = JsonConvert.DeserializeObject<JObject>(match.Groups["inner"].Value.Replace("\"","'").Replace("' || '", ""));
+				foreach (var item in json.Children())
 				{
-					string url = item.First.Value<string>("src");
+                    string url = (item as JProperty).Value.Value<string>("src");
 					if (!string.IsNullOrEmpty(url))
 					{
 						if (url.EndsWith("f4m"))
