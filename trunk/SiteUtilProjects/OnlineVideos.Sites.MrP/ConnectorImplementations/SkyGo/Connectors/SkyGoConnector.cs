db295be578a8b5b72ed15cf120341058a139933f
jlbpyke@googlemail.com <jlbpyke@googlemail.com@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2015-04-24 14:06:08 +0000

    Updates to improve Sky Go play/pause handling

--- trunk/SiteUtilProjects/OnlineVideos.Sites.MrP/ConnectorImplementations/SkyGo/Connectors/SkyGoConnector.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.MrP/ConnectorImplementations/SkyGo/Connectors/SkyGoConnector.cs
@@ -7,11 +7,13 @@ using OnlineVideos.Sites;
 using System.Windows.Forms;
 using OnlineVideos.Helpers;
 using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace OnlineVideos.Sites.WebAutomation.ConnectorImplementations.SkyGo.Connectors
 {
     public class SkyGoConnector : BrowserUtilConnectorBase
     {
+
         /// <summary> 
         /// The states this connector can be in - useful when waiting for browser responses
         /// </summary>
@@ -149,15 +151,15 @@ namespace OnlineVideos.Sites.WebAutomation.ConnectorImplementations.SkyGo.Connec
                             Application.DoEvents();
                             System.Threading.Thread.Sleep(200);
                         }
-                        
-                        Cursor.Position = new System.Drawing.Point(50, 50);
+
+                        CursorHelper.MoveMouseTo(50, 50);
                         Application.DoEvents();
                         CursorHelper.DoLeftMouseClick();
                         Application.DoEvents();
                         HideLoading();
                         Browser.FindForm().Activate();
                         Browser.FindForm().Focus();
-                        SetFocusToDummy();
+                        
                         ProcessComplete.Finished = true;
                         ProcessComplete.Success = true;
                     }
@@ -243,33 +245,30 @@ namespace OnlineVideos.Sites.WebAutomation.ConnectorImplementations.SkyGo.Connec
         /// <returns></returns>
         private EventResult DoPlayOrPause()
         {
-            MessageHandler.Info("DoPlayOrPause() IsPlayOrPausing: {0} Document Null: {1} ", _isPlayOrPausing, Browser.Document == null);
             if (_isPlayOrPausing || Browser.Document == null || Browser.Document.Body == null) return EventResult.Complete();
             
             _isPlayOrPausing = true;
             
             if (_playPauseHeight <= 0 || _playPausePos <= 0) _playPauseHeight = Browser.FindForm().Bottom - 80;
 
-            MessageHandler.Info("DoPlayOrPause() Move To Top Of Form", "");
-
             // Move the cursor near the top of the screen and left click to make sure the play/pause buttons become visible
-            Cursor.Position = new System.Drawing.Point(Browser.FindForm().Left + 10, Browser.FindForm().Top + 10);
+            CursorHelper.MoveMouseTo(Browser.FindForm().Left + 10, Browser.FindForm().Top + 10);
             Application.DoEvents();
             CursorHelper.DoLeftMouseClick();
             Application.DoEvents();
-            MessageHandler.Info("DoPlayOrPause() PlayPausePos: {0}", _playPausePos);
+            
             // We've previously found the play/pause button, so re-use its position
             if (_playPausePos > -1)
             {
-                Cursor.Position = new System.Drawing.Point(Browser.FindForm().Left + 10, _playPauseHeight);
+                CursorHelper.MoveMouseTo(Browser.FindForm().Left + 10, _playPauseHeight);
                 Application.DoEvents();
                 // We have to move the cursor off the play button for this to work
                 while (Cursor.Position.X < _playPausePos)
                 {
-                    Cursor.Position = new System.Drawing.Point(Cursor.Position.X + 2, _playPauseHeight);
+                    CursorHelper.MoveMouseTo(Cursor.Position.X + 2, _playPauseHeight);
                     Application.DoEvents();
                 }
-                Cursor.Position = new System.Drawing.Point(_playPausePos, _playPauseHeight);
+                CursorHelper.MoveMouseTo(_playPausePos, _playPauseHeight);
                 Application.DoEvents();
                 CursorHelper.DoLeftMouseClick();
                 Application.DoEvents();
@@ -277,7 +276,6 @@ namespace OnlineVideos.Sites.WebAutomation.ConnectorImplementations.SkyGo.Connec
             else
             {
                 _playPausePos = FindPlayPauseButton(_playPauseHeight);
-                MessageHandler.Info("DoPlayOrPause() PlayPausePos (after find): {0}", _playPausePos);
                 var attempts = 0;
                 // Move up the screen in 10 pixel increments trying to find play - only go up 20 times
                 while (attempts <= 20)
@@ -286,7 +284,6 @@ namespace OnlineVideos.Sites.WebAutomation.ConnectorImplementations.SkyGo.Connec
                     {
                         _playPauseHeight -= 10;
                         _playPausePos = FindPlayPauseButton(_playPauseHeight);
-                        MessageHandler.Info("DoPlayOrPause() PlayPausePos (after find): {0}, attempt {1}", _playPausePos, attempts);
                     }
                     else
                         break;
@@ -295,7 +292,6 @@ namespace OnlineVideos.Sites.WebAutomation.ConnectorImplementations.SkyGo.Connec
             }
 
             _isPlayOrPausing = false;
-            SetFocusToDummy();
             return EventResult.Complete();
         }
 
@@ -308,14 +304,12 @@ namespace OnlineVideos.Sites.WebAutomation.ConnectorImplementations.SkyGo.Connec
         {
             var startX = Browser.FindForm().Left;
             var coloursToLookFor = new[] { "0090BF", "D8DDE1", "0099CB", "009BCE", "007297", "00789F", "EFEDEA", "0086B1", "00789E", "0083AD" };
-            MessageHandler.Info("FindPlayPauseButton() Height: {0}, StartX: {1}", height, startX);
             // Very primitive, but set the cursor at the correct height and move across till we hit the right colour!
             // We have to move the cursor otherwise the play controls disappear
             var currentPos = startX + 40;
             while (currentPos < (startX + (Browser.Document.Body.ClientRectangle.Width / 8)))
             {
-                MessageHandler.Info("FindPlayPauseButton() Height: {0}, currentPos: {1}", height, currentPos);
-                Cursor.Position = new System.Drawing.Point(currentPos + 5, height);
+                CursorHelper.MoveMouseTo(currentPos + 5, height);
                 currentPos = Cursor.Position.X;
                 Application.DoEvents();
                 if (coloursToLookFor.Contains(CursorHelper.GetColourUnderCursor().Name.Substring(2).ToUpper()))
@@ -347,19 +341,5 @@ namespace OnlineVideos.Sites.WebAutomation.ConnectorImplementations.SkyGo.Connec
             WebBrowserHelper.ToogleSilverlightAppStorage(false);
         }
 
-        private void SetFocusToDummy()
-        {
-/*            if (Browser.Document.GetElementById("dummyFocusControl") == null)
-            {
-                //InvokeScript("$('#player_object').attr('height','99%');");
-                var newCtl = "$('<input  type=\"text\" id=\"dummyFocusControl\" style=\"width: 1px; height: 1%;opacity:0;color: transparent;\"/>')";
-                InvokeScript("$('#silverlightControlHost').append(" + newCtl + ");");
-            }
-
-            // Set focus to our dummy control every few seconds using javascript
-            InvokeScript("function setFocus() {$('#dummyFocusControl').focus();setTimeout('setFocus()', 3000);}");
-            InvokeScript("setFocus();");
-            */
-        }
     }
 }
