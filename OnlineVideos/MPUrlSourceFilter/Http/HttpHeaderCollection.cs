348ed897ebd2e852897f1c8b0ed28c00edd002f3
juraj.horvath <juraj.horvath@inmail.sk>
2015-05-02 06:54:35 +0000

    commited missing files (Http/HttpHeader.cs and Http/HttpHeaderCollection.cs)

---
+++ OnlineVideos/MPUrlSourceFilter/Http/HttpHeaderCollection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace OnlineVideos.MPUrlSourceFilter.Http
+{
+    /// <summary>
+    /// Represents class for collection of HTTP headers.
+    /// </summary>
+    [Serializable]
+    public class HttpHeaderCollection : Collection<HttpHeader>
+    {
+        #region Methods
+
+        protected override void InsertItem(int index, HttpHeader item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            base.InsertItem(index, item);
+        }
+
+        protected override void SetItem(int index, HttpHeader item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            base.SetItem(index, item);
+        }
+
+        #endregion
+    }
+}
