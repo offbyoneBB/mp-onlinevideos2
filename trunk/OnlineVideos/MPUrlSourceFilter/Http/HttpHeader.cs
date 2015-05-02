98a7704465b79b1abd7d930b9b101d8d36ef6af9
juraj.horvath@inmail.sk <juraj.horvath@inmail.sk@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2015-05-02 06:54:35 +0000

    commited missing files (Http/HttpHeader.cs and Http/HttpHeaderCollection.cs)

---
+++ trunk/OnlineVideos/MPUrlSourceFilter/Http/HttpHeader.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineVideos.MPUrlSourceFilter.Http
+{
+    /// <summary>
+    /// Represents class for HTTP header.
+    /// </summary>
+    [Serializable]
+    public class HttpHeader
+    {
+        #region Private fields
+
+        private String name;
+        private String value;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="HttpHeader"/> class.
+        /// </summary>
+        /// <param name="name">The name of HTTP header.</param>
+        /// <param name="value">The value of HTTP header.</param>
+        public HttpHeader(String name, String value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the name of HTTP header.
+        /// </summary>
+        public String Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Name");
+                }
+
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Cannot be empty string", "Name", null);
+                }
+
+                this.name = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the value of HTTP header.
+        /// </summary>
+        public String Value
+        {
+            get { return this.value; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Value");
+                }
+
+                this.value = value;
+            }
+        }
+
+        #endregion
+    }
+}
