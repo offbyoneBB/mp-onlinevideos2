ae8504ba8c89d350c08c90b166f03e165c2f5588
paul.doskabouter <paul.doskabouter@0f7611a1-2371-95b1-720d-0c3a6f3ffb8b>
2015-05-06 20:22:35 +0000

    Fixed Escapistmagazine

--- trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/TheEscapistUtil.cs
+++ trunk/SiteUtilProjects/OnlineVideos.Sites.doskabouter/TheEscapistUtil.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
 
 namespace OnlineVideos.Sites
 {
@@ -10,6 +14,35 @@ namespace OnlineVideos.Sites
             string title2 = matchGroups["Title2"].Value;
             if (!String.IsNullOrEmpty(title2))
                 video.Title += " " + title2;
+        }
+
+        public override string GetVideoUrl(VideoInfo video)
+        {
+            string data = GetWebData(video.VideoUrl);
+            Match m = Regex.Match(data, @"imsVideo.play\({(?<preUrl>[^}]*)}", defaultRegexOptions);
+            if (m.Success)
+            {
+                string preUrl = @"http://www.escapistmagazine.com/videos/vidconfig.php?" + m.Groups["preUrl"].Value.Replace("\"", "").Replace(':', '=').Replace(',', '&').Replace(";", "%3B");
+                m = Regex.Match(m.Groups["preUrl"].Value, @"""hash"":""(?<hash>[^""]*)""");
+                string s = GetWebData(preUrl);
+                byte[] a = new byte[s.Length / 2];
+                byte[] e = Encoding.ASCII.GetBytes(m.Groups["hash"].Value);
+                for (int i = 0; i < s.Length / 2; i++)
+                    a[i] = (byte)(Convert.ToByte(s.Substring(i * 2, 2), 16) ^ e[i % e.Length]);
+
+                var json = JObject.Parse(Encoding.ASCII.GetString(a));
+                var vids = json["files"]["videos"];
+                video.PlaybackOptions = new Dictionary<string, string>();
+                foreach (var vid in vids)
+                    video.PlaybackOptions.Add(vid.Value<string>("type") + ' ' + vid.Value<string>("res"), vid.Value<string>("src"));
+            }
+
+            string resultUrl;
+            if (video.PlaybackOptions.Count == 0) return "";// if no match, return empty url -> error
+            else
+                resultUrl = video.PlaybackOptions.Last().Value;
+            if (video.PlaybackOptions.Count == 1) video.PlaybackOptions = null;// only one url found, PlaybackOptions not needed
+            return resultUrl;
         }
     }
 }
