382d2a8e82c2486237a22e91cc5491259dd610b6
doskabouter <paul.doskabouter@gmail.com>
2015-10-15 20:26:54 +0200

    watchseries: fixed search

--- SiteUtilProjects/OnlineVideos.Sites.doskabouter/WatchSeriesUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/WatchSeriesUtil.cs
@@ -265,21 +265,13 @@ namespace OnlineVideos.Sites
             return getOnePageVideoList(currCategory, nextVideoListPageUrl);
         }
 
-        public override bool CanSearch
-        {
-            get
-            {
-                return true;
-            }
-        }
-
         public override List<SearchResultItem> Search(string query, string category = null)
         {
             List<SearchResultItem> cats = new List<SearchResultItem>();
 
             Regex r = new Regex(@"<tr><td\svalign=""top"">\s*<a\stitle=""[^""]*""\shref=""(?<url>[^""]*)"">\s*(?:<img\ssrc=""(?<thumb>[^""]*)"">\s*)?</a>\s*</td>\s*<td\svalign=""top"">\s*<a[^>]*><b>(?<title>[^<]*)</b></a>\s*<br>\s*<b>Description:</b>(?<description>[^<]*)</td>", defaultRegexOptions);
 
-            string webData = GetWebData(baseUrl + "/search/" + query, forceUTF8: true);
+            string webData = GetWebData(String.Format(searchUrl, query), forceUTF8: true);
             Match m = r.Match(webData);
             while (m.Success)
             {
