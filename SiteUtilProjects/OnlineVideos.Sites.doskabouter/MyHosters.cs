c66f0ecd337d880a59de55b5f13a0eaa3d4df53c
doskabouter <paul.doskabouter@gmail.com>
2016-02-21 17:12:46 +0100

    improved StreamIn

--- SiteUtilProjects/OnlineVideos.Sites.doskabouter/MyHosters.cs
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/MyHosters.cs
@@ -697,22 +697,20 @@ namespace OnlineVideos.Hoster
         {
             string webdata = WebCache.Instance.GetWebData(url);
             string timeToWait = Regex.Match(webdata, @"<span\sid=""countdown_str"">[^>]*>(?<time>[^<]+)</span>").Groups["time"].Value;
-            if (Convert.ToInt32(timeToWait) < 10)
+            if (String.IsNullOrEmpty(timeToWait) && webdata.Length < 50)
+                throw new OnlineVideosException(webdata);
+            int time;
+            if (Int32.TryParse(timeToWait, out time) && time < 10)
+                System.Threading.Thread.Sleep(time * 1001);
+            webdata = GetFromPost(url, webdata, false, null, new[] { "imhuman=+" });
+
+            string file = Helpers.StringUtils.GetSubString(webdata, @"file: """, @"""");
+            string streamer = Helpers.StringUtils.GetSubString(webdata, @"streamer: """, @"""");
+            RtmpUrl rtmpUrl = new RtmpUrl(streamer)
             {
-                System.Threading.Thread.Sleep(Convert.ToInt32(timeToWait) * 1001);
-
-                webdata = GetFromPost(url, webdata, false, null, new[] { "imhuman=+" });
-
-                string file = Helpers.StringUtils.GetSubString(webdata, @"file: """, @"""");
-                string streamer = Helpers.StringUtils.GetSubString(webdata, @"streamer: """, @"""");
-                RtmpUrl rtmpUrl = new RtmpUrl(streamer)
-                {
-                    PlayPath = file
-                };
-                return rtmpUrl.ToString();
-            }
-
-            return String.Empty;
+                PlayPath = file
+            };
+            return rtmpUrl.ToString();
 
         }
     }
