402e0f6e0be3ae36fb63e1bf84619dd71b85c28b
morpheusxx <morpheus_xx@team-mediaportal.com>
2016-05-29 20:11:00 +0200

    Fixed Amazon Prime again, adding dynamic cookies and required headers

--- SiteUtilProjects/OnlineVideos.Sites.JSurf/ConnectorImplementations/AmazonPrime/Connectors/AmazonBrowserSession.cs
+++ SiteUtilProjects/OnlineVideos.Sites.JSurf/ConnectorImplementations/AmazonPrime/Connectors/AmazonBrowserSession.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Net;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace OnlineVideos.Sites.JSurf.ConnectorImplementations.AmazonPrime.Connectors
 {
@@ -35,6 +38,10 @@ namespace OnlineVideos.Sites.JSurf.ConnectorImplementations.AmazonPrime.Connecto
                 Log.Info("Login to Amazon " + Properties.Resources.AmazonRootUrl);
                 _isLoggedIn = false;
                 _lastCulture = culture;
+
+                // Load the main page inside a WebBrowser control to retrieve all dynamically created cookies (session ids by JS)
+                _cc = GetCookies(Properties.Resources.AmazonLoginUrl);
+
                 var loginDoc = Load(Properties.Resources.AmazonLoginUrl);
                 var formElements = new FormElementCollection(loginDoc);
                 // There can appear different login pages, using other names for form / controls
@@ -65,15 +72,100 @@ namespace OnlineVideos.Sites.JSurf.ConnectorImplementations.AmazonPrime.Connecto
                     formElements["ap_password"] = password;
                 }
 
+                NameValueCollection headers = new NameValueCollection();
+                headers["Accept"] = "text/html, application/xhtml+xml, image/jxr, */*";
+                headers["Accept-Language"] = "de-DE";
+                headers["Cache-Control"] = "no-cache";
+                headers["User-Agent"] = UserAgent;
+
                 string postUrl = loginForm.Attributes["action"].Value;
                 Thread.Sleep(500);
-                string login = GetWebData(postUrl, formElements.AssemblePostPayload(), _cc, null, null, false, false, UserAgent);
+                string login = GetWebData(postUrl, formElements.AssemblePostPayload(), _cc, Properties.Resources.AmazonLoginUrl, null, false, false, UserAgent, null, headers);
                 // TODO add login check
+                if (!login.Contains("nav_prime_member_btn"))
+                {
+                    _isLoggedIn = false;
+                    Log.Info("Login at AP failed.");
+                    return;
+                }
                 _isLoggedIn = true;
                 _lastLogin = day;
                 Log.Info("Login complete");
             }
         }
 
+        /// <summary>
+        /// Starts a new thread using STA apartment state (<see cref="ApartmentState.STA"/>). This is required for accessing some windows features like the clipboard.
+        /// </summary>
+        /// <param name="threadStart">Thread to start.</param>
+        public static Thread RunSTAThreaded(ThreadStart threadStart)
+        {
+            Thread newThread = new Thread(threadStart);
+            newThread.SetApartmentState(ApartmentState.STA);
+            newThread.Start();
+            return newThread;
+        }
+
+        public CookieContainer GetCookies(string url)
+        {
+            ManualResetEvent ready = new ManualResetEvent(false);
+            CookieContainer cc = null;
+            RunSTAThreaded(
+                () =>
+                {
+                    var wnd = new BrowserWindow { NavigateUrl = url };
+                    Application.Run(wnd);
+                    cc = wnd.CookieContainer;
+                    ready.Set();
+                });
+
+            ready.WaitOne(10000);
+            return cc;
+        }
+    }
+
+    class BrowserWindow : Form
+    {
+        public string NavigateUrl { get; set; }
+        public CookieContainer CookieContainer { get; private set; }
+
+        public BrowserWindow()
+        {
+            ShowInTaskbar = false;
+            WindowState = FormWindowState.Minimized;
+            Load += new EventHandler(Window_Load);
+        }
+
+        void Window_Load(object sender, EventArgs e)
+        {
+            WebBrowser wb = new WebBrowser { AllowNavigation = true };
+            wb.DocumentCompleted += wb_DocumentCompleted;
+            wb.Navigate(NavigateUrl);
+        }
+
+        void wb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            WebBrowser webBrowser = (WebBrowser)sender;
+            if (webBrowser.ReadyState != WebBrowserReadyState.Complete)
+                return;
+
+            CookieContainer = GetCookieContainer(webBrowser);
+            Close();
+        }
+
+        public CookieContainer GetCookieContainer(WebBrowser webBrowser)
+        {
+            CookieContainer container = new CookieContainer();
+            // Will change: www.amazon.de to .amazon.de
+            var domain = webBrowser.Url.Host.Replace("www", string.Empty);
+            foreach (string cookie in webBrowser.Document.Cookie.Split(';'))
+            {
+                string name = cookie.Split('=')[0];
+                string value = cookie.Substring(name.Length + 1);
+                string path = "/";
+                container.Add(new Cookie(name.Trim(), value.Trim(), path, domain));
+            }
+            return container;
+        }
     }
 }
