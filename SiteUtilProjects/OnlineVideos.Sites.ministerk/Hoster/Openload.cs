bb846e295b90e29e4d7cae48748c3831afeb6de7
ministerkrister <ministerkrister@gmail.com>
2016-08-30 00:42:08 +0200

    Openload, implement ISubtitle

--- SiteUtilProjects/OnlineVideos.Sites.ministerk/Hoster/Openload.cs
+++ SiteUtilProjects/OnlineVideos.Sites.ministerk/Hoster/Openload.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace OnlineVideos.Hoster
 {
-    public class Openload : HosterBase
+    public class Openload : HosterBase, ISubtitle
     {
 
         public override string GetHosterUrl()
@@ -13,10 +13,13 @@ namespace OnlineVideos.Hoster
             return "openload.co";
         }
 
+        string sub = "";
+
         public override string GetVideoUrl(string url)
         {
 
             string data = GetWebData<string>(url);
+            sub = "";
             Regex rgx = new Regex(@"<span id=""hiddenurl"">(?<enc>.+?)</span>");
             Match m = rgx.Match(data);
             if (m.Success)
@@ -37,11 +40,44 @@ namespace OnlineVideos.Hoster
                         ScriptEngine engine = new ScriptEngine();
                         engine.Execute(js);
                         string decoded = engine.CallGlobalFunction("aaDecode", enc).ToString();
+                        SetSub(data);
                         return "https://openload.co/stream/" + decoded + "?mime=true";
                     }                    
                 }
             }
             return "";
+        }
+
+        private void SetSub(string data)
+        {
+            try
+            {
+                Regex r = new Regex(@"captions""\s+src=""(?<u>[^""]*)[^>]*?default");
+                Match m = r.Match(data);
+                if (m.Success)
+                {
+                    sub = m.Groups["u"].Value;
+                    sub = GetWebData(sub, encoding: System.Text.Encoding.UTF8, forceUTF8: true, allowUnsafeHeader:true);
+                    string oldSub = sub;
+                    r = new Regex(@"(?<time>\d\d:\d\d:\d\d.\d\d\d -->)");
+                    int i = 1;
+                    foreach (Match match in r.Matches(oldSub))
+                    {
+                        string time = match.Groups["time"].Value;
+                        sub = sub.Replace(time, "\r\n" + i.ToString() + "\r\n" + time);
+                        i++;
+                    }
+                    sub = sub.Substring(sub.IndexOf("1"));
+                }
+            } catch
+            {
+                sub = "";
+            }
+        }
+
+        public string SubtitleText
+        {
+            get { return sub; }
         }
     }
 
