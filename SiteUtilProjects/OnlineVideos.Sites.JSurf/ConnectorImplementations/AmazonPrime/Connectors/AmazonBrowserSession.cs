99e729603244366105239a861cba76a949cf580a
morpheusxx <morpheus_xx@team-mediaportal.com>
2016-11-21 18:41:40 +0100

    Fix for missing DD+ audio streams

--- SiteUtilProjects/OnlineVideos.Sites.JSurf/ConnectorImplementations/AmazonPrime/Connectors/AmazonBrowserSession.cs
+++ SiteUtilProjects/OnlineVideos.Sites.JSurf/ConnectorImplementations/AmazonPrime/Connectors/AmazonBrowserSession.cs
@@ -122,6 +122,10 @@ namespace OnlineVideos.Sites.JSurf.ConnectorImplementations.AmazonPrime.Connecto
 
             streamUrl = playbackData.SelectToken("audioVideoUrls.avCdnUrlSets[0].avUrlInfoList[0].url").Value<string>(); // Cloudfront
 
+            // Fix for missing DD+ audio streams, remove DeviceTypeID(?) part in url
+            Regex reCleanUrl = new Regex("(/d.*/).*/(video)");
+            streamUrl = reCleanUrl.Replace(streamUrl, "$1$2");
+
             additionalTags = new Dictionary<string, string>();
             for (int s = 0; ; s++)
             {
