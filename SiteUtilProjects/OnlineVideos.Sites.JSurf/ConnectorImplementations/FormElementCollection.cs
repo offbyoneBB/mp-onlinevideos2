6270b59bd9e5b60f6f9c33a96035bf0576f55730
morpheusxx <morpheus_xx@team-mediaportal.com>
2016-12-30 12:54:45 +0100

    Reformatted, added sanity check inside AssemblePostPayload

--- SiteUtilProjects/OnlineVideos.Sites.JSurf/ConnectorImplementations/FormElementCollection.cs
+++ SiteUtilProjects/OnlineVideos.Sites.JSurf/ConnectorImplementations/FormElementCollection.cs
@@ -6,97 +6,100 @@ using System.Text;
 
 namespace OnlineVideos.Sites.JSurf.ConnectorImplementations
 {
+    /// <summary>
+    /// Represents a combined list and collection of Form Elements.
+    /// </summary>
+    public class FormElementCollection : Dictionary<string, string>
+    {
         /// <summary>
-        /// Represents a combined list and collection of Form Elements.
+        /// Constructor. Parses the HtmlDocument to get all form input elements. 
         /// </summary>
-        public class FormElementCollection : Dictionary<string, string>
+        public FormElementCollection(HtmlDocument htmlDoc)
         {
-            /// <summary>
-            /// Constructor. Parses the HtmlDocument to get all form input elements. 
-            /// </summary>
-            public FormElementCollection(HtmlDocument htmlDoc)
+            var inputs = htmlDoc.DocumentNode.Descendants("input");
+            foreach (var element in inputs)
             {
-                var inputs = htmlDoc.DocumentNode.Descendants("input");
-                foreach (var element in inputs)
-                {
-                    AddInputElement(element);
-                }
+                AddInputElement(element);
+            }
 
-                var menus = htmlDoc.DocumentNode.Descendants("select");
-                foreach (var element in menus)
-                {
-                    AddMenuElement(element);
-                }
+            var menus = htmlDoc.DocumentNode.Descendants("select");
+            foreach (var element in menus)
+            {
+                AddMenuElement(element);
+            }
 
-                var textareas = htmlDoc.DocumentNode.Descendants("textarea");
-                foreach (var element in textareas)
+            var textareas = htmlDoc.DocumentNode.Descendants("textarea");
+            foreach (var element in textareas)
+            {
+                AddTextareaElement(element);
+            }
+        }
+        private void AddInputElement(HtmlNode element)
+        {
+            string name = element.GetAttributeValue("name", "");
+            string value = element.GetAttributeValue("value", "");
+            string type = element.GetAttributeValue("type", "");
+
+            if (string.IsNullOrEmpty(name)) return;
+
+            switch (type.ToLower())
+            {
+                case "checkbox":
+                case "radio":
+                    if (!ContainsKey(name)) Add(name, "");
+                    string isChecked = element.GetAttributeValue("checked", "unchecked");
+                    if (!isChecked.Equals("unchecked")) this[name] = value;
+                    break;
+                default:
+                    Add(name, value);
+                    break;
+            }
+        }
+        private void AddMenuElement(HtmlNode element)
+        {
+            string name = element.GetAttributeValue("name", "");
+            var options = element.Descendants("option");
+
+            if (string.IsNullOrEmpty(name)) return;
+
+            // choose the first option as default
+            var firstOp = options.First();
+            string defaultValue = firstOp.GetAttributeValue("value", firstOp.NextSibling.InnerText);
+
+            Add(name, defaultValue);
+
+            // check if any option is selected
+            foreach (var option in options)
+            {
+                string selected = option.GetAttributeValue("selected", "notSelected");
+                if (!selected.Equals("notSelected"))
                 {
-                    AddTextareaElement(element);
+                    string selectedValue = option.GetAttributeValue("value", option.NextSibling.InnerText);
+                    this[name] = selectedValue;
                 }
             }
-private void AddInputElement(HtmlNode element)
-{
-    string name = element.GetAttributeValue("name", "");
-    string value = element.GetAttributeValue("value", "");
-    string type = element.GetAttributeValue("type", "");            
-
-    if (string.IsNullOrEmpty(name)) return;
-
-    switch (type.ToLower())
-    {
-        case "checkbox": 
-        case "radio":
-            if (!ContainsKey(name)) Add(name, "");
-            string isChecked = element.GetAttributeValue("checked", "unchecked"); 
-            if (!isChecked.Equals("unchecked")) this[name] = value;
-            break; 
-        default: 
-            Add(name, value); 
-            break;
-    }            
-}
-private void AddMenuElement(HtmlNode element)
-{
-    string name = element.GetAttributeValue("name", "");
-    var options = element.Descendants("option");
-
-    if (string.IsNullOrEmpty(name)) return;
-
-    // choose the first option as default
-    var firstOp = options.First();
-    string defaultValue = firstOp.GetAttributeValue("value", firstOp.NextSibling.InnerText); 
-
-    Add(name, defaultValue); 
-
-    // check if any option is selected
-    foreach (var option in options)
-    {
-        string selected = option.GetAttributeValue("selected", "notSelected");
-        if (!selected.Equals("notSelected"))
+        }
+        private void AddTextareaElement(HtmlNode element)
         {
-            string selectedValue = option.GetAttributeValue("value", option.NextSibling.InnerText);
-            this[name] = selectedValue;
+            string name = element.GetAttributeValue("name", "");
+            if (string.IsNullOrEmpty(name)) return;
+            Add(name, element.InnerText);
+        }
+        /// <summary>
+        /// Assembles all form elements and values to POST. Also html encodes the values.  
+        /// </summary>
+        public string AssemblePostPayload()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var element in this)
+            {
+                var val = element.Value;
+                if (string.IsNullOrEmpty(val))
+                    continue;
+                string value = Uri.EscapeDataString(val);
+                sb.Append("&" + element.Key + "=" + value);
+            }
+            return sb.ToString().Substring(1);
         }
     }
-}
-private void AddTextareaElement(HtmlNode element)
-{
-    string name = element.GetAttributeValue("name", "");
-    if (string.IsNullOrEmpty(name)) return;
-    Add(name, element.InnerText);
-}
-            /// <summary>
-            /// Assembles all form elements and values to POST. Also html encodes the values.  
-            /// </summary>
-            public string AssemblePostPayload()
-            {
-                StringBuilder sb = new StringBuilder();
-                foreach (var element in this)
-                {
-                    string value = Uri.EscapeDataString(element.Value);
-                    sb.Append("&" + element.Key + "=" + value);
-                }
-                return sb.ToString().Substring(1);
-            }
-        }
 }
