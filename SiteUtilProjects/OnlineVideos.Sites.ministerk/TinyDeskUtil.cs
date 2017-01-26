63a15863b01b41f778f3dabd107b01cc23b40ed4
ministerkrister <ministerkrister@gmail.com>
2017-01-26 21:10:42 +0100

    Tiny Desk, new site

---
+++ SiteUtilProjects/OnlineVideos.Sites.ministerk/TinyDeskUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OnlineVideos.Sites
+{
+    public class TinyDeskUtil : GenericSiteUtil
+    {
+        public override Dictionary<string, string> GetPlaybackOptions(string playlistUrl)
+        {
+            return base.GetPlaybackOptions(playlistUrl).ToDictionary(p => p.Key, p => p.Value.Replace("\\/", "/"));
+        }
+    }
+}
