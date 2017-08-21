333b405e316d3ce710d25a8e10abbca0480ba62c
doskabouter <paul.doskabouter@gmail.com>
2017-08-21 21:25:36 +0200

    fixed ruutu

--- SiteUtilProjects/OnlineVideos.Sites.doskabouter/RuutuUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/RuutuUtil.cs
@@ -315,9 +315,9 @@ namespace OnlineVideos.Sites
                     cat.Url = @"http://www.ruutu.fi/component/726950/update?type=component&content%5Bsource%5D%5Bparams%5D%5Bmedia_type%5D=video_clip&content%5Bsource%5D%5Bparams%5D%5B" +
                         "series%5D=" + (string)parentCategory.Other + @"&content%5Bsource%5D%5Bparams%5D%5Borderby%5D=airtime&content%5Bsource%5D%5Bparams%5D%5Border_direction%5D=desc&content%5Bsource%5D%5Bparams%5D%5Bdrm%5D=1&content%5Bsource%5D%5Bparams%5D%5Blimit%5D=30&content%5Bsource%5D%5Bparams%5D%5Boffset%5D=0&content%5Bsource%5D%5Burl%5D=http%3A%2F%2Fdynamic-gatling.nelonenmedia.fi%2Fcos%2Fvideos%2F2%2F&content%5Btype%5D=video&active=true&formatter=video_cards_grid";
                 else
-                    cat.Url = @"http://www.ruutu.fi/component/726940/update?type=component&content%5Bsource%5D%5Bparams%5D%5Bhas_current_or_upcoming_online_rights%5D=2160&content%5Bsource%5D%5Bparams%5D%5Blimit%5D=100&content%5Bsource%5D%5Bparams%5D%5Borderby%5D=sequence&content%5Bsource%5D%5Bparams%5D%5Border_direction%5D=asc&content%5Bsource%5D%5Bparams%5D%5B" +
+                    cat.Url = @"http://www.ruutu.fi/component/726940/update?type=component&content%5Btype%5D=episode&content%5Bsource%5D%5Bparams%5D%5Bhas_current_or_upcoming_online_rights%5D=960&content%5Bsource%5D%5Bparams%5D%5Blimit%5D=100&content%5Bsource%5D%5Bparams%5D%5Borderby%5D=sequence&content%5Bsource%5D%5Bparams%5D%5Border_direction%5D=asc&content%5Bsource%5D%5Bparams%5D%5Bseason_id%5D=" +
                         "season_id%5D=" + node.Attributes["data-value"].Value + @"&content%5Bsource%5D%5Bparams%5D%5Bdrm%5D=1&content%5Bsource%5D%5Bparams%5D%5B" +
-                        "series_id%5D=" + (string)parentCategory.Other + @"&content%5Bsource%5D%5Bparams%5D%5Boffset%5D=0&content%5Bsource%5D%5Burl%5D=http%3A%2F%2Fdynamic-gatling.nelonenmedia.fi%2Fcos%2Fepisode%2F2%2F&content%5Btype%5D=episode&active=true&formatter=episode_thumbnails_by_season";
+                        "series_id%5D=" + (string)parentCategory.Other + @"&content%5Bsource%5D%5Bparams%5D%5Boffset%5D=0&content%5Bsource%5D%5Burl%5D=http%3A%2F%2Fdynamic-gatling.nelonenmedia.fi%2Fcos%2Fepisode%2F2%2F&active=true&formatter=episode_thumbnails_by_season";
                 parentCategory.SubCategories.Add(cat);
             }
             parentCategory.SubCategoriesDiscovered = true;
