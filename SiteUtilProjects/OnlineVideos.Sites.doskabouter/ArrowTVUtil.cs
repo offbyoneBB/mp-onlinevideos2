8fec59e2f3a28f5bdb868afac2546a179b146b0e
doskabouter <paul.doskabouter@gmail.com>
2017-10-28 21:50:20 +0200

    Added ArrowTV

---
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/ArrowTVUtil.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using OnlineVideos.Helpers;
+
+namespace OnlineVideos.Sites
+{
+    public class ArrowTVUtil : GenericSiteUtil
+    {
+        private string userHash = null;
+        private string lastId = null;
+
+        public override List<VideoInfo> GetVideos(Category category)
+        {
+            nextPageAvailable = true;
+            return GetVideoList();
+        }
+
+        public override List<VideoInfo> GetNextPageVideos()
+        {
+            nextPageAvailable = true;
+            return GetVideoList();
+        }
+
+        public override string GetVideoUrl(VideoInfo video)
+        {
+            var data = GetWebData(video.VideoUrl);
+            video.PlaybackOptions = HlsPlaylistParser.GetPlaybackOptions(data, video.VideoUrl);
+            return video.GetPreferredUrl(false);
+        }
+
+        private List<VideoInfo> GetVideoList()
+        {
+            var videoList = new List<VideoInfo>();
+            JObject webData;
+            if (userHash == null)
+                webData = GetWebData<JObject>(@"http://arrow.tv/rock/account/register", postData: @"{""campaign"":""""}");
+            else
+                webData = GetWebData<JObject>(@"http://arrow.tv/rock/account/loginbyhash", postData: @"{""userhash"":""" + userHash + @""",""campaign"":"""",""from"":" + lastId + "}");
+
+            foreach (var item in webData["playlist"])
+            {
+                var info = item["info"];
+                if (info["isMusic"].Value<bool>())
+                {
+                    VideoInfo vid = new VideoInfo()
+                    {
+                        Title = info.Value<string>("artist") + " - " + info.Value<string>("title"),
+                        VideoUrl = item["sources"][0]["file"].Value<string>(),
+                        Length = TimeUtils.TimeFromSeconds(info.Value<string>("duration"))
+
+                    };
+                    videoList.Add(vid);
+
+                    lastId = info.Value<string>("id");
+                }
+            }
+            userHash = webData["session"]["userhash"].Value<string>();
+            return videoList;
+        }
+    }
+}
