c5924a1c59ab18354350d786ea507596c0cc7030
BobSilent <andre_1@gmx.net>
2018-06-23 00:47:52 +0200

    Fix DasErste, WDR and ZDF Mediathek to new website layout and m3u8 parsing without touching onlinevideo.dll

--- SiteUtilProjects/OnlineVideos.Sites.offbyone/WDRUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.offbyone/WDRUtil.cs
@@ -15,6 +15,7 @@ namespace OnlineVideos.Sites
 		protected const string sendungenUrl = "http://www1.wdr.de/mediathek/video/sendungen-a-z/index.html";
         protected const string searchUrl = "http://www1.wdr.de/suche/index.jsp";
         protected const string livestreamUrl = "http://wdr_fs_geo-lh.akamaihd.net/z/wdrfs_geogeblockt@112044/manifest.f4m?b=608-&";
+        private string m3u8Regex = @"#EXT-X-STREAM-INF:PROGRAM-ID=\d,BANDWIDTH=(?<bitrate>\d+),RESOLUTION=(?<resolution>\d+x\d+),?CODECS=""(?<codecs>[^""]+)"".*?\n(?<url>.*)";
 
         public override int DiscoverDynamicCategories()
         {
@@ -32,7 +33,7 @@ namespace OnlineVideos.Sites
 			}
 
 			var shows = new List<RssLink>();
-            
+
             // add shows for current letter (A)
             FindCategories(doc, baseUri, shows);
 
@@ -46,7 +47,7 @@ namespace OnlineVideos.Sites
 					int o_i = (int)o;
 					var o_doc = GetWebData<HtmlDocument>(urls[o_i]);
 					// wait for the previous one to be loaded so we are in alphabetical order
-                    if (o_i > 0) 
+                    if (o_i > 0)
 						WaitHandle.WaitAny(new ManualResetEvent[] { threadWaitHandles[o_i - 1] });
 					FindCategories(o_doc, baseUri, shows);
 
@@ -66,14 +67,14 @@ namespace OnlineVideos.Sites
         public override List<VideoInfo> GetVideos(Category category)
         {
             if (category.Name == "Livestream")
-                return new List<VideoInfo>() { new VideoInfo() { Title = "Livestream", VideoUrl = "http://www1.wdr.de/mediathek/video/live/index.html" } };
+                return new List<VideoInfo>() { new VideoInfo() { Title = "Livestream", VideoUrl = "https://www1.wdr.de/mediathek/video/live/index.html" } };
             else
             {
                 var baseUri = new Uri(((RssLink)category).Url);
                 return getVideos(GetWebData<HtmlDocument>(baseUri.AbsoluteUri), baseUri);
             }
         }
-		
+
 		public override bool CanSearch { get { return true; } }
 
 		public override List<SearchResultItem> Search(string query, string category = null)
@@ -95,23 +96,50 @@ namespace OnlineVideos.Sites
                     Airdate = teaser.Descendants("p").First(p => p.GetAttributeValue("class", "") == "dachzeile").Element("a").LastChild.InnerText.Replace("vom", "").Trim()
                 });
             }
-            
+
             return result;
 		}
 
         public override string GetVideoUrl(VideoInfo video)
         {
             var doc = GetWebData<HtmlDocument>(video.VideoUrl);
-            var link = doc.DocumentNode.Descendants("a").FirstOrDefault(a => a.GetAttributeValue("data-extension", "").Contains("'url': 'http://"));
+            var link = doc.DocumentNode.Descendants("a").FirstOrDefault(a => a.GetAttributeValue("data-extension", "").Contains("\"url\":\"http"));
             if (link != null)
             {
                 var json = JObject.Parse(link.GetAttributeValue("data-extension", ""));
                 var playlistUrl = json["mediaObj"].Value<string>("url");
                 var f = GetWebData(playlistUrl);
-                var videoUrl = Regex.Match(f, "\"videoURL\":\"(?<url>.*?(f4m|smil))\"").Groups["url"].Value;
-                return (videoUrl.EndsWith("smil") ? GetStreamUrlFromSmil(videoUrl) + "&g=" : videoUrl + "?g=") + Helpers.StringUtils.GetRandomLetters(12) + "&hdcore=3.9.0";
-            }
+                var startIdx = f.IndexOf('{');
+                var endIdx = f.LastIndexOf('}');
+                var length = endIdx - startIdx + 1;
+                var json2 = JObject.Parse(f.Substring(startIdx, length));
+                var videoUrl = Regex.Match(f, "\"videoURL\":\"(?<url>.*?(f4m|smil|m3u8))\"").Groups["url"].Value;
+                var url = json2["mediaResource"]["alt"].Value<string>("videoURL");
 
+
+                var playbackOptionsByUrl = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+                if (url.Contains("master.m3u8"))
+                {
+                    var uri = new Uri(new Uri(playlistUrl), url);
+                    var m3u8Data = GetWebData(uri.ToString());
+                    foreach (Match match in Regex.Matches(m3u8Data, m3u8Regex))
+                    {
+                        playbackOptionsByUrl[match.Groups["url"].Value] =
+                            string.Format("HLS - {0} - {1} kbps", match.Groups["resolution"].Value, int.Parse(match.Groups["bitrate"].Value) / 1000);
+                    }
+                }
+
+                video.PlaybackOptions = new Dictionary<string, string>();
+                foreach (var lookup in playbackOptionsByUrl.ToLookup(kvp => kvp.Value))
+                {
+                    var i = 0;
+                    foreach (var optionByUrl in lookup)
+                    {
+                        video.PlaybackOptions.Add(string.Format("{0} - {1}", optionByUrl.Value, i++), optionByUrl.Key);
+                    }
+                }
+                return video.PlaybackOptions.Select(p => p.Value).LastOrDefault();
+            }
             return null;
         }
 
