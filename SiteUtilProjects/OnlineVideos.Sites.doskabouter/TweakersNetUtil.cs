a8d9ebbaf012a501109d8e28087406cec79285d9
doskabouter <paul.doskabouter@gmail.com>
2019-05-16 21:42:43 +0200

    Added new tls version plugin-wide

--- SiteUtilProjects/OnlineVideos.Sites.doskabouter/TweakersNetUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/TweakersNetUtil.cs
@@ -13,9 +13,6 @@ namespace OnlineVideos.Sites
 
         public override int DiscoverDynamicCategories()
         {
-            ServicePointManager.Expect100Continue = true;
-            ServicePointManager.SecurityProtocol = (SecurityProtocolType) 3072|SecurityProtocolType.Ssl3|SecurityProtocolType.Tls;
-
             cc = new CookieContainer();
             string data = GetWebData(baseUrl, cookies: cc);
             Match m = Regex.Match(data, @"<input\stype=""hidden""\sname=""tweakers_token""\svalue=""(?<tweakerstoken>[^""]+)"">");
