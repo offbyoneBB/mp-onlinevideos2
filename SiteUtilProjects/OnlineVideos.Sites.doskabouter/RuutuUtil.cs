162c7b9d93f36470da36952ca1d34a913d7ac831
doskabouter <paul.doskabouter@gmail.com>
2019-05-16 21:40:58 +0200

    Added new tls version plugin-wide

--- SiteUtilProjects/OnlineVideos.Sites.doskabouter/RuutuUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/RuutuUtil.cs
@@ -15,6 +15,7 @@ namespace OnlineVideos.Sites
         public override int DiscoverDynamicCategories()
         {
             int res = base.DiscoverDynamicCategories();
+            //TODO: revert after next release (after 
             ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072 | SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls;
             foreach (RssLink cat in Settings.Categories)
                 cat.HasSubCategories = true;
