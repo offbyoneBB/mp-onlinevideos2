c3b4545915c752c2e53e074604f5db02b9475380
doskabouter <paul.doskabouter@gmail.com>
2019-09-16 22:06:02 +0200

    fixed dumpert

--- SiteUtilProjects/OnlineVideos.Sites.ksya/DumpertUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.ksya/DumpertUtil.cs
@@ -1,109 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Linq;
-using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace OnlineVideos.Sites
 {
     public class DumpertUtil : GenericSiteUtil
     {
+        public override List<VideoInfo> GetVideos(Category category)
+        {
+            string url = ((RssLink)category).Url;
+            string[] parts = url.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            int pageInd = parts.Length - 1;
+            int pageNr = Int32.Parse(parts[pageInd]);
+            parts[pageInd] = (pageNr++).ToString();
+
+            nextPageUrl = string.Join("/", parts) + '/';
+
+            var jsonData = GetWebData<JObject>(url);
+            var items = jsonData.Value<JArray>("items");
+
+            List<VideoInfo> videoList = new List<VideoInfo>();
+
+            foreach (var item in items)
+            {
+                VideoInfo videoInfo = CreateVideoInfo();
+                videoInfo.Title = item.Value<string>("title");
+                var media = item.Value<JArray>("media");
+                var variants = media[0].Value<JArray>("variants");
+                if (variants[0].Value<string>("uri").StartsWith("youtube:"))
+                {
+                    videoInfo.Other = variants[0].Value<string>("uri").Split(':')[1];
+                }
+                else
+                if (variants.Count > 1)
+                {
+                    Dictionary<string, string> vidUrls = new Dictionary<string, string>();
+                    foreach (var variant in variants)
+                        vidUrls.Add(variant.Value<string>("version"), variant.Value<string>("uri"));
+                    videoInfo.PlaybackOptions = new Dictionary<string, string>();
+
+                    //sort from high to low quality: 720p, tablet, flv, mobile
+                    if (vidUrls.ContainsKey("720p"))
+                    {
+                        videoInfo.PlaybackOptions.Add("720p", vidUrls["720p"]);
+                    }
+                    if (vidUrls.ContainsKey("tablet"))
+                    {
+                        videoInfo.PlaybackOptions.Add("High", vidUrls["tablet"]);
+                    }
+                    if (vidUrls.ContainsKey("flv"))
+                    {
+                        videoInfo.PlaybackOptions.Add("Medium", vidUrls["flv"]);
+                    }
+                    if (vidUrls.ContainsKey("mobile"))
+                    {
+                        videoInfo.PlaybackOptions.Add("Low", vidUrls["mobile"]);
+                    }
+                }
+                videoInfo.Thumb = item.Value<string>("thumbnail");
+                videoInfo.Length = Helpers.TimeUtils.TimeFromSeconds(media[0].Value<string>("duration"));
+                videoInfo.Airdate = item.Value<DateTime>("date").ToString("g");
+                videoInfo.Description = item.Value<string>("description");
+                videoList.Add(videoInfo);
+            }
+            HasNextPage = videoList.Count > 0;
+            return videoList;
+        }
 
         public override string GetVideoUrl(VideoInfo video)
         {
-            //string encodedHTML = getUrl(video);
-            string resultUrl = GetFormattedVideoUrl(video);
-            string playListUrl = GetPlaylistUrl(resultUrl);
-            if (String.IsNullOrEmpty(playListUrl))
-                return String.Empty; // if no match, return empty url -> error
-
-            // 3.b find a match in the retrieved data for the final playback url
-            if (regEx_FileUrl != null)
+            string youtubeId = video.Other as string;
+            if (!String.IsNullOrEmpty(youtubeId))
             {
-                video.PlaybackOptions = GetPlaybackOptions(playListUrl);
-                if (video.PlaybackOptions.Count == 0)
-                {
-                    //poss. youtube
-                    var data = GetWebData(playListUrl);
-                    Match m = Regex.Match(data, @"<iframe\sclass='yt-iframe'\sstyle='[^']*'\ssrc='(?<url>[^']*)'", defaultRegexOptions);
-                    if (m.Success)
-                    {
-                        video.PlaybackOptions = Hoster.HosterFactory.GetHoster("Youtube").GetPlaybackOptions(m.Groups["url"].Value);
-                        video.Other = true;
-                    }
-                    return video.GetPreferredUrl(false);
-                }
-                else
-                {
-                    // return first found url as default
-                    var enumer = video.PlaybackOptions.GetEnumerator();
-                    enumer.MoveNext();
-                    resultUrl = enumer.Current.Value;
-                }
-                if (video.PlaybackOptions.Count == 1) video.PlaybackOptions = null; // only one url found, PlaybackOptions not needed
+                var hoster = Hoster.HosterFactory.GetHoster("Youtube");
+                if (hoster != null)
+                    video.PlaybackOptions = hoster.GetPlaybackOptions("https://www.youtube.com/watch?v=" + youtubeId);
+                return video.GetPreferredUrl(false);
             }
-
-            string encodedHTML = resultUrl; //is a base64 encoded piece of javascript which contains the videos
-
-            Dictionary<String, String> videos = getVidList(encodedHTML);
-
-            if (videos != null)
-            {
-                if (videos.ContainsKey("embed"))
-                {
-                    //youtube video {"embed":"youtube:dap5lEuS5uM","still":"http:\/\/static.dumpert.nl\/stills\/6650394_e53155f4.jpg"}
-                    string youtubeId = videos["embed"].Split(':')[1];
-                    video.PlaybackOptions = Hoster.HosterFactory.GetHoster("Youtube").GetPlaybackOptions("https://www.youtube.com/watch?v=" + youtubeId);
-                    return (video.PlaybackOptions == null || video.PlaybackOptions.Count == 0) ? "" : video.PlaybackOptions.First().Value;
-                }
-                else
-                {
-                    video.PlaybackOptions = new System.Collections.Generic.Dictionary<string, string>();
-
-                    //sort from high to low quality: 720p, tablet, flv, mobile
-                    if (videos.ContainsKey("720p"))
-                    {
-                        video.PlaybackOptions.Add("720p", videos["720p"]);
-                    }
-                    if (videos.ContainsKey("tablet"))
-                    {
-                        video.PlaybackOptions.Add("High", videos["tablet"]);
-                    }
-                    if (videos.ContainsKey("flv"))
-                    {
-                        video.PlaybackOptions.Add("Medium", videos["flv"]);
-                    }
-                    if (videos.ContainsKey("mobile"))
-                    {
-                        video.PlaybackOptions.Add("Low", videos["mobile"]);
-                    }
-
-                    return video.PlaybackOptions.First().Value;
-                }
-            }
-            else
-                return String.Empty;
-        }
-
-        public override string GetFileNameForDownload(VideoInfo video, Category category, string url)
-        {
-            const string YouTube = "YouTube";
-
-            SiteUtilBase site = this;
-            if (true.Equals(video.Other) && OnlineVideoSettings.Instance.SiteUtilsList.ContainsKey(YouTube))
-                site = OnlineVideoSettings.Instance.SiteUtilsList[YouTube];
-            return base.GetFileNameForDownload(video, category, url);
-        }
-
-        private Dictionary<String, String> getVidList(string encodedHTML)
-        {
-            Dictionary<String, String> videos = new Dictionary<String, String>();
-
-            Log.Debug("DumpertUtil: base64encoded videoURLs: " + encodedHTML);
-            byte[] data = Convert.FromBase64String(encodedHTML);
-            string videoJson = Encoding.UTF8.GetString(data);
-            return JsonConvert.DeserializeObject<Dictionary<string, string>>(videoJson);
+            return video.GetPreferredUrl(true);
         }
     }
 }
