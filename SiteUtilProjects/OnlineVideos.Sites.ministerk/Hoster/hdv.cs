8c100df53fdb0cd2d7f3ec20d09b9f83e1f432f0
doskabouter <paul.doskabouter@gmail.com>
2020-04-05 16:51:55 +0200

    Fixed hdv hoster

--- SiteUtilProjects/OnlineVideos.Sites.ministerk/Hoster/hdv.cs
+++ SiteUtilProjects/OnlineVideos.Sites.ministerk/Hoster/hdv.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Text;
+using System.Linq;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json.Linq;
 
@@ -19,25 +19,12 @@ namespace OnlineVideos.Hoster
             return "hdv.fun";
         }
 
-        public override string GetVideoUrl(string url)
+        public override Dictionary<string, string> GetPlaybackOptions(string url)
         {
-            var data = GetWebData(url);
-            Match m = Regex.Match(data, @"\$\.post\('(?<url>[^']*)',");
-            Match m2 = Regex.Match(data, @"'(?<key>[^']*)':'(?<value>[^']*)'");
+            Match m = Regex.Match(url, @"/(?<imdbid>[^/]+)$");
             if (m.Success)
             {
-                StringBuilder sb = new StringBuilder();
-                while (m2.Success)
-                {
-                    if (sb.Length != 0)
-                        sb.Append('&');
-                    sb.Append(m2.Groups["key"].Value);
-                    sb.Append('=');
-                    sb.Append(m2.Groups["value"].Value);
-                    m2 = m2.NextMatch();
-                }
-
-                data = GetWebData("https://api.hdv.fun" + m.Groups["url"].Value, sb.ToString());
+                var data = GetWebData("https://api.hdv.fun/l1", "imdb=" + m.Groups["imdbid"].Value);
                 JArray json = JArray.Parse(data);
                 string urlwithsubs = null;
                 string firsturl = null;
@@ -45,10 +32,17 @@ namespace OnlineVideos.Hoster
                 foreach (var src in json)
                 {
                     if (firsturl == null)
+                    {
                         firsturl = src["src"][0].Value<String>("src");
+                        if (!firsturl.StartsWith("http"))
+                            firsturl = "https:" + firsturl;
+                    }
                     if (!String.IsNullOrEmpty(subtitleLanguages) && urlwithsubs == null && src["sub"] is JObject)
                     {
                         urlwithsubs = src["src"][0].Value<String>("src");
+                        if (!urlwithsubs.StartsWith("http"))
+                            urlwithsubs = "https:" + urlwithsubs;
+
                         foreach (var sub in src["sub"].Children())
                         {
                             string lang = sub.First.Value<string>("lg");
@@ -66,9 +60,24 @@ namespace OnlineVideos.Hoster
                         subtitleText = Helpers.SubtitleUtils.Webvtt2SRT(subData);
                     }
                 }
-                return urlwithsubs != null ? urlwithsubs : firsturl;
+
+                string finalUrl = urlwithsubs != null ? urlwithsubs : firsturl;
+                data = GetWebData(finalUrl);
+                var res = Helpers.HlsPlaylistParser.GetPlaybackOptions(data, finalUrl);
+                if (res.Count == 1 && res.First().Key == @"0x0 (0 Kbps)")
+                {
+                    res.Clear();
+                    res.Add("dummy", finalUrl);
+                }
+                return res;
             }
             return null;
+        }
+        public override string GetVideoUrl(string url)
+        {
+            var result = GetPlaybackOptions(url);
+            if (result != null && result.Count > 0) return result.Last().Value;
+            else return String.Empty;
         }
 
         private string getSubUrl(Dictionary<string, string> subs, string languages)
