0ba0491fc9e42b89a3ac6b6d97c5b6ad35f8309b
doskabouter <paul.doskabouter@gmail.com>
2021-02-28 13:25:18 +0100

    fixed losmovies

--- SiteUtilProjects/OnlineVideos.Sites.ministerk/LosMoviesUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.ministerk/LosMoviesUtil.cs
@@ -41,7 +41,7 @@ namespace OnlineVideos.Sites
         [Category("OnlineVideosUserConfiguration"), Description("Select subtitle language preferences (; separated and ISO 639-2), for example: eng;ger")]
         protected string subtitleLanguages = "";
 
-        private const string baseUrl = @"http://losmovies.live";
+        private const string baseUrl = @"https://losmovies.site";
         private string nextPageUrl = "";
         private string currentCategoryThumb = "";
 
