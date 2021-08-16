c38400d3050c8e7464414b6095c31709a0a037a9
doskabouter <paul.doskabouter@gmail.com>
2021-08-16 21:13:22 +0200

    Added generic httpurl util

---
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/HttpUtil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using OnlineVideos.MPUrlSourceFilter;
+
+namespace OnlineVideos.Sites
+{
+    public class HttpUtil : GenericSiteUtil
+    {
+        [Category("OnlineVideosConfiguration"), Description("LiveStream")]
+        protected bool liveStream;
+
+        [Category("OnlineVideosConfiguration"), Description("Referer")]
+        protected String referer = HttpUrl.DefaultHttpReferer;
+
+        [Category("OnlineVideosConfiguration"), Description("UserAgent")]
+        protected String userAgent = HttpUrl.DefaultHttpUserAgent;
+
+        private String streamFileName = HttpUrl.DefaultStreamFileName;
+        public override string GetVideoUrl(VideoInfo video)
+        {
+            HttpUrl httpUrl = new HttpUrl(video.VideoUrl)
+            {
+                LiveStream = liveStream,
+                Referer = referer,
+                UserAgent = userAgent,
+                StreamFileName = streamFileName
+            };
+            return httpUrl.ToString();
+        }
+    }
+}
