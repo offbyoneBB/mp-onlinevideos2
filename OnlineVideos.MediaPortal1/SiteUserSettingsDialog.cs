4ca392a72bc90cda64f4e4013ac0fd2e7983cd01
doskabouter <paul.doskabouter@gmail.com>
2021-11-21 20:22:22 +0100

    - Minor improvement for boolean settings

--- OnlineVideos.MediaPortal1/SiteUserSettingsDialog.cs
+++ OnlineVideos.MediaPortal1/SiteUserSettingsDialog.cs
@@ -27,7 +27,7 @@ namespace OnlineVideos.MediaPortal1
                     GUIDialogMenu dlgSiteOptions = (GUIDialogMenu)GUIWindowManager.GetWindow(windowId) as GUIDialogSiteUserSettings;
                     if (dlgSiteOptions == null || !((GUIDialogSiteUserSettings)dlgSiteOptions).IsAvailable) // if not available use the default one
                     {
-                        windowId = (int)GUIWindow.Window.WINDOW_DIALOG_MENU; 
+                        windowId = (int)GUIWindow.Window.WINDOW_DIALOG_MENU;
                         dlgSiteOptions = (GUIDialogMenu)GUIWindowManager.GetWindow(windowId);
                     }
                     if (dlgSiteOptions == null) return selectedSite;
@@ -37,16 +37,16 @@ namespace OnlineVideos.MediaPortal1
                     {
                         /*object valueO = ovsUserCfg.GetValue(selectedSite);
                         string value = valueO != null ? valueO.ToString() : string.Empty;*/
-						string value = selectedSite.GetConfigValueAsString(ovsUserCfg);
+                        string value = selectedSite.GetConfigValueAsString(ovsUserCfg);
                         if (ovsUserCfg.IsPassword)
                         {
                             value = new string('*', value.Length);
                         }
                         string desc = ovsUserCfg.Description;
-                        dlgSiteOptions.Add(new GUIListItem(ovsUserCfg.DisplayName, value, "", false, null) 
+                        dlgSiteOptions.Add(new GUIListItem(ovsUserCfg.DisplayName, value, "", false, null)
                         {
                             // don't set Label3 if we are not using our custom dialog
-                            Label3 = windowId == GUIDialogSiteUserSettings.GUIDIALOGMENU_ONLINEVIDEO && !string.IsNullOrEmpty(desc) ? desc : string.Empty 
+                            Label3 = windowId == GUIDialogSiteUserSettings.GUIDIALOGMENU_ONLINEVIDEO && !string.IsNullOrEmpty(desc) ? desc : string.Empty
                         });
                     }
                     dlgSiteOptions.SelectedLabel = selectIndex;
@@ -55,32 +55,19 @@ namespace OnlineVideos.MediaPortal1
                     if (dlgSiteOptions.SelectedId == -1) break;
                     else
                     {
-						OnlineVideos.Reflection.FieldPropertyDescriptorByRef prop = actualProps.First(a => a.DisplayName == dlgSiteOptions.SelectedLabelText);
+                        OnlineVideos.Reflection.FieldPropertyDescriptorByRef prop = actualProps.First(a => a.DisplayName == dlgSiteOptions.SelectedLabelText);
                         if (prop.IsBool)
                         {
-                            GUIDialogMenu dlgTrueFalse = (GUIDialogMenu)GUIWindowManager.GetWindow(windowId);
-                            dlgTrueFalse.Reset();
-                            dlgTrueFalse.SetHeading(string.Format("{0}: {1}", selectedSite.Settings.Name, prop.DisplayName));
-                            dlgTrueFalse.Add(true.ToString());
-                            dlgTrueFalse.Add(false.ToString());
-                            string value = selectedSite.GetConfigValueAsString(prop);
-							if (value == false.ToString()) dlgTrueFalse.SelectedLabel = 1;
-                            dlgTrueFalse.DoModal(GUIWindowManager.ActiveWindow);
-                            if (dlgTrueFalse.SelectedId != -1)
-                            {
-								if (value != dlgTrueFalse.SelectedLabelText)
-                                {
-									selectedSite.SetConfigValueFromString(prop, dlgTrueFalse.SelectedLabelText);
-                                    changes = true;
-                                }
-                            }
+                            bool currVal = selectedSite.GetConfigValueAsString(prop) == true.ToString();
+                            selectedSite.SetConfigValueFromString(prop, (!currVal).ToString());
+                            changes = true;
                         }
                         else if (prop.IsEnum)
                         {
                             GUIDialogMenu dlgEnum = (GUIDialogMenu)GUIWindowManager.GetWindow(windowId);
                             dlgEnum.Reset();
                             dlgEnum.SetHeading(string.Format("{0}: {1}", selectedSite.Settings.Name, prop.DisplayName));
-							string value = selectedSite.GetConfigValueAsString(prop);
+                            string value = selectedSite.GetConfigValueAsString(prop);
                             int i = 0;
                             foreach (string e in prop.GetEnumValues())
                             {
@@ -93,14 +80,14 @@ namespace OnlineVideos.MediaPortal1
                             {
                                 if (value != dlgEnum.SelectedLabelText)
                                 {
-									selectedSite.SetConfigValueFromString(prop, dlgEnum.SelectedLabelText);
+                                    selectedSite.SetConfigValueFromString(prop, dlgEnum.SelectedLabelText);
                                     changes = true;
                                 }
                             }
                         }
                         else
                         {
-							string value = selectedSite.GetConfigValueAsString(prop);
+                            string value = selectedSite.GetConfigValueAsString(prop);
                             string newValue = (string)value.Clone();
                             if (GUIOnlineVideos.GetUserInputString(ref newValue, prop.IsPassword))
                             {
@@ -108,7 +95,7 @@ namespace OnlineVideos.MediaPortal1
                                 {
                                     try
                                     {
-										selectedSite.SetConfigValueFromString(prop, newValue);
+                                        selectedSite.SetConfigValueFromString(prop, newValue);
                                         changes = true;
                                     }
                                     catch (Exception ex)
@@ -119,7 +106,7 @@ namespace OnlineVideos.MediaPortal1
                                         {
                                             dlg_error.Reset();
                                             dlg_error.SetHeading(PluginConfiguration.Instance.BasicHomeScreenName);
-											dlg_error.SetLine(1, Translation.Instance.Error);
+                                            dlg_error.SetLine(1, Translation.Instance.Error);
                                             dlg_error.SetLine(2, ex.Message);
                                             dlg_error.DoModal(GUIWindowManager.ActiveWindow);
                                         }
@@ -133,8 +120,8 @@ namespace OnlineVideos.MediaPortal1
                 if (changes)
                 {
                     var newUtilInstance = Sites.SiteUtilFactory.CloneFreshSiteFromExisting(selectedSite);
-					OnlineVideoSettings.Instance.SiteUtilsList[newUtilInstance.Settings.Name] = newUtilInstance;
-					return newUtilInstance;
+                    OnlineVideoSettings.Instance.SiteUtilsList[newUtilInstance.Settings.Name] = newUtilInstance;
+                    return newUtilInstance;
                 }
             }
             return selectedSite;
