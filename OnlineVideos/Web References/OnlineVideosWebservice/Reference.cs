452ee652ae597834336741ee66f73f37240aaa76
morpheusxx <morpheus_xx@team-mediaportal.com>
2022-02-22 19:41:26 +0100

    Updated solutions and projects to VS2022 / NET48 / PlatformToolset v143

--- OnlineVideos/Web References/OnlineVideosWebservice/Reference.cs
+++ OnlineVideos/Web References/OnlineVideosWebservice/Reference.cs
@@ -14,12 +14,12 @@
 #pragma warning disable 1591
 
 namespace OnlineVideos.OnlineVideosWebservice {
-    using System;
-    using System.Web.Services;
     using System.Diagnostics;
-    using System.Web.Services.Protocols;
+    using System;
     using System.Xml.Serialization;
     using System.ComponentModel;
+    using System.Web.Services.Protocols;
+    using System.Web.Services;
     
     
     /// <remarks/>
