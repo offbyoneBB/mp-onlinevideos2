20cc2e218c45878ae6e98f38a786f057ed84f45f
brownard <brownard@live.co.uk>
2023-12-30 14:24:43 +0000

    [WebService] Move login handling to separate page and add logged in indicator to main navbar

--- WebServiceCore/Pages/Index.cshtml.cs
+++ WebServiceCore/Pages/Index.cshtml.cs
@@ -1,26 +1,18 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
 using WebServiceCore.Models;
 using WebServiceCore.Models.Entities;
-using WebServiceCore.Services;
 
 namespace WebServiceCore.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly IUserService _userService;
         private readonly OnlineVideosDataContext _context;
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(IUserService userService, OnlineVideosDataContext context, ILogger<IndexModel> logger)
+        public IndexModel(OnlineVideosDataContext context)
         {
-            _userService = userService;
             _context = context;
-            _logger = logger;
         }
 
         public string CurrentSortingProperty { get; set; }
@@ -31,24 +23,6 @@ namespace WebServiceCore.Pages
         {
             // Populate the model with the list of sites using the default sorting
             await SetSites(null);
-        }
-
-        public async Task<IActionResult> OnPostLogin(string username, string password) 
-        {
-            ClaimsPrincipal principal = await _userService.TryAuthenticate(username, password);
-            if (principal == null)
-                return Page();
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
-            _logger.LogInformation("User {Email} logged in at {Time}", username, DateTime.UtcNow);
-            return RedirectToPage();
-        }
-
-        public async Task<IActionResult> OnPostLogout()
-        {
-            string username = HttpContext.User.Identity.Name;
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            _logger.LogInformation("User {Email} logged out at {Time}", username, DateTime.UtcNow);
-            return RedirectToPage();
         }
 
         /// <summary>
